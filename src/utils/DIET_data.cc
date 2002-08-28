b51c639dc35d9ec59efd3bcc45b973170255e677
Philippe Combes <Philippe.Combes@enseeiht.fr>
2002-08-28 10:07:19 +0000

    This commit sets the frame for version 1.0 - does not work properly yet - Some files were still not processed -

---
+++ src/utils/DIET_data.cc
@@ -0,0 +1,335 @@
+/****************************************************************************/
+/* $Id$ */
+/* DIET data interface for clients as well as servers                       */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Philippe COMBES           - LIP - ENS Lyon (France)                 */
+/*                                                                          */
+/*  This is part of DIET software.                                          */
+/*  Copyright (C) 2002 ReMaP/INRIA                                          */
+/*                                                                          */
+/****************************************************************************/
+/*
+ * $Log$
+ * Revision 1.1  2002/08/28 10:07:18  pcombes
+ * This commit sets the frame for version 1.0 - does not work properly yet
+ * - Some files were still not processed -
+ *
+ ****************************************************************************/
+
+
+#include <iostream.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "DIET_data.h"
+#include "DIET_client.h"
+#include "DIET_server.h"
+#include "types.hh"
+//#include "dietTypes.hh"
+
+
+/****************************************************************************/
+/* Useful functions for data descriptors manipulation                       */
+/* <implemented in DIET_data.cc>                                            */
+/****************************************************************************/
+
+
+size_t type_sizeof(const diet_base_type_t type)
+{
+  switch (type) {
+  case DIET_CHAR:
+  case DIET_BYTE:
+    return 1;
+  case DIET_INT:
+    return 4;
+  case DIET_LONGINT:
+    return 8;
+  case DIET_FLOAT:
+    return sizeof(float);
+  case DIET_DOUBLE:
+    return sizeof(double);
+  case DIET_SCOMPLEX:
+    return 2 * sizeof(float);
+  case DIET_DCOMPLEX:
+    return  2 * sizeof(double);
+  default:
+    return 0;
+  }
+}
+
+size_t macro_data_sizeof(const diet_data_desc_t *desc)
+{
+  switch (desc->generic.type) {
+  case DIET_SCALAR:
+    return 1;
+  case DIET_VECTOR:
+    return desc->specific.vect.size;
+  case DIET_MATRIX:
+    return (desc->specific.mat.nb_c * desc->specific.mat.nb_r);
+  case DIET_STRING:
+    return desc->specific.str.length;
+  case DIET_FILE:
+    return desc->specific.file.size;
+  default:
+    cerr << "macro_data_sizeof: Error in type (cons type)\n";
+    return 0;
+  }
+}
+
+size_t data_sizeof(const diet_data_desc_t *desc)
+{
+  return (macro_data_sizeof(desc) * type_sizeof(desc->generic.base_type));
+}
+
+size_t data_sizeof(const corba_data_desc_t *desc)
+{
+  size_t base_size = type_sizeof((diet_base_type_t) desc->base_type);
+  size_t size;
+#if 0
+  switch(desc->base_type) {
+  case DIET_CHAR:
+  case DIET_BYTE:
+    base_size = sizeof(CORBA::Char); break;
+  case DIET_INT:
+    base_size = sizeof(CORBA::Long); break;
+  case DIET_LONGINT:
+    base_size = sizeof(CORBA::LongLong); break;
+  case DIET_FLOAT:
+    base_size = sizeof(CORBA::Float); break;
+  case DIET_DOUBLE:
+    base_size = sizeof(CORBA::Double); break;
+  case DIET_SCOMPLEX:
+    base_size = 2 * sizeof(CORBA::Float); break;
+  case DIET_DCOMPLEX:
+    base_size =  2 * sizeof(CORBA::Double); break;
+  default:
+    base_size = 0;
+  }
+#endif // 0
+  switch (desc->specific._d()) {
+  case DIET_SCALAR:
+    size = 1; break;
+  case DIET_VECTOR:
+    size = desc->specific.vect().size; break;
+  case DIET_MATRIX:
+    size = (desc->specific.mat().nb_c * desc->specific.mat().nb_r); break;
+  case DIET_STRING:
+    size = desc->specific.str().length; break;
+  case DIET_FILE:
+    size = desc->specific.file().size; break;
+  default:
+    cerr << "macro_data_sizeof: Error in type (cons type)\n";
+    size = 0;
+  }
+  return (base_size * size);
+}
+
+
+/****************************************************************************/
+/* Useful functions for profile manipulation                                */
+/****************************************************************************/
+
+
+int profile_desc_cmp(const corba_profile_desc_t *p1,
+		     const corba_profile_desc_t *p2)
+{
+  int res;
+  if ((res = strcmp(p1->path, p2->path)))
+    return res;
+  if ((res = !((p1->last_in == p2->last_in)
+	       && (p1->last_inout == p2->last_inout)
+	       && (p1->last_out == p2->last_out)
+	       && (p1->param_desc.length() == p2->param_desc.length()))))
+    return res;
+  res = 0;
+  for (size_t i = 0; i < p1->param_desc.length(); i++) {
+    if ((res = !((p1->param_desc[i].type
+		  == p2->param_desc[i].type)
+		 && (p1->param_desc[i].base_type
+		     == p2->param_desc[i].base_type))))
+      return res;
+  }
+  return res;
+}
+
+
+int profile_match(const corba_profile_desc_t *sv_profile,
+		  const corba_profile_t *pb_profile)
+{
+  int res;
+  if ((res = strcmp(sv_profile->path, pb_profile->path)))
+    return res;
+  if ((res = !((sv_profile->last_in == pb_profile->last_in)
+	       && (sv_profile->last_inout == pb_profile->last_inout)
+	       && (sv_profile->last_out == pb_profile->last_out)
+	       && (sv_profile->param_desc.length()
+		   == pb_profile->param_desc.length()))))
+    return res;
+  res = 0;
+  for (size_t i = 0; i < sv_profile->param_desc.length(); i++) {
+    if ((res = !((sv_profile->param_desc[i].type
+		  == pb_profile->param_desc[i].specific._d())
+		 && (sv_profile->param_desc[i].base_type
+		     == pb_profile->param_desc[i].base_type))))
+      return res;
+  }
+  return res;
+}
+
+
+
+extern "C" {
+
+/****************************************************************************/
+/* Useful functions for data descriptors                                    */
+/****************************************************************************/
+
+
+int scalar_desc_set(diet_data_desc_t *desc, diet_persistence_mode_t mode,
+		    diet_base_type_t base_type, void *value)
+{
+  int status = 0;
+  if (!desc)
+    return 1;
+  if ((status = generic_desc_set(&(desc->generic), DIET_SCALAR, base_type)))
+    return status;
+  desc->mode = mode;
+  desc->specific.scal.value = value;
+  return status;
+}
+
+int vector_desc_set(diet_data_desc_t *desc, diet_persistence_mode_t mode,
+		    diet_base_type_t base_type, size_t size)
+{
+  int status = 0;
+  if (!desc)
+    return 1;
+  if ((status = generic_desc_set(&(desc->generic), DIET_VECTOR, base_type)))
+    return status;
+  desc->mode = mode;
+  desc->specific.vect.size = size;
+  return status;
+}
+
+int matrix_desc_set(diet_data_desc_t *desc, diet_persistence_mode_t mode,
+		    diet_base_type_t base_type, size_t nb_r, size_t nb_c,
+		    int istrans)
+{
+  int status = 0;
+  if (!desc)
+    return 1;
+  if ((status = generic_desc_set(&(desc->generic), DIET_MATRIX, base_type)))
+    return status;
+  desc->mode = mode;
+  desc->specific.mat.nb_r = nb_r;
+  desc->specific.mat.nb_c = nb_c;
+  desc->specific.mat.istrans = istrans;
+  return status;
+ 
+}
+
+int string_desc_set(diet_data_desc_t *desc, diet_persistence_mode_t mode,
+		    size_t length)
+{
+  int status = 0;
+  if (!desc)
+    return 1;
+  if ((status = generic_desc_set(&(desc->generic), DIET_STRING, DIET_CHAR)))
+    return status;
+  desc->mode = mode;
+  desc->specific.str.length = length;
+  return status;
+}
+
+/* Computes the file size */
+int file_desc_set(diet_data_desc_t *desc, diet_persistence_mode_t mode,
+		  char *path)
+{
+  int status = 0;
+  struct stat buf;
+  if (!desc)
+    return 1;
+  if ((status = generic_desc_set(&(desc->generic), DIET_FILE, DIET_CHAR)))
+    return status;
+  desc->mode = mode;
+  desc->specific.file.path = path;
+  if ((status = stat(path, &buf)))
+    return status;
+  if (buf.st_mode != S_IFREG)
+    return 2;
+  desc->specific.file.size = (size_t) buf.st_size;
+  return status;
+}
+
+
+
+
+/****************************************************************************/
+/* Useful functions for data                                                */
+/****************************************************************************/
+
+int scalar_set(diet_data_t *data, void *value, diet_persistence_mode_t mode,
+	       diet_base_type_t base_type)
+{
+  int status = 0;
+  if (!data)
+    return 1;
+  if ((status = scalar_desc_set(&(data->desc), mode, base_type, value)))
+    return status;
+  data->value = value;
+  return status;
+}
+
+int vector_set(diet_data_t *data, void *value, diet_persistence_mode_t mode,
+	       diet_base_type_t base_type, size_t size)
+{
+  int status = 0;
+  if (!data)
+    return 1;
+  if ((status = vector_desc_set(&(data->desc), mode, base_type, size)))
+    return status;
+  data->value = value;
+  return status;
+}
+
+int matrix_set(diet_data_t *data, void *value, diet_persistence_mode_t mode,
+	       diet_base_type_t base_type, size_t nb_r, size_t nb_c, int istrans)
+{
+  int status = 0;
+  if (!data)
+    return 1;
+  if ((status = matrix_desc_set(&(data->desc),mode,base_type,nb_r,nb_c,istrans)))
+    return status;
+  data->value = value;
+  return status;
+}
+
+int string_set(diet_data_t *data, char *value, diet_persistence_mode_t mode,
+	       size_t length)
+{
+  int status = 0;
+  if (!data)
+    return 1;
+  if ((status = string_desc_set(&(data->desc), mode, length)))
+    return status;
+  data->value = value;
+  return status;
+}
+
+/* Computes the file size */
+int file_set(diet_data_t *data, diet_persistence_mode_t mode, char *path)
+{
+  int status = 0;
+  if (!data)
+    return 1;
+  if ((status = file_desc_set(&(data->desc), mode, path)))
+    return status;
+  data->value = NULL;
+  return status;
+}
+
+
+} // extern "C"
+
+
---
+++ src/utils/dietTypes.hh
@@ -0,0 +1,508 @@
+/****************************************************************************/
+/* $Id$ */
+/* DIET types specification                                                 */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Frederic LOMBARD          - LIFC Besançon (France)                  */
+/*                                                                          */
+/*  This is part of DIET software.                                          */
+/*  Copyright (C) 2002 ReMaP/INRIA                                          */
+/*                                                                          */
+/****************************************************************************/
+/*
+ * $Log$
+ * Revision 1.1  2002/08/28 10:07:19  pcombes
+ * This commit sets the frame for version 1.0 - does not work properly yet
+ * - Some files were still not processed -
+ *
+ * Revision 1.2  2002/05/17 20:32:12  pcombes
+ * Version alpha without FAST
+ *
+ ****************************************************************************/
+
+
+#ifndef _DIETTYPES_HH_
+#define _DIETTYPES_HH_
+
+#include <omnithread.h>
+
+#include "dlist.hh"
+#include "types.hh"
+#include "SeD.hh"
+#include "agent.hh"
+#include "DIET_server.h"
+
+#ifndef withoutfast
+#include "slimfast_api.h"
+#else
+#include "slimfast_api_local.h"
+#endif
+
+/*************************/
+/********** MAs **********/
+/*************************/
+
+struct diet_MA_desc {
+  char* name;
+  Agent_var ior;
+};
+
+typedef struct diet_MA_desc diet_MA_desc_t;
+      
+class dietMADescListElt : public _dlink {
+public:
+  diet_MA_desc_t MA;
+
+  dietMADescListElt() {}
+  ~dietMADescListElt() {}
+};
+
+class dietMADescList : public _dlist {
+public:
+  omni_mutex write;
+
+  dietMADescList():_dlist() {}
+  ~dietMADescList() {}
+};
+
+class dietMADescListIterator : public _dlistIterator {
+public:
+  dietMADescListIterator(dietMADescList *l):_dlistIterator(l) {}
+  ~dietMADescListIterator() {}
+};
+
+/*************************/
+/***** SONS DATABASE *****/
+/*************************/
+
+/*------------------------------------------------------------*/
+/* Agent's son descriptor. Describes a server/agent and its   */
+/* properties. The Corba IOR is meant to be replaced by a     */
+/* generic communicator in the final version.                 */
+/*------------------------------------------------------------*/
+
+typedef struct {
+  int id;
+  SeD_var ior;
+  char *hostname;
+} diet_SeD_desc_t;
+
+typedef struct {
+  int id;
+  Agent_var ior;
+  char *hostname;
+} diet_Agent_desc_t;
+
+/*------------------------------------------------------------*/
+/* Agent's sons lists.                                        */
+/*                                                            */
+/* FIXME: beeing copyrighted, dlists should be rewritten.     */
+/*------------------------------------------------------------*/
+
+class dietAgentDescListElt : public _dlink {
+public:
+  diet_Agent_desc_t desc;
+  dietAgentDescListElt() {}
+  ~dietAgentDescListElt() {}
+};
+
+class dietAgentDescList : public _dlist {
+public:
+  dietAgentDescList():_dlist() {}
+  ~dietAgentDescList() {}
+};
+
+class dietAgentDescListIterator : public _dlistIterator {
+public:
+  dietAgentDescListIterator(dietAgentDescList *l):_dlistIterator(l) {}
+  ~dietAgentDescListIterator() {}
+};
+
+class dietSeDDescListElt : public _dlink {
+public:
+  diet_SeD_desc_t desc;
+  dietSeDDescListElt() {}
+  ~dietSeDDescListElt() {}
+};
+
+class dietSeDDescList : public _dlist {
+public:
+  dietSeDDescList():_dlist() {}
+  ~dietSeDDescList() {}
+};
+
+class dietSeDDescListIterator : public _dlistIterator {
+public:
+  dietSeDDescListIterator(dietSeDDescList *l):_dlistIterator(l) {}
+  ~dietSeDDescListIterator() {}
+};
+
+
+/*******************************/
+/***** PROPERTIES DATABASE *****/
+/*******************************/
+
+#if 0
+
+/*------------------------------------------------------------*/
+/* Agent's sonIds lists.                                      */
+/*                                                            */
+/* FIXME: beeing copyrighted, dlists should be rewritten.     */
+/*------------------------------------------------------------*/
+
+class dietSonIdListElt : public _dlink {
+public:
+  int sonId;
+  int nbCapableServers;
+
+  dietSonIdListElt() {}
+  ~dietSonIdListElt() {}
+};
+
+
+class dietSonIdList : public _dlist {
+public:
+  dietSonIdList():_dlist() {}
+  ~dietSonIdList() {}
+};
+
+class dietSonIdListIterator : public _dlistIterator {
+public:
+  dietSonIdListIterator(dietSonIdList *l):_dlistIterator(l) {}
+  ~dietSonIdListIterator() {}
+};
+
+/*------------------------------------------------------------*/
+/* Agent's property descriptor . Describes a problem and      */
+/* lists the Ids of all sons that are able to solve it.       */
+/*------------------------------------------------------------*/
+
+struct diet_prop_desc_ {
+  diet_profile_desc_t *profile;
+  dietSonIdList *capableSons;
+};
+typedef struct diet_prop_desc_ diet_prop_desc_t;
+
+/*------------------------------------------------------------*/
+/* Agent's properties lists.                                  */
+/*                                                            */
+/* FIXME: beeing copyrighted, dlists should be rewritten.     */
+/*------------------------------------------------------------*/
+
+class dietPropDescListElt : public _dlink {
+public:
+  diet_prop_desc_t prop;
+
+  dietPropDescListElt() {}
+  ~dietPropDescListElt() {}
+};
+
+class dietPropDescList : public _dlist {
+public:
+  dietPropDescList():_dlist() {}
+  ~dietPropDescList() {}
+};
+
+class dietPropDescListIterator : public _dlistIterator {
+public:
+  dietPropDescListIterator(dietPropDescList *l):_dlistIterator(l) {}
+  ~dietPropDescListIterator() {}
+};
+
+#endif // 0
+
+
+/************/
+/* DECISION */
+/************/
+
+#if 0
+/*------------------------------------------------------------*/
+/* Request and response structures :                          */
+/* These are the standard structures for communication        */
+/* between agents and SeDs during ressource localization      */
+/* and performances evaluation,                               */
+/* The response structure is not sockets compliant for now.   */
+/* SeD IORs should be replaced by generic communicators.      */
+/*------------------------------------------------------------*/
+
+/*------------------------------------------------------*/
+/* Rem: The role field of the sf_pb_desc_t structure    */
+/* is not used in this context. It may be used to store */
+/* the data unique identifier (for the localization     */
+/* service. Using an int as identifier should speed up  */
+/* the lookups in the localization table.               */
+/*------------------------------------------------------*/
+
+typedef struct {
+  int reqId;
+  sf_inst_desc_t pb_profile;
+} diet_request_t;
+
+struct diet_data_loc_ {
+  SeD_var localization;
+  char* hostname;
+  int port;
+
+  double timeToMe;
+};
+
+typedef struct diet_data_loc_ diet_data_loc_t;
+
+struct diet_server_comp_ {
+  SeD_var myRef;
+  char* hostname;
+  int port;
+
+  char* implName;
+  double tComp;
+  double *tComm;
+};
+
+typedef struct diet_server_comp_ diet_server_comp_t;
+
+struct diet_response_ {
+  int reqId;
+
+  int myId;
+  int nbIn;
+  int nbServers;
+
+  diet_data_loc_t *data;
+  diet_server_comp_t *comp;
+};
+
+typedef struct diet_response_ diet_response_t;
+
+#endif // 0
+
+/*------------------------------------------------------------*/
+/* Request log structure :                                    */
+/* This structure describe the state of a request that is     */
+/* beeing processed. All responses are included in the log    */
+/* of a request. The log structure includes a mutex that      */
+/* allow different threads to access it. A condition variable */
+/* allows to wake up the main thread of this request when all */
+/* responses are gathered.                                    */
+/* This structure is not meant to be sent to another process. */
+/* It thus has no Corba equivalent.                           */
+/*------------------------------------------------------------*/
+
+typedef struct {
+  int reqId;           /* Request ID */
+
+  corba_request_t req;  /* A copy of the request */
+
+  int nbSons;          /* how many sons where contacted */
+
+  omni_mutex respMutex; /* responses gathering mutual exclusion */
+
+  omni_condition *gatheringEnded;
+
+  int nbResp;          /* how many respomnses where gathered for now */
+
+  corba_response_t *responses; /* this is where responses are gathered */
+
+} diet_request_log_t;
+
+
+/*------------------------------------------------------------*/
+/* Request log structure list :                               */
+/* This is the type of the list where an agent stores all its */
+/* requests. Write access id protected by an encapsulated     */
+/* mutex.                                                     */
+/*------------------------------------------------------------*/
+
+class dietRequestLogListElt : public _dlink {
+public:
+  diet_request_log_t log;
+
+  dietRequestLogListElt() {}
+  ~dietRequestLogListElt() {}
+};
+
+class dietRequestLogList : public _dlist {
+public:
+  omni_mutex logMutex;
+  dietRequestLogList():_dlist() {}
+  ~dietRequestLogList() {}
+};
+
+class dietRequestLogListIterator : public _dlistIterator {
+public:
+  dietRequestLogListIterator(dietRequestLogList *l):_dlistIterator(l) {}
+  ~dietRequestLogListIterator() {}
+};
+
+#if 0
+/*------------------------------------------------------------*/
+/* Decision descriptor :                                      */
+/* Allows temporary storage of decisions                      */
+/*------------------------------------------------------------*/
+
+struct diet_decision_desc_ {
+  SeD_var chosenServer; /* Chosen server */
+
+  char *chosenServerName;
+  int chosenServerPort;  
+
+  int nbIn;
+
+  diet_data_loc_t *dataLocs; /* Localisation of each parameter */
+                             /* (localization==nil means that the parameter was not located, */
+                             /* i.e, it's still on the client) */
+
+  char *implPath; /* name of the implementation to call */
+};
+
+typedef struct  diet_decision_desc_ diet_decision_desc_t;
+
+struct diet_decision_sequence_ {
+  int nbElts;
+
+  diet_decision_desc_t* decisions;
+};
+
+typedef diet_decision_sequence_ diet_decision_sequence_t; 
+
+#endif // 0
+
+
+/*------------------------------------------------------------*/
+/* Decision descriptor list :                                 */
+/* Stores all decisions in progress in a Master Agent         */
+/*------------------------------------------------------------*/
+
+class dietDecisionDescListElt : public _dlink {
+public:
+  int reqId;
+  SeqCorbaDecision_t decision;
+
+  /* This condition variable allow to wake the 'client thread' up */
+  /* when the decision is taken :                                 */
+
+  omni_mutex mutex; 
+
+  omni_condition *decisionTaken;
+
+  dietDecisionDescListElt() {}
+  ~dietDecisionDescListElt() {}
+};
+
+class dietDecisionDescList : public _dlist {
+public:
+  omni_mutex logMutex;
+  dietDecisionDescList():_dlist() {}
+  ~dietDecisionDescList() {}
+};
+
+class dietDecisionDescListIterator : public _dlistIterator {
+public:
+  dietDecisionDescListIterator(dietDecisionDescList *l):_dlistIterator(l) {}
+  ~dietDecisionDescListIterator() {}
+};
+
+/************************/
+/* LOCALIZATION SERVICE */
+/************************/
+
+/*------------------------------------------------------------*/
+/* The following list is used on each agent to store the data */
+/* localization. It associates a data identifier (int) which  */
+/* must be unique in the system with a son Id. If a data      */
+/* identifier is in the list, then this data is in the        */
+/* subtree which root is the agent. The sonId field is the ID */
+/* of the son under which the data is.                        */
+/*------------------------------------------------------------*/
+
+class dietDataLocListElt : public _dlink {
+public:
+  int dataId;
+  int sonId;
+
+  dietDataLocListElt() {}
+  ~dietDataLocListElt() {}
+};
+
+class dietDataLocList : public _dlist {
+public:
+  omni_mutex logMutex;
+  dietDataLocList():_dlist() {}
+  ~dietDataLocList() {}
+};
+
+class dietDataLocListIterator : public _dlistIterator {
+public:
+  dietDataLocListIterator(dietDataLocList *l):_dlistIterator(l) {}
+  ~dietDataLocListIterator() {}
+};
+
+/*------------------------------------------------------------*/
+/* These structures are used by a server to keep a list of    */
+/* the datas it owns. The diet_server_data_desc_t just        */
+/* contains the data ID for now. It should be extended later  */
+/* to include informations about he data type, size and       */
+/* distribution.                                              */
+/*------------------------------------------------------------*/
+
+struct diet_server_data_desc_
+{
+  long id;
+};
+
+typedef struct diet_server_data_desc_ diet_server_data_desc_t;
+
+
+class dietServerDataDescListElt : public _dlink {
+public:
+  diet_server_data_desc_t *data;
+  
+  dietServerDataDescListElt() {}
+  ~dietServerDataDescListElt() {}
+};
+
+class dietServerDataDescList : public _dlist {
+public:
+  omni_mutex logMutex;
+  dietServerDataDescList():_dlist() {}
+  ~dietServerDataDescList() {}
+};
+
+class dietServerDataDescListIterator : public _dlistIterator {
+public:
+  dietServerDataDescListIterator(dietServerDataDescList *l):_dlistIterator(l) {}
+  ~dietServerDataDescListIterator() {}
+};
+
+
+/****************************************************************************/
+/* Useful functions for data descriptors manipulation                       */
+/* <implemented in DIET_data.cc>                                            */
+/****************************************************************************/
+
+/* Compute size in bytes of data from its descriptor. */
+size_t data_sizeof(const diet_data_desc_t *desc);
+size_t data_sizeof(const corba_data_desc_t *desc);
+
+
+/****************************************************************************/
+/* Useful functions for profile manipulation                                */
+/* <implemented in DIET_data.cc>                                            */
+/****************************************************************************/
+
+/* Profile descriptor comparison: as strcmp, return 0 if equal. */
+int profile_desc_cmp(const corba_profile_desc_t *p1,
+		     const corba_profile_desc_t *p2);
+
+/* Return true if sv_profile describes a service that matches the problem
+   that pb_profile describes. */
+int profile_match(const corba_profile_desc_t *sv_profile,
+		  const corba_profile_t *pb_profile);
+
+
+#endif // _DIETTYPES_HH_
+
+
+
+
+
