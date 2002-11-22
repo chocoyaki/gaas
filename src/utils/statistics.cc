06c8948eb4ffa150e74baeaeb51e203f5ffc38c9
Ludovic Bertsch <Ludovic.Bertsch@ens-lyon.fr>
2002-11-22 13:44:14 +0000

    I forget some files. Silly me.

---
+++ src/utils/statistics.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "statistics.hh"
+
+#ifdef HAVE_STATISTICS
+
+char* stat_type_string[] = {
+  "IN  ",
+  "OUT ",
+  "INFO"
+};
+
+static char* stat_file_name; // We don't need it to be global
+FILE* stat_file = NULL;
+
+void do_stat_init() {
+  if (stat_file != NULL) {
+    fprintf(stderr, "init_stat(): Stat module already initialized!\n"
+	    "             Continuing without reopening it!\n");
+  } else {
+    stat_file_name = getenv("DIET_STAT_FILE_NAME");
+    if (stat_file_name != NULL) {
+      stat_file = fopen(stat_file_name, "wc");
+      if (stat_file == NULL) {
+	fprintf(stderr, "init_stat(): Unable to open file \"%s\"\n", stat_file_name);
+	perror("init_stat");
+      }
+    }
+  }
+}
+
+#endif // HAVE_STATISTICS
---
+++ src/utils/statistics.hh
@@ -0,0 +1,60 @@
+#ifndef __STATISTICS_HH__
+
+#define __STATISTICS_HH__
+
+#ifdef HAVE_STATISTICS
+
+#include <stdio.h>
+#include <sys/time.h>
+
+enum stat_type { STAT_IN, STAT_OUT, STAT_INFO };
+
+// Please, don't use those variables
+extern FILE* stat_file;
+extern char* stat_type_string[3];
+
+// Don't call this, call stat_in, stat_out & stat_info instead
+inline void gen_stat(int type, char *message) {
+  if (stat_file != NULL) {
+    struct timeval tv;
+    struct timezone tz;
+
+    if (gettimeofday(&tv, &tz) == 0) {
+      fprintf(stat_file, "%10ld.%06ld|%s|%s\n", 
+	      tv.tv_sec, tv.tv_usec,
+	      stat_type_string[type],
+	      message);
+
+      /* Examples of generated trace :
+       * 123456.340569|IN  |submission.start
+       * 123456.340867|INFO|submission.phase1
+       * 123455.345986|INFO|submission.phase2
+       * 123456.354032|OUT |submission.end
+       */
+    }
+  }
+}
+
+// Don't call this, call init_stat instead!
+void do_stat_init();
+
+/////////////////////////////////
+// Use only the following calls :
+
+#define stat_init()        do_stat_init()
+
+#define stat_in(message)   gen_stat(STAT_IN, message)
+#define stat_out(message)  gen_stat(STAT_OUT, message)
+#define stat_info(message) gen_stat(STAT_INFO, message)
+
+#else // HAVE_STATISTICS
+
+#define stat_init()
+
+#define stat_in()
+#define stat_out()
+#define stat_info()
+
+#endif // HAVE_STATISTICS
+
+#endif // __STATISTICS_HH__
