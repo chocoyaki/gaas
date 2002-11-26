aebf718b8e5cd02563b18ca18ebe59233eda38aa
Ludovic Bertsch <Ludovic.Bertsch@ens-lyon.fr>
2002-11-26 13:58:06 +0000

    Small correction

--- src/utils/statistics.cc
+++ src/utils/statistics.cc
@@ -16,16 +16,11 @@ static char* stat_file_name; // We don't need it to be global
 FILE* stat_file = NULL;
 
 void do_stat_init() {
-  char *stat_file_name_base;
-  char tmp[100000];
-
   if (stat_file != NULL) {
     fprintf(stderr, "do_stat_init(): Stat module already initialized!\n"
 	    "             Continuing without reopening it!\n");
   } else {
-    stat_file_name_base = getenv("DIET_STAT_FILE_NAME");
-    sprintf(tmp, "%s.%d", stat_file_name_base, getpid());
-    stat_file_name_base = strdup(tmp);
+    stat_file_name = getenv("DIET_STAT_FILE_NAME");
 
     if (stat_file_name != NULL) {
       stat_file = fopen(stat_file_name, "wc");
