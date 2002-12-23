789c3c2a46628d2598fff87d1a9574745570895f
Sylvain Dahan <sylvain.dahan@laposte.net>
2002-12-23 16:45:58 +0000

    uses the absolute name of the exception "std::bad_alloc" instead of the local name "bad_alloc". This changes nothing about the code but now it compile on darwin.

--- src/utils/ms_function.cc
+++ src/utils/ms_function.cc
@@ -1,16 +1,24 @@
+/* $Log$
+/* Revision 1.2  2002/12/23 16:45:58  sdahan
+/* uses the absolute name of the exception "std::bad_alloc" instead of the
+/* local name "bad_alloc". This changes nothing about the code but now it
+/* compile on darwin.
+/*
+ */
+
 #include "ms_function.hh"
 #include <new> // definition of the bad_alloc exception
 
 char* ms_strdup(const char* str) {
   char* copy = CORBA::string_dup(str) ;
   if (copy == NULL)
-    throw bad_alloc() ;
+    throw std::bad_alloc() ;
   return copy ;
 } 
 
 char* ms_stralloc(int length) {
   char* str = CORBA::string_alloc(length) ;
   if (str == NULL)
-    throw bad_alloc() ;
+    throw std::bad_alloc() ;
   return str ;
 }
