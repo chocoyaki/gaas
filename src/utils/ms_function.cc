c2b7f844100269a8610d87c173c3c050f0f22682
Sylvain Dahan <sylvain.dahan@laposte.net>
2002-12-23 17:02:06 +0000

    Add the documentation of the ms_function tool. This is a set of memory use functions that throw an bad_alloc exception if there is not enough memory.

--- src/utils/ms_function.cc
+++ src/utils/ms_function.cc
@@ -1,9 +1,13 @@
-/* $Log$
-/* Revision 1.2  2002/12/23 16:45:58  sdahan
-/* uses the absolute name of the exception "std::bad_alloc" instead of the
-/* local name "bad_alloc". This changes nothing about the code but now it
-/* compile on darwin.
 /*
+ * $Log$
+ * Revision 1.3  2002/12/23 17:02:06  sdahan
+ * Add the documentation of the ms_function tool. This is a set of memory use
+ * functions that throw an bad_alloc exception if there is not enough memory.
+ *
+ * Revision 1.2  2002/12/23 16:45:58  sdahan
+ * uses the absolute name of the exception "std::bad_alloc" instead of the
+ * local name "bad_alloc". This changes nothing about the code but now it
+ * compile on darwin.
  */
 
 #include "ms_function.hh"
--- src/utils/ms_function.hh
+++ src/utils/ms_function.hh
@@ -1,11 +1,57 @@
+// $Id$
+
+/*
+ * $Log$
+ * Revision 1.3  2002/12/23 17:02:06  sdahan
+ * Add the documentation of the ms_function tool. This is a set of memory use
+ * functions that throw an bad_alloc exception if there is not enough memory.
+ *
+ */
+
+/**
+ * \page ms_function
+ *
+ * This is a set of function that allocate and deallocate the memory
+ * and send a \c bad_alloc() if there is not enough memory. By using
+ * those functions the programmer don't have to check every return of
+ * the memory functions.
+ *
+ * @author Sylvain DAHAN - LIFC Besancon (France)
+ */
+
 #ifndef _MS_FUNCTION_HH_
 #define _MS_FUNCTION_HH_
 
 #include <omniORB4/CORBA.h>
 
+/**
+ * Duplicates the string given in argument with the \c
+ * CORBA::string_dup() function. If there is no enough memory to
+ * allocate the string, it throws a \c bad_alloc exception. 
+ *
+ * @param str the string to duplicate
+ *
+ * @return a clone of \c str.
+ */
 char* ms_strdup(const char* str) ;
+
+/**
+ * Create an new string of length \c length with the \c
+ * CORBA::string_alloc() function. If there is no enough memory to
+ * allocate the string, it throws a \c bad_alloc exception.
+ *
+ * @param length the length of the new string.
+ *
+ * @return an undefined string of length \c length.
+ */
 char* ms_stralloc(int length) ;
 
+/**
+ * Frees the memory of the string given in argument with the function
+ * \c CORBA::string_free.
+ *
+ * @param str the string who is destroyed
+ */
 #define ms_strfree(str) CORBA::string_free(str)
 
 #endif _MS_FUNCTION_HH_
