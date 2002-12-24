b16a2c4cf5f1c3c42ba86af3c8d42701fa79a3fe
Ludovic Bertsch <Ludovic.Bertsch@ens-lyon.fr>
2002-12-24 08:25:38 +0000

    Added a way to execute n tests by a command line argument : Usage is : client --repeat <n> <cfg file> <op>

--- src/examples/dmat_manips/client.c
+++ src/examples/dmat_manips/client.c
@@ -12,6 +12,10 @@
 /****************************************************************************/
 /*
  * $Log$
+ * Revision 1.11  2002/12/24 08:25:38  lbertsch
+ * Added a way to execute n tests by a command line argument :
+ * Usage is : client --repeat <n> <cfg file> <op>
+ *
  * Revision 1.10  2002/12/12 18:17:05  pcombes
  * Small bug fixes on prints (special thanks to Jean-Yves)
  *
@@ -44,6 +48,13 @@
 
 #include "DIET_client.h"
 
+void usage() {
+  fprintf(stderr, "Usage: client [--repeat <n>] <config file> [T|MatSUM|MatPROD]\n");
+  fprintf(stderr, "       ex: client client.cfg T\n");
+  fprintf(stderr, "           client --repeat 1000 client.cfg MatSUM\n");
+  exit(1);
+}
+
 #define print_matrix(mat, m, n)            \
   {                                        \
     size_t i, j;                           \
@@ -63,6 +74,8 @@
 int
 main(int argc, char **argv)
 {
+  int i;
+  int n_loops = 1;
   char *path;
   diet_function_handle_t *fhandle;
   diet_profile_t *profile;
@@ -72,80 +85,91 @@ main(int argc, char **argv)
 
   char *PB[3] = {"T", "MatSUM", "MatPROD"};
 
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s <file.cfg> [T|MatSUM|MatPROD]\n", argv[0]);
-    return 1;
+  for (i = 1; i < argc - 2; i++) {
+    if (strcmp("--repeat", argv[i]) == 0) {
+      n_loops = atoi(argv[i + 1]);
+      i++;
+      memcpy(argv + i - 2, argv + i, (argc - i)*sizeof(char*));
+      i -= 2;
+      argc -= 2;
+    } else {
+      fprintf(stderr, "Unrecognized option %s\n", argv[i]);
+      usage();
+    }
   }
-  path = argv[2];
+
+  if (argc - i != 2) {
+    usage();
+  }
+  path = argv[argc - 1];
   
   A = mat1;
   B = mat2;
 
-  if (diet_initialize(argc, argv, argv[1])) {
-    fprintf(stderr, "DIET initialization failed !\n");
-    return 1;
-  } 
-
-  if (!strcmp(path, PB[0])) {
+  for (i = 0; i < n_loops; i++) {
+    if (diet_initialize(argc, argv, argv[1])) {
+      fprintf(stderr, "DIET initialization failed !\n");
+      return 1;
+    } 
     
-    fhandle = diet_function_handle_default(path);
-    profile = profile_alloc(-1, 0, 0);
-    matrix_set(&(profile->parameters[0]),
-	       A, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
-    print_matrix(A,
-		 profile->parameters[0].desc.specific.mat.nb_r,
-		 profile->parameters[0].desc.specific.mat.nb_c);
-
-  } else if (!(strcmp(path, PB[1]) && strcmp(path, PB[2]))) {
-
-    fhandle = diet_function_handle_default(path);
-    profile = profile_alloc(1, 1, 2);
-    matrix_set(&(profile->parameters[0]),
-	       A, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
-    if (!(strcmp(path, PB[1]))) {
-      matrix_set(&(profile->parameters[1]),
-		 B, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
-      matrix_set(&(profile->parameters[2]),
-		 NULL, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
-    } else {
-      matrix_set(&(profile->parameters[1]),
-		 B, DIET_VOLATILE, DIET_DOUBLE, 2, 3, 0);
-      matrix_set(&(profile->parameters[2]),
-		 NULL, DIET_VOLATILE, DIET_DOUBLE, 3, 3, 0);
-    }
-
-    print_matrix(A,
-		 profile->parameters[0].desc.specific.mat.nb_r,
-		 profile->parameters[0].desc.specific.mat.nb_c);
-    print_matrix(B,
-		 profile->parameters[1].desc.specific.mat.nb_r,
-		 profile->parameters[1].desc.specific.mat.nb_c);
-
-  } else {
-    fprintf(stderr, "DIET initialization failed !\n");
-    return 1;
-  } 
-    
-  if (!diet_call(fhandle, profile)) {
     if (!strcmp(path, PB[0])) {
+      
+      fhandle = diet_function_handle_default(path);
+      profile = profile_alloc(-1, 0, 0);
+      matrix_set(&(profile->parameters[0]),
+		 A, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
       print_matrix(A,
 		   profile->parameters[0].desc.specific.mat.nb_r,
 		   profile->parameters[0].desc.specific.mat.nb_c);
+      
+    } else if (!(strcmp(path, PB[1]) && strcmp(path, PB[2]))) {
+      
+      fhandle = diet_function_handle_default(path);
+      profile = profile_alloc(1, 1, 2);
+      matrix_set(&(profile->parameters[0]),
+		 A, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
+      if (!(strcmp(path, PB[1]))) {
+	matrix_set(&(profile->parameters[1]),
+		   B, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
+	matrix_set(&(profile->parameters[2]),
+		   NULL, DIET_VOLATILE, DIET_DOUBLE, 3, 2, 0);
+      } else {
+	matrix_set(&(profile->parameters[1]),
+		   B, DIET_VOLATILE, DIET_DOUBLE, 2, 3, 0);
+	matrix_set(&(profile->parameters[2]),
+		   NULL, DIET_VOLATILE, DIET_DOUBLE, 3, 3, 0);
+      }
+      
+      print_matrix(A,
+		   profile->parameters[0].desc.specific.mat.nb_r,
+		   profile->parameters[0].desc.specific.mat.nb_c);
+      print_matrix(B,
+		   profile->parameters[1].desc.specific.mat.nb_r,
+		   profile->parameters[1].desc.specific.mat.nb_c);
+      
     } else {
-      C = profile->parameters[2].value;
-      print_matrix(C,
-		   profile->parameters[2].desc.specific.mat.nb_r,
-		   profile->parameters[2].desc.specific.mat.nb_c);
-    }
-  }
-  
-  profile_free(profile);
-  diet_function_handle_destruct(fhandle);
+      fprintf(stderr, "DIET initialization failed !\n");
+      return 1;
+    } 
     
-  diet_finalize();
+    if (!diet_call(fhandle, profile)) {
+      if (!strcmp(path, PB[0])) {
+	print_matrix(A,
+		     profile->parameters[0].desc.specific.mat.nb_r,
+		     profile->parameters[0].desc.specific.mat.nb_c);
+      } else {
+	C = profile->parameters[2].value;
+	print_matrix(C,
+		     profile->parameters[2].desc.specific.mat.nb_r,
+		     profile->parameters[2].desc.specific.mat.nb_c);
+      }
+    }
+    
+    profile_free(profile);
+    diet_function_handle_destruct(fhandle);
+    
+    diet_finalize();
+  }
 
   return 0;
 }
-
-
-
