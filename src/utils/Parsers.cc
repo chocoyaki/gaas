43c338c28e32cb9b0aa669a48e16d2cced57c2b6
Philippe Combes <Philippe.Combes@enseeiht.fr>
2003-05-10 08:49:33 +0000

    New Parsers for new configuration files.

--- src/utils/Parsers.cc
+++ src/utils/Parsers.cc
@@ -8,157 +8,515 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
+ * Revision 1.2  2003/05/10 08:49:33  pcombes
+ * New Parsers for new configuration files.
+ *
  * Revision 1.1  2003/04/10 12:51:22  pcombes
  * Static class for configuration files parsing.
- *
  ****************************************************************************/
 
 #include <iostream>
 using namespace std;
+#include <fstream>
 
 #include "Parsers.hh"
+#include "assert.h"
 #include "debug.hh"
 #include "DIET_config.h"
 
-
-FILE* Parsers::file = NULL;
-
 extern unsigned int TRACE_LEVEL;
 
+// !!! None of these keywords can be a prefix for another !!!
+Parsers::Results::param_t Parsers::Results::params[] =
+  {/* [0]  */ {"traceLevel", 10, Parsers::parseTraceLevel, 0, NULL},
+   /* [1]  */ {"MAName", 6, Parsers::parseName, 0, NULL},
+   /* [2]  */ {"agentType", 9, Parsers::parseAgentType, 0, NULL},
+   /* [3]  */ {"endPoint", 8, Parsers::parsePort, 0, NULL},
+   /* [4]  */ {"name", 4, Parsers::parseName, 0, NULL},
+   /* [5]  */ {"parentName", 10, Parsers::parseName, 0, NULL},
+   /* [6]  */ {"fastUse", 7, Parsers::parseUse, 0, NULL},
+   /* [7]  */ {"ldapUse", 7, Parsers::parseUse, 0, NULL},
+   /* [8]  */ {"ldapBase", 8, Parsers::parseAddress, 0, NULL},
+   /* [9]  */ {"ldapMask", 8, Parsers::parseName, 0, NULL},
+   /* [10] */ {"nwsUse", 6, Parsers::parseUse, 0, NULL},
+   /* [11] */ {"nwsNameserver", 13, Parsers::parseAddress, 0, NULL},
+   /* [12] */ {"nwsForecaster", 13, Parsers::parseAddress, 0, NULL}};
+
+#define IS_ADDRESS(i) ((i == 8) || (i == 11) || (i == 12))
+
+
+ifstream Parsers::file;
+char*    Parsers::path = "";
+size_t   Parsers::noLine = 0;
+
+
+#define DIET_PARSE_ERROR        1
+#define DIET_FILE_IO_ERROR      2
+#define DIET_MISSING_PARAMETERS 3
+
+#define CHECK_PARAM(type) \
+  if (Results::params[(type)].noLine > 0) {                           \
+    cerr << "Warning: " << FUNCTION_NAME << ": "                    \
+	 << Results::params[(type)].kwd  << " already set at line " \
+	 << Results::params[(type)].noLine << " - ignored.\n";      \
+    return 0;                                                       \
+  }
+
+
+/**
+ * Prepare the parsing of the file \c filePath (open the file and initialize
+ * the parameter structures)
+ */
 int
 Parsers::beginParsing(char* filePath)
 {
-  if (file)
-    cerr << "Warning: Parsers::beginParsing: file was not closed !\n";
-  file = fopen(filePath, "r");
-  if (!file) {
-    cerr << "Error: Parsers::beginParsing: could not open <<"
-	 << filePath << endl;
-    return 1;
+  static char* FUNCTION_NAME = "Parsers::beginParsing";
+  if ((filePath == NULL) || (*filePath == '\0')) {
+    cerr << "Error: " << FUNCTION_NAME << ": no file to parse !\n";
+    return DIET_FILE_IO_ERROR;
   }
+  Parsers::path = filePath;
+  Parsers::file.open(filePath);
+  // FIXME: this test should be wrong !!!
+  if (Parsers::file == NULL) {
+    cerr << "Error: " << FUNCTION_NAME << ": could not open "
+	 << filePath << endl;
+    return DIET_FILE_IO_ERROR;
+  }
+  Parsers::noLine = 0;
   return 0;
 }
 
+/**
+ * Free the parameters structures: all calls to Results::getParamValue must
+ * have been done before call to this method.
+ */
 int
 Parsers::endParsing()
 {
-  if (!file)
-    cerr << "Warning: Parsers::endParsing: file was already closed !\n";
-  if (fclose(file))
-    cerr << "Warning: Parsers::endParsing: could not close file !\n";
-  file = NULL;
-  return 0;
-}
+  static char* FUNCTION_NAME = "Parsers::endParsing";
 
-int
-Parsers::parseName(char* name)
-{
-  if (fscanf(file, "%256s ", name) != 1) {
-    cerr << "Error: Parsers::parseName: failed to read name.\n";
-    return 1;
+//   if (Parsers::file == NULL)
+//     cerr << "Warning: " << FUNCTION_NAME << ": file was already closed !\n";
+//   else {
+    Parsers::file.close();
+    // FIXME: this test is wrong
+    // if (Parsers::file != NULL)
+    //   cerr << "Warning: " << FUNCTION_NAME << ": could not close file !\n";
+//  }
+  for (size_t i = Results::TRACELEVEL; i < Results::NB_PARAM_TYPE; i++) {
+    if (Results::params[i].value != NULL) {
+      if (IS_ADDRESS(i))
+	delete((Results::Address*)Results::params[i].value);
+      else
+	free(Results::params[i].value);
+    }
+    Results::params[i].noLine = 0;
   }
   return 0;
 }
 
+/**
+ * Parse the file \c filePath and fill in the parameters structure. 
+ * It is possible to specify a list of compulsory parameters, so that the
+ * parsers can check itself for presence of these parameters.
+ * FAST parameters are processed apart, since they depend on each other.
+ * @param checkFASTEntries   tells if FAST entries must be checked.
+ * @param nbCompulsoryParams is the length of the list \c compulsoryParams
+ * @param compulsoryParams   is the list of compulsory parameters
+ */
 int
-Parsers::parseTraceLevel()//(unsigned int* traceLevel)
+Parsers::parseCfgFile(bool checkFASTEntries, size_t nbCompulsoryParams,
+		      Results::param_type_t* compulsoryParams)
 {
-  if (fscanf(file, "traceLevel = %ud ", &TRACE_LEVEL) != 1) {
-    cerr << "Warning: Parsers::ParseTraceLevel: could not read traceLevel "
-	 << "(set to " << TRACE_DEFAULT << ")\n";
+  static char* FUNCTION_NAME = "Parsers::parseCfgFile";
+  static char full_line[512];
+  char* ptr;
+  int parse_res;
+
+  if (Parsers::file == NULL) {
+    cerr << "Error: " << FUNCTION_NAME << ": no file has been opened."
+	 << " Please consider calling beginParsing first !\n";
+    return DIET_FILE_IO_ERROR;
+  }
+
+  while (Parsers::file.getline(full_line, 512)) {
+
+    Parsers::noLine++;
+
+    // Set Parsers::line
+    ptr = (char*) full_line;
+    // Skip leading blank characters
+    while ((*ptr == ' ') || (*ptr == '\t'))
+      ptr++;
+    if (*ptr == '#')  /* comment lines */
+      continue;
+    if (*ptr == '\0') /* empty lines */
+      continue;
+    parse_res = Parsers::parseCfgLine(ptr);
+    if (parse_res)
+      return parse_res;
+  }
+
+  /* If no traceLevel specified, set it to default */
+  if (Results::params[Results::TRACELEVEL].noLine == 0)
     TRACE_LEVEL = TRACE_DEFAULT;
-  }
+  
+  if ((parse_res =
+       Parsers::checkValidity(checkFASTEntries,
+			      nbCompulsoryParams, compulsoryParams)))
+    return parse_res;
+
   return 0;
 }
 
 
+/**
+ * Check the coherence of the FAST parameters, essentially depending on the
+ * various "Use" parameters entered.
+ */
 int
-Parsers::parseFASTEntries(int*  ldapUse, char* ldapHost,  int* ldapPort,
-			  char* ldapMask,
-			  int*  nwsUse,  char* nwsNSHost, int* nwsNSPort,
-			  char* nwsForecasterHost, int* nwsForecasterPort)
+Parsers::checkFASTEntries()
 {
-
-  /* Parse LDAP config lines */
-
-  *ldapUse = 0;
-  *ldapHost = '\0';
-  *ldapPort = 0;
-  *ldapMask = '\0';
+  static char* FUNCTION_NAME = "Parsers::checkFASTEntries";
+  size_t use(0);
 
 #if HAVE_FAST
-#define TEST != 1 /* All the configurations must be valid */
+
+  Results::Address* addr;
+
+  if (Results::params[Results::FASTUSE].value == NULL) {
+    cerr << "Warning: " << FUNCTION_NAME << ": " << Results::params[FASTUSE].kwd
+	 << " is missing. As DIET was compiled with FAST, I guess "
+	 << Results::params[Results::FASTUSE].kwd << " = 1.\n";
+    Results::params[Results::FASTUSE].value = new size_t(1);
+  }
+  use = *((size_t*)Results::params[Results::FASTUSE].value);
+
+  if (TRACE_LEVEL >= TRACE_ALL_STEPS)
+    cout << Results::params[FASTUSE].kwd << " = " << use << ".\n";
+
+  if (use > 0) {
+
+    /* Check LDAP entries */
+    if (Results::params[Results::LDAPUSE].value == NULL) {
+      // Display warning for SeDs only, when ldapUse is not set
+      if (Results::params[Results::AGENTTYPE].value != NULL) {
+	cerr << "Warning: " << FUNCTION_NAME << ": "
+	     << Results::params[LDAPUSE].kwd << " is missing."
+	     << " As DIET was compiled with FAST, and "
+	     << Results::params[FASTUSE].kwd << " = 1, I guess "
+	     << Results::params[LDAPUSE].kwd << " = 1.\n";
+	*((size_t*)Results::params[Results::LDAPUSE].value) = 1;
+      } else {
+	// for agents, default is 0
+	*((size_t*)Results::params[Results::LDAPUSE].value) = 0;
+      }
+    }
+    use = *((size_t*)Results::params[Results::LDAPUSE].value);
+    
+    if (TRACE_LEVEL >= TRACE_ALL_STEPS)
+      cout << ' ' << Results::params[LDAPUSE].kwd << " = " << use << endl;
+    
+    if (use > 0) {
+      if (Results::params[Results::LDAPBASE].value == NULL) {
+	cerr << "Error: " << FUNCTION_NAME << ": "
+	     << Results::params[Results::LDAPBASE].kwd << " is missing !\n";
+	return DIET_MISSING_PARAMETERS;
+      }
+      if (TRACE_LEVEL >= TRACE_ALL_STEPS)
+	cout << "  " << Results::params[LDAPBASE].kwd << " = "
+	     << ((Results::Address*)Results::params[LDAPBASE].value)->host
+	     << ':'
+	     << ((Results::Address*)Results::params[LDAPBASE].value)->port
+	     << endl; 
+
+      if (Results::params[Results::LDAPMASK].value == NULL) {
+	cerr << "Error: " << FUNCTION_NAME << ": "
+	     << Results::params[Results::LDAPMASK].kwd << " is missing !\n";
+	return DIET_MISSING_PARAMETERS;
+      }
+      if (TRACE_LEVEL >= TRACE_ALL_STEPS)
+	cout << "  " << Results::params[LDAPBASE].kwd << " = "
+	     << (char*)Results::params[Results::LDAPMASK].value << endl;
+      
+    }
+
+    /* Check NWS entries */
+    if (Results::params[Results::NWSUSE].value == NULL) {
+      cerr << "Warning: " << FUNCTION_NAME << ": "
+	   << Results::params[NWSUSE].kwd << " is missing."
+	   << " As DIET was compiled with FAST, and "
+	   << Results::params[FASTUSE].kwd << " = 1, I guess "
+	   << Results::params[NWSUSE].kwd << " = 1.\n";
+      *((size_t*)Results::params[Results::NWSUSE].value) = 1;
+    }
+    use = *((size_t*)Results::params[Results::NWSUSE].value);
+
+    if (TRACE_LEVEL >= TRACE_ALL_STEPS)
+      cout << ' ' << Results::params[LDAPUSE].kwd << " = " << use << endl;
+
+    if (use > 0) {
+      if (Results::params[Results::NWSNAMESERVER].value == NULL) {
+	cerr << "Error: " << FUNCTION_NAME << ": "
+	     << Results::params[Results::NWSNAMESERVER].kwd
+	     << " is missing !\n";
+	return DIET_MISSING_PARAMETERS;
+      }
+      if (TRACE_LEVEL >= TRACE_ALL_STEPS)
+	cout << "  " << Results::params[NWSNAMESERVER].kwd << " = "
+	     << ((Results::Address*)Results::params[NWSNAMESERVER].value)->host
+	     << ':'
+	     << ((Results::Address*)Results::params[NWSNAMESERVER].value)->port
+	     << endl;
+      if (Results::params[Results::NWSFORECASTER].value == NULL) {
+	cerr << "Error: " << FUNCTION_NAME << ": "
+	     << Results::params[Results::NWSFORECASTER].kwd
+	     << " is missing !\n";
+	return DIET_MISSING_PARAMETERS;
+      }
+      if (TRACE_LEVEL >= TRACE_ALL_STEPS)
+	cout << "  " << Results::params[NWSFORECASTER].kwd << " = "
+	     << ((Results::Address*)Results::params[NWSFORECASTER].value)->host
+	     << ':'
+	     << ((Results::Address*)Results::params[NWSFORECASTER].value)->port
+	     << endl; 
+    }
+  }
+
 #else  // HAVE_FAST
-#define TEST && 0 /* Read each lines and ignore the result */
+
+  if (Results::params[Results::FASTUSE].value != NULL)
+    use = *((size_t*)Results::params[Results::FASTUSE].value);
+  if (use > 0) {
+    cerr << "Warning: " << FUNCTION_NAME
+	 << ": fastUse is set to 1 at line " << Parsers::noLine
+	 << " but DIET was compiled without FAST - ignored.\n";
+    *((size_t*)Results::params[Results::FASTUSE].value) = 0;
+  }
+
 #endif // HAVE_FAST
-
-  if (fscanf(file, "LDAP_USE = %d ", ldapUse) TEST) {
-    cerr << "Error: Parsers::parseFASTEntries: failed to read LDAP_USE.\n";
-    return 1;
-  }
-  if (fscanf(file, "LDAP_HOST = %s ", ldapHost) TEST && *ldapUse) {
-    cerr << "Error: Parsers::parseFASTEntries: failed to read LDAP_HOST.\n";
-    return 1;
-  }
-  if (fscanf(file, "LDAP_PORT = %d ", ldapPort) TEST && *ldapUse) {
-    cerr << "Error: Parsers::parseFASTEntries: failed to read LDAP_PORT.\n";
-    return 1;
-  }
-  if (fscanf(file, "LDAP_MASK = %s ", ldapMask) TEST && *ldapUse) {
-    cerr << "Error: Parsers::parseFASTEntries: failed to read LDAP_MASK.\n";
-    return 1;
-  }
-
-  /* Parse NWS config lines */
-
-  *nwsUse    = 0;
-  *nwsNSHost = '\0';
-  *nwsNSPort = 0;
-  *nwsForecasterHost = '\0';
-  *nwsForecasterPort = 0;
-  
-  if (fscanf(file, "NWS_USE = %d ", nwsUse) TEST) {
-    cerr << "Error: Parsers::parseFASTEntries: failed to read NWS_USE.\n";
-    return 1;
-  }
-  if (fscanf(file, "NWS_NAMESERVER_HOST = %s ", nwsNSHost) TEST && *nwsUse) {
-    cerr << "Error: Parsers::parseFASTEntries: "
-	 << "failed to read NWS_NAMESERVER_HOST.\n";
-    return 1;
-  }
-  if (fscanf(file, "NWS_NAMESERVER_PORT = %d ", nwsNSPort) TEST && *nwsUse) {
-    cerr << "Error: Parsers::parseFASTEntries: "
-	 << "failed to read NWS_NAMESERVER_PORT.\n";
-    return 1;
-  }
-  if (fscanf(file, "NWS_FORECASTER_HOST = %s ", nwsForecasterHost) TEST
-      && *nwsUse) {
-    cerr << "Error: Parsers::parseFASTEntries: "
-	 << "failed to read NWS_FORECASTER_HOST.\n";
-    return 1;
-  }
-  if (fscanf(file, "NWS_FORECASTER_PORT = %d ", nwsForecasterPort) TEST
-      && *nwsUse) {
-    cerr << "Error: Parsers::parseFASTEntries: "
-	 << "failed to read NWS_FORECASTER_PORT.\n";
-    return 1;
-  }
-  
-#undef TEST
 
   return 0;
 }
 
+/**
+ * Check for presence of compulsory params, and call checkFASTEntries.
+ */
 int
-Parsers::scanLine256(char* line)
+Parsers::checkValidity(bool checkFASTEntries, size_t nbCompulsoryParams,
+		       Results::param_type_t* compulsoryParams)
 {
-  int res = fscanf(file, "%256s", line);
-  if (res != 1)
-    res = 2;
-  else if (res == EOF)
-    res = 1;
-  else
-    res = 0;
-  return res;
+  static char* FUNCTION_NAME = "Parsers::checkValidity";
+
+  if (checkFASTEntries)
+    Parsers::checkFASTEntries();
+  
+  if (nbCompulsoryParams == 0)
+    return 0;
+  assert(compulsoryParams != NULL);
+  
+  bool someAreMissing(false);
+
+  for (size_t i = 0; i < nbCompulsoryParams; i++) {
+    Results::param_type_t type = compulsoryParams[i];
+    if (Results::params[type].noLine == 0) {
+      someAreMissing = true;
+      break;
+    }
+  }
+  if (someAreMissing) {
+    cerr << "Error: " << FUNCTION_NAME
+	 << ": some compulsory parameters are missing:\n";
+    for (size_t i = 0; i < nbCompulsoryParams - 1; i++) {
+      Results::param_type_t type = compulsoryParams[i];
+      if (Results::params[type].noLine == 0)
+	cerr << " " << Results::params[type].kwd;
+    }
+    cerr << endl;
+    return DIET_MISSING_PARAMETERS;
+  }
+  return 0;
 }
 
+/**
+ * Parse a valid line (not empty, not commented), and call the parser
+ * associated to the keyword found in first place.
+ */
+int
+Parsers::parseCfgLine(char* line)
+{
+  static char* FUNCTION_NAME = "Parsers::parseCfgLine";
+  char* ptr = line;
+  int parse_res = DIET_PARSE_ERROR;
+
+  // Set ptr to the character after the '='
+  ptr = strchr(line, '=');
+  if (ptr == NULL) {
+    cerr << "Error: " << FUNCTION_NAME << ": no '=' at line "
+	 << Parsers::noLine << ".\n";
+    return DIET_PARSE_ERROR;
+  }
+  ptr++;
+  // Call the parser associated to the first keyword matched.
+  for (size_t i = Results::TRACELEVEL; i < Results::NB_PARAM_TYPE; i++) {
+    if (!strncmp(line, Results::params[i].kwd, Results::params[i].len)) {
+      parse_res =
+	(*Results::params[i].parser)(ptr, (Results::param_type_t)i);
+      break;
+    }
+  }
+
+  if (parse_res) {
+    cerr << "Error: " << FUNCTION_NAME << ": syntax error at "
+	 << Parsers::path << ':' << Parsers::noLine << ".\n";
+  }
+  return parse_res;
+}
+
+/**
+ * Parse a <host:port> address and fill in Results::params with an Address.
+ */
+int
+Parsers::parseAddress(char* address, Results::param_type_t type)
+{
+  static char* FUNCTION_NAME = "Parsers::parseAddress";
+  static char buf[257];
+  size_t port;
+  char* ptr;
+  CHECK_PARAM(type);
+
+  ptr = strchr(address, ':');
+  if ((ptr == address) || (ptr == NULL)) {
+    cerr << "Error: " << FUNCTION_NAME << ": "
+	 << Results::params[type].kwd << " should be <host:port>.\n";
+    return DIET_PARSE_ERROR;
+  }
+  *ptr = '\0';
+  if (sscanf(address, "%256s", (char*)buf) != 1) {
+    cerr << "Error: " << FUNCTION_NAME << ": "
+	 << Results::params[type].kwd << " should be <host:port>.\n";
+    return DIET_PARSE_ERROR;
+  }
+  *ptr = ':';
+  if (sscanf(ptr + 1, "%u ", &port) != 1) {
+    cerr << "Error: " << FUNCTION_NAME << ": "
+	 << Results::params[type].kwd << " should be <host:port>.\n";
+    return DIET_PARSE_ERROR;
+  }
+  Results::params[type].noLine = Parsers::noLine;
+  Results::params[type].value = new Results::Address((char*)buf, port);
+  return 0;
+}
+
+/**
+ * Parse an agent type.<br>
+ * NB: parameter \c type is useless since it is called only for AGENTTYPE.
+ */
+int
+Parsers::parseAgentType(char* agtType_str, Results::param_type_t type)
+{
+   static char* FUNCTION_NAME = "Parsers::parseAgentType";
+   static char buf[257];
+   // dummy initialization to avoid warning at compile time.
+   Parsers::Results::agent_type_t agtType = Results::DIET_LOCAL_AGENT;
+   int res(0);
+
+   CHECK_PARAM(type);
+   if (sscanf(agtType_str,"%256s", (char*)buf) != 1)
+     res = DIET_PARSE_ERROR;
+   else if ((!strncmp(buf, "LA", 2)) ||
+	    (!strncmp(buf, "DIET_LOCAL_AGENT", 16)))
+     agtType = Results::DIET_LOCAL_AGENT;
+   else if ((!strncmp(buf, "MA", 2)) ||
+	    (!strncmp(buf, "DIET_MASTER_AGENT", 17)))
+     agtType = Results::DIET_MASTER_AGENT;
+   else
+     res = DIET_PARSE_ERROR;
+   if (res)
+    cerr << "Error: " << FUNCTION_NAME << ": " << Results::params[type].kwd
+	 << " should be DIET_LOCAL_AGENT or DIET_MASTER_AGENT.\n";
+   else {
+     Results::params[type].noLine = Parsers::noLine;
+     Results::params[type].value =
+       new Parsers::Results::agent_type_t(agtType);
+   }
+   return res;
+}
+
+/**
+ * Parse the name of a DIET entity (mine or my parent's name).
+ */
+int
+Parsers::parseName(char* name, Results::param_type_t type)
+{
+  static char* FUNCTION_NAME = "Parsers::parseName";
+  static char buf[257];
+
+  CHECK_PARAM(type);
+  if (sscanf(name, "%256s ", buf) != 1) {
+    return DIET_PARSE_ERROR;
+  }
+  Results::params[type].noLine = Parsers::noLine;
+  Results::params[type].value = strdup(buf);
+  return 0;
+}
+
+/**
+ * Parse a port entry (for endPoint, ldapPort, etc.)
+ */
+int
+Parsers::parsePort(char* port_str, Results::param_type_t type)
+{
+  static char* FUNCTION_NAME = "Parsers::parsePort";
+  size_t port;
+
+  CHECK_PARAM(type);
+  if (sscanf(port_str, "%u ", &port) != 1) {
+    return DIET_PARSE_ERROR;
+  }
+  Results::params[type].noLine = Parsers::noLine;
+  Results::params[type].value = new size_t(port);
+  return 0;
+}
+
+/**
+ * Parse an unsigned int and set global variable TRACE_LEVEL.
+ * If the integer was wrong, TRACE_LEVEL is set to TRACE_DEFAULT,
+ * and a warning is printed.
+ */
+int
+Parsers::parseTraceLevel(char* traceLevel, Results::param_type_t type)
+{
+  static char* FUNCTION_NAME = "Parsers::parseTraceLevel";
+
+  CHECK_PARAM(type);
+  if (sscanf(traceLevel, "%u ", &TRACE_LEVEL) != 1) {
+    cerr << "Warning: " << FUNCTION_NAME << ": could not read traceLevel "
+	 << "(set to " << TRACE_DEFAULT << ")\n";
+    TRACE_LEVEL = TRACE_DEFAULT;
+  } else { // TRACE_LEVEL is set
+    Results::params[type].noLine = Parsers::noLine;
+  }
+  return 0;
+}
+
+/**
+ * Parse a use (for fastUse, ldapUse, etc.): 0 or 1.
+ */
+int
+Parsers::parseUse(char* use_str, Results::param_type_t type)
+{
+  static char* FUNCTION_NAME = "Parsers::parseUse";
+  size_t use;
+
+  CHECK_PARAM(type);
+  if ((sscanf(use_str, "%u ", &use) != 1) || (use != 0 && use != 1)) {
+    cerr << "Error: " << FUNCTION_NAME << ": "
+	 << Results::params[type].kwd  << " must be 0 or 1.\n";
+    return DIET_PARSE_ERROR;
+  }
+  Results::params[type].noLine = Parsers::noLine;
+  Results::params[type].value = new size_t(use);
+  return 0;
+}
--- src/utils/Parsers.hh
+++ src/utils/Parsers.hh
@@ -8,9 +8,11 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
+ * Revision 1.2  2003/05/10 08:49:33  pcombes
+ * New Parsers for new configuration files.
+ *
  * Revision 1.1  2003/04/10 12:51:22  pcombes
  * Static class for configuration files parsing.
- *
  ****************************************************************************/
 
 #ifndef _PARSERS_HH_
@@ -19,55 +21,189 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
+
 /**
- * Tool to simplify file parsing in DIET.
+ * Tool which aims at make file parsing simpler in DIET.
  */
 
 class Parsers
-{
-  
+{  
 public:
+
+  /**
+   * The structure filled in by Parsers.
+   */
+  friend class Results
+  {
+  public:
+    friend class Parsers;
+    
+    /** The configuration parameter type. */
+    typedef enum {
+      TRACELEVEL = 0,
+      MANAME,
+      AGENTTYPE,
+      ENDPOINT,
+      NAME,
+      PARENTNAME,
+      FASTUSE,
+      LDAPUSE, LDAPBASE, LDAPMASK,
+      NWSUSE, NWSNAMESERVER, NWSFORECASTER,
+      NB_PARAM_TYPE
+    } param_type_t;
+
+    /* Some of these parameters are complex types ... */
+    
+    /** The agent type: MA or LA. */
+    typedef enum {DIET_LOCAL_AGENT, DIET_MASTER_AGENT} agent_type_t;
+
+    /** A <host:port> address. */
+    class Address {
+    public:
+      char*  host;
+      size_t port;
+      inline
+      Address(char* host, size_t port) {
+	this->host = strdup(host);
+	this->port = port;
+      }
+      ~Address() {
+	if (this->host)
+	  free(this->host);
+      }
+    };
+
+    /**
+     * Return a pointer to the value parsed for the parameter of type \c type.
+     */
+    static inline void*
+    getParamValue(param_type_t param_type) {
+      return Results::params[param_type].value;
+    };
+    
+    
+  protected:
+
+    /** Type of all private parsers */
+    typedef int (*parser_t)(char*, Results::param_type_t);
+
+    /** Basic element of the parameters structure */
+    typedef struct {
+      /** keyword */
+      char*  kwd;
+      /** length of the keyword */
+      size_t len;
+      /** parser */
+      parser_t parser;
+      /** line where it is set */
+      size_t noLine;
+      /** pointer to the value of the parameter */
+      void* value;
+    } param_t;
+    /** All keywords and their lengths */
+    static param_t params[];
+  };
+  
+  
+  /**
+   * Prepare the parsing of the file \c filePath (open the file and initialize
+   * the parameter structures)
+   */
   static int
   beginParsing(char* filePath);
   
+  /**
+   * Free the parameters structures: all calls to Results::getParamValue must
+   * have been done before call to this method.
+   */
   static int
   endParsing();
 
   /**
-   * Parse the name of a DIET entity.
-   * name must point at an array of 257 characters.
+   * Parse the file \c filePath and fill in the parameters structure. 
+   * It is possible to specify a list of compulsory parameters, so that the
+   * parsers can check itself for presence of these parameters.
+   * FAST parameters are processed apart, since they depend on each other.
+   * @param checkFASTEntries   tells if FAST entries must be checked.
+   * @param nbCompulsoryParams is the length of the list \c compulsoryParams
+   * @param compulsoryParams   is the list of compulsory parameters
    */
   static int
-  parseName(char* name);
+  parseCfgFile(bool checkFASTEntries, size_t nbCompulsoryParams,
+	       Results::param_type_t* compulsoryParams);
+
+
+private:
+
+  /** Path of the file to parse */
+  static char* path;
+  /** Stream of the file to parse */
+  static ifstream file;
+  /** Current line number */
+  static size_t noLine;
 
   /**
-   * Parse the 'traceLevel = ' config line.
-   * If the line is not matched, traceLevel is set to TRACE_DEFAULT,
+   * Check the coherence of the FAST parameters, essentially depending on the
+   * various "Use" parameters entered.
+   */
+  static int
+  checkFASTEntries();
+
+  /**
+   * Check for presence of compulsory params, and call checkFASTEntries.
+   */
+  static int
+  checkValidity(bool checkFASTEntries, size_t nbCompulsoryParams,
+		Results::param_type_t* compulsoryParams);
+  
+  /**
+   * Parse a valid line (not empty, not commented), and call the parser
+   * associated to the keyword found in first place.
+   */
+  static int
+  parseCfgLine(char* line);
+
+  /**
+   * Parse a <host:port> address and fill in Results::params with an Address.
+   */
+  static int
+  parseAddress(char* address, Results::param_type_t type);
+
+  /**
+   * Parse an agent type.
+   * NB: parameter \c type is useless since it is called only for AGENTTYPE.
+   */
+  static int
+  parseAgentType(char* agtType_str, Results::param_type_t type);
+
+  /**
+   * Parse the name of a DIET entity (mine or my parent's name).
+   */
+  static int
+  parseName(char* name, Results::param_type_t type);
+
+  /**
+   * Parse a port entry (for endPoint, ldapPort, etc.)
+   */
+  static int
+  parsePort(char* port_str, Results::param_type_t type);
+
+  /**
+   * Parse an unsigned int and set global variable TRACE_LEVEL.
+   * If the integer was wrong, TRACE_LEVEL is set to TRACE_DEFAULT,
    * and a warning is printed.
    */
   static int
-  parseTraceLevel();//(unsigned int* traceLevel);
+  parseTraceLevel(char* traceLevel,
+		  Results::param_type_t type = Results::TRACELEVEL);
 
   /**
-   * Parse all FAST entries for a SeD or an agent.
-   * ldapHost, ldapMask, nwsNSHost ans nwsForecasterHost must point at an array
-   * of 257 characters.
+   * Parse a use (for fastUse, ldapUse, etc.): 0 or 1.
    */
   static int
-  parseFASTEntries(int*  ldapUse, char* ldapHost,  int* ldapPort,
-		   char* ldapMask,
-		   int*  nwsUse,  char* nwsNSHost, int* nwsNSPort,
-		   char* nwsForecasterHost, int* nwsForecasterPort);
+  parseUse(char* use_str, Results::param_type_t type);
 
-  /**
-   * Parse a 256-char line
-   * @return 0 if a line has been read, 1 if EOF, and 2 when errors occur.
-   */
-  static int
-  scanLine256(char* line);
-
-private:
-  static FILE* file;
 
 };
 
