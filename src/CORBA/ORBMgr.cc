24128cf44ff73fe1d40e178168964dbf2eb48f7e
Philippe Combes <Philippe.Combes@enseeiht.fr>
2003-05-13 17:20:06 +0000

    Catch all CORBA exceptions in registers and requests to name server.

--- src/CORBA/ORBMgr.cc
+++ src/CORBA/ORBMgr.cc
@@ -9,6 +9,9 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
+ * Revision 1.4  2003/05/13 17:20:06  pcombes
+ * Catch all CORBA exceptions in registers and requests to name server.
+ *
  * Revision 1.3  2003/05/05 14:10:55  pcombes
  * Add destroy and stringToObject methods.
  *
@@ -111,14 +114,19 @@ ORBMgr::getAgentReference(const char* agentName)
     // Narrow the object returned by resolve_initial_references()
     // to a CosNaming::NamingContext object:
     rootContext = CosNaming::NamingContext::_narrow(initServ);
-    if (CORBA::is_nil(rootContext)) 
-      {
-        cerr << "Failed to narrow naming context." << endl;
-        return CORBA::Object::_nil();
-      }
-  }
-  catch(CORBA::ORB::InvalidName& ex) {
+    if (CORBA::is_nil(rootContext)) {
+      cerr << "Failed to narrow naming context." << endl;
+      return CORBA::Object::_nil();
+    }
+  } catch(CORBA::ORB::InvalidName& ex) {
     cerr << "Service required is invalid [does not exist]." << endl;
+    return CORBA::Object::_nil();
+  } catch (CORBA::Exception& e) {
+    CORBA::Any tmp;
+    tmp <<= e;
+    CORBA::TypeCode_var tc = tmp.type();
+    cerr << "Caught exception " << tc->name()
+	 << " while attempting to connect to the CORBA name server.\n";
     return CORBA::Object::_nil();
   }
 
@@ -140,23 +148,18 @@ ORBMgr::getAgentReference(const char* agentName)
     // Resolve the name to an object reference, and assign the reference 
     // returned to a CORBA::Object:
     obj = rootContext->resolve(name);
-  }
-  catch(CosNaming::NamingContext::NotFound& ex)
-    {
-      // This exception is thrown if any of the components of the
-      // path [contexts or the object] aren't found:
-      cerr << "Context not found." << endl;
-      return CORBA::Object::_nil();
-    }
-  catch (CORBA::COMM_FAILURE& ex) {
+  } catch(CosNaming::NamingContext::NotFound& ex) {
+    // This exception is thrown if any of the components of the
+    // path [contexts or the object] aren't found:
+    cerr << "Context not found." << endl;
+    return CORBA::Object::_nil();
+  } catch (CORBA::COMM_FAILURE& ex) {
     cerr << "Caught system exception COMM_FAILURE, unable to contact the "
          << "naming service." << endl;
     return CORBA::Object::_nil();
-  }
-  catch(omniORB::fatalException& ex) {
+  } catch(omniORB::fatalException& ex) {
     throw;
-  }
-  catch (...) {
+  } catch (...) {
     cerr << "Caught a system exception while using the naming service."<< endl;
     return CORBA::Object::_nil();
   }
@@ -182,9 +185,16 @@ ORBMgr::bindAgentToName(CORBA::Object_ptr obj, const char* agentName)
       cerr << "Failed to narrow naming context." << endl;
       return 1;
     }
-  }
-  catch(CORBA::ORB::InvalidName& ex) {
+  } catch(CORBA::ORB::InvalidName& ex) {
     cerr << "Service required is invalid [does not exist]." << endl;
+    return 1;
+  } catch (CORBA::Exception& e) {
+    CORBA::Any tmp;
+    tmp <<= e;
+    CORBA::TypeCode_var tc = tmp.type();
+    cerr << "Caught exception " << tc->name()
+	 << " while attempting to register as " << agentName
+	 << " to the CORBA Name server.\n";
     return 1;
   }
 
