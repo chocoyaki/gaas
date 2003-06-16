944e8d47f04120367cbc8e52efc63d65bec70f9f
Philippe Combes <Philippe.Combes@enseeiht.fr>
2003-06-16 17:49:54 +0000

    Rename autogen.sh into bootstrap.sh, so that completion is faster ... Comment the configure launch at the end (and the warning associated)

---
+++ bootstrap.sh
@@ -0,0 +1,155 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+######
+###### Some defs
+######
+
+PKG_NAME="DIET"
+# a list of all dirs containing possibly some macros
+acmacrosdirs="${HOME}/share/aclocal ${PWD}/acmacros"
+ 
+######
+###### End of conf part
+######
+for dir in $acmacrosdirs ; do
+   if test -d $dir ; then
+   aclocalinclude="$aclocalinclude -I $dir";
+   fi
+done
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+echo "Autoregenerate package \`$PKG_NAME\` in directory \`$srcdir\`";
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "\`$srcdir\`" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+######
+###### Some tests (borrowed from macros/autogen.sh from achtung)
+######
+# Changlog : 
+# 27/5/00: Changed "to compile gnome" to "to compile $PKG_NAME"
+# 27/5/00: removed gettext
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have \`autoconf\` installed to compile $PKG_NAME."
+  echo "Download the appropriate package for your distribution,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+  DIE=1
+}
+
+(grep "PROG_LIBTOOL" $srcdir/configure.ac >/dev/null) && {
+  (libtool --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "**Error**: You must have \`libtool\` installed to compile $PKG_NAME."
+    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.4.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+		NO_LIBTOOLIZE=yes
+  }
+}
+
+(automake-1.7 --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have \`automake\` 1.7 installed to compile $PKG_NAME."
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.7.tar.gz"
+  echo "(or a newer version if it is available)"
+  DIE=1
+  NO_AUTOMAKE=yes
+}
+
+# if no automake, don't bother testing for aclocal
+test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: Missing \`aclocal\`.  The version of \`automake\`"
+  echo "installed doesn't appear recent enough."
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+  echo "(or a newer version if it is available)"
+  DIE=1
+}
+
+if test "$DIE" -eq 1; then
+  exit 1
+fi
+
+###### END of test part
+
+
+######
+###### Action part
+######
+
+
+#if test -z "$*"; then
+#  echo "**Warning**: I am going to run \`configure\` with no arguments."
+#  echo "If you wish to pass any to it, please specify them on the"
+#  echo \`$0\`" command line."
+#  echo
+#fi
+
+case $CC in
+xlc )
+  am_opt=--include-deps;;
+esac
+
+for coin in  $srcdir/configure.ac # `find $srcdir -name configure.ac -print`
+do 
+  dr=`dirname $coin`
+  if test -f $dr/NO-AUTO-GEN; then
+    echo skipping $dr -- flagged as no auto-gen
+  else
+    echo "************"
+    echo "* Processing directory $dr"
+    echo "************"
+    
+    macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
+    ( cd $dr
+      if grep "PROG_LIBTOOL" configure.ac >/dev/null; then
+				if test -z "$NO_LIBTOOLIZE" ; then 
+	  			echo "Running libtoolize --automake --force --copy in "`pwd`"..."
+	  			libtoolize --automake --force --copy
+				fi
+      fi
+      echo "Running aclocal-1.7 $aclocalinclude..."
+      aclocal-1.7 $aclocalinclude 
+      if grep "CONFIG_HEADER" configure.ac >/dev/null; then
+				echo "Running autoheader in "`pwd`"..."
+				autoheader
+      fi
+      echo "Running automake-1.7 --add-missing --copy $am_opt ..."
+      automake-1.7 --add-missing --copy $am_opt
+      echo "Running autoconf..."
+      autoconf
+    )
+  fi
+done
+
+#conf_flags="--enable-maintainer-mode --enable-compile-warnings"
+#--enable-compile-warnings=minimum"
+
+#echo "rebuilding the api documentation... "
+#doxygen && echo "Done with the rebuilding of the api documentation." && cd .. || exit 1
+
+
+#if test x$NOCONFIGURE = x; then
+#  echo "************"
+#  echo "* Running $srcdir/configure $conf_flags $@ ..."
+#  echo "************"
+#
+#  date>$srcdir/autogen-timestamp
+#  $srcdir/configure $conf_flags "$@" || exit 1
+#else
+#  echo Skipping configure process.
+#fi
+
+#echo Now type \`make\` to compile $PKG_NAME
+
+
