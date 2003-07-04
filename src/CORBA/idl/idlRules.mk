c0c665de5a5d6938e500c131c368e271f0a7a2be
Philippe Combes <Philippe.Combes@enseeiht.fr>
2003-07-04 09:47:56 +0000

    Indentation. Remove useless comments.

--- src/CORBA/idl/idlRules.mk
+++ src/CORBA/idl/idlRules.mk
@@ -8,15 +8,14 @@
 #****************************************************************************#
 #* $Id$
 #* $Log$
-#* Revision 1.3  2003/06/02 14:51:54  cpera
-#* Applying coding standards on Callback.idl.
+#* Revision 1.4  2003/07/04 09:47:56  pcombes
+#* Indentation. Remove useless comments.
 #*
 #* Revision 1.2  2003/06/02 08:08:11  cpera
 #* Beta version of asynchronize DIET API.
 #*
 #* Revision 1.1  2003/04/10 12:34:32  pcombes
 #* IDL descriptions are back in their directory.
-#*
 #****************************************************************************#
 
 # IDL source files
@@ -30,15 +29,11 @@ MasterAgentSK.cc MasterAgentDynSK.cc MasterAgent.hh: Agent.hh
 LocalAgentSK.cc LocalAgentDynSK.cc LocalAgent.hh:    Agent.hh
 SeDSK.cc SeDDynSK.cc SeD.hh:                         common_types.hh Callback.hh
 responseSK.cc responseDynSK.cc response.hh:          SeD.hh
-Callback.hh CallbackSK.cc CallbackDynSK.cc : 		common_types.hh	
+Callback.hh CallbackSK.cc CallbackDynSK.cc:          common_types.hh	
 
 
 # compiling and dependency rules for idl files
 %.hh %SK.cc %DynSK.cc: %.idl $(IDL_COMPILER)
-# FIXME: Why not -(rm -f $@) ???
-#	@rm -f $(basename $(notdir $<)).hh   \
-#	       $(basename $(notdir $<))SK.cc \
-#	       $(basename $(notdir $<))DynSK.cc || true > /dev/null
 	$(IDL_COMPILER) $(IDLFLAGS) $<
 # FIXME: The following two lines are a trick for omniORB 4 compiled with
 #        PYTHON 2.3.
