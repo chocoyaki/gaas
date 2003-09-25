681355282db7615d1716695168733b2065eea5f7
Christophe Pera <christophe.pera@univ-lyon1.fr>
2003-09-25 10:06:24 +0000

    Delete messages and useless include.

--- src/client/CallbackImpl.cc
+++ src/client/CallbackImpl.cc
@@ -8,6 +8,9 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
+ * Revision 1.4  2003/09/25 10:06:24  cpera
+ * Delete messages and useless include.
+ *
  * Revision 1.3  2003/09/16 10:26:41  cpera
  * Fix a warning.
  *
@@ -19,10 +22,11 @@
  *
  ****************************************************************************/
 
+using namespace std;
 #include <iostream>
 #include <CallbackImpl.hh>
-
-using namespace std;
+#define TRACE_LEVEL 50
+#include "debug.hh"
 
 CallbackImpl::CallbackImpl()
 {
@@ -40,15 +44,15 @@ long CallbackImpl::ping()
 }
 
 long CallbackImpl::notifyResults(const char * path,
-                                const corba_profile_t& pb,
-				long reqID)
-				{
+    const corba_profile_t& pb,
+    long reqID)
+{
   return 0;
 }
 
 long CallbackImpl::solveResults(const char * path, 
-                                const corba_profile_t& pb,
-                                long reqID)
+    const corba_profile_t& pb,
+    long reqID)
 {
   // notify solve result availability
   return CallAsyncMgr::Instance()->notifyRst(int(reqID), &(const_cast<corba_profile_t&>(pb)));
