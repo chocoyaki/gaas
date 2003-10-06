c1ad90a58897fca654c4953d4966fe3890a09b0d
Cyrille Pontvieux <jrd@enialis.net>
2003-10-06 10:11:55 +0000

    Class to manage a clean exit, call by EXITFUNCTION

---
+++ src/agent/ExitClass.cc
@@ -0,0 +1,53 @@
+/****************************************************************************/
+/* DIET Agent Exit Class implementation                                     */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Cyrille PONTVIEUX (cyrille.pontvieux@ens-lyon.fr)                   */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2003/10/06 10:11:55  cpontvie
+ * Class to manage a clean exit, call by EXITFUNCTION
+ *
+ ****************************************************************************/
+
+using namespace std;
+#include "ExitClass.hh"
+
+#include <iostream>
+#include <stdlib.h>
+
+#include "Parsers.hh"
+
+/** The trace level. */
+extern unsigned int TRACE_LEVEL;
+
+AgentImpl* ExitClass::agent = NULL;
+
+void
+ExitClass::init(AgentImpl* agent)
+{
+  ExitClass::agent = agent;
+}
+
+void
+ExitClass::exitFunction()
+{
+  TRACE_TEXT(TRACE_MAIN_STEPS, "______________________________\n");
+  TRACE_TEXT(TRACE_MAIN_STEPS, "Stopping the Agent...\n");
+  // Deactivate and destroy the agent
+  try {
+    ORBMgr::deactivate();
+    if (ExitClass::agent != NULL) {
+      delete ExitClass::agent;
+    }
+    ORBMgr::destroy();
+  } catch (...) {}
+  TRACE_TEXT(TRACE_MAIN_STEPS, "Agent stopped !\n");
+
+  /* We do not need the parsing results any more */
+  Parsers::endParsing();
+}
+
---
+++ src/agent/ExitClass.hh
@@ -0,0 +1,49 @@
+/****************************************************************************/
+/* DIET Agent Exit Class header                                             */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Cyrille PONTVIEUX (cyrille.pontvieux@ens-lyon.fr)                   */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2003/10/06 10:11:35  cpontvie
+ * Class to manage a clean exit, call by EXITFUNCTION
+ *
+ ****************************************************************************/
+
+/*
+ !! YOU MUST INCLUDE THIS HEADER PRIOR TO THE OTHERS !!
+*/
+
+#ifndef _EXITCLASS_HH_
+#define _EXITCLASS_HH_
+
+#define EXIT_FUNCTION ExitClass::exitFunction()
+
+#include "debug.hh"
+#include "ORBMgr.hh"
+#include "AgentImpl.hh"
+
+class ExitClass
+{
+public:
+  /**
+   * You must initialize the ExitClass with all informations needed by the
+   * ExitFunction
+   */
+  static void
+  init(AgentImpl* agent);
+
+  /**
+   * The ExitFunction called by the INTERNAL_ERROR macro.
+   */
+  static void
+  exitFunction();
+
+private:
+  static AgentImpl* agent;
+};
+
+#endif // _EXITCLASS_HH_
