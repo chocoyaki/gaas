590f3b05d03271eedbaff86f5481542b23001690
Raphael Bolze <raphael.bolze@ens-lyon.fr>
2004-03-01 19:00:44 +0000

    Thread to enable monitoring from LogCentral

---
+++ src/utils/MonitoringThread.cc
@@ -0,0 +1,71 @@
+/****************************************************************************/
+/* MonitoringThread - actively collects monitoring information for          */
+/* DIET, mainly from FAST                                                   */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Georg Hoesch (hoesch@in.tum.de)                                     */
+/*    - Cyrille Pontvieux (cyrille.pontvieux@edu.univ-fcomte.fr)            */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2004/03/01 19:00:37  rbolze
+ * Thread to enable monitoring from LogCentral
+ *
+ ****************************************************************************/
+
+#include "MonitoringThread.hh"
+
+MonitoringThread::MonitoringThread(DietLogComponent* DLC) {
+  dietLogComponent = DLC;
+}
+
+void
+MonitoringThread::startThread() {
+  if (threadRunning) {
+    return;
+  } else {
+    threadRunning = true;
+    start_undetached();
+  }
+}
+
+void
+MonitoringThread::stopThread() {
+  if (!threadRunning) {
+    return;
+  } else {
+    threadRunning = false;
+    join(NULL);
+  }
+}
+
+void*
+MonitoringThread::run_undetached(void* params) {
+  while (threadRunning) {
+    /* This is empty, as I do not have the time to implement and
+     * test it. Whoever is interested in mem, load, latency and
+     * bandwidth from FAST must gather the values here and use the
+     * corresponding dietLogComponent functions to send them.
+     */
+
+    if (dietLogComponent != NULL) {
+#if HAVE_FAST
+      // check if FAST is available
+      // gather data here
+      // dietLogComponent->logMem(mem);
+      // dietLogComponent->logLoad(load);
+      // dietLogComponent->logBandwidth(bw);
+      // dietLogComponent->logLatency(latency);
+#endif
+    }
+
+    sleep(1,0);
+  }
+  return NULL;
+}
+
+MonitoringThread::~MonitoringThread() {
+  // nothing to do
+}
---
+++ src/utils/MonitoringThread.hh
@@ -0,0 +1,78 @@
+/****************************************************************************/
+/* MonitoringThread header - actively collects monitoring information for   */
+/* DIET, mainly from FAST                                                   */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Georg Hoesch (hoesch@in.tum.de)                                     */
+/*    - Cyrille Pontvieux (cyrille.pontvieux@edu.univ-fcomte.fr)            */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2004/03/01 19:00:44  rbolze
+ * Thread to enable monitoring from LogCentral
+ *
+ ****************************************************************************/
+
+#ifndef _MONITORINGTHREAD_HH_
+#define _MONITORINGTHREAD_HH_
+
+#include "omnithread.h"
+#include "DietLogComponent.hh"
+
+/**
+ * This thread actively queries FAST every second for mem, load and
+ * other passive values which cannot be measured directly in the
+ * sorucecode
+ */
+class MonitoringThread: public omni_thread
+{
+public:
+  /**
+   * Constructor.
+   * @param DLC reference to the DietLogComponent of this thread.
+   */
+  MonitoringThread(DietLogComponent* DLC);
+
+  /**
+   * Start the thread.
+   */
+  void
+  startThread();
+
+  /**
+   * Stop the thread.
+   * The object will be deleted by the orb.
+   */
+  void
+  stopThread();
+
+protected:
+  /**
+   * The main loop of the thread.
+   * Logs FAST values with the dietLogComponent
+   */
+  void*
+  run_undetached(void* params);
+
+  /**
+   * Destructor. Is called by the orb when the thread is
+   * stopped. Do not call it yourself.
+   */
+  ~MonitoringThread();
+
+private:
+  /**
+   * This threads dietLogComponent. NULL if no monitoring information
+   * is to be gathered (although you don't need to start this thread then)
+   */
+  DietLogComponent* dietLogComponent;
+
+  /**
+   * controls the main loop
+   */
+  bool threadRunning;
+};
+
+#endif
