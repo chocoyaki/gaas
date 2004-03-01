728ead8ad72a1192da0950cf1d46b0b51f52462b
Raphael Bolze <raphael.bolze@ens-lyon.fr>
2004-03-01 18:56:03 +0000

    make Diet objects as LogComponent for LogCentral DietLogComponent objects can contact LogCentral and transmit log message

---
+++ src/utils/DietLogComponent.cc
@@ -0,0 +1,619 @@
+/****************************************************************************/
+/* DietLogComponent - LogService module for DIET                            */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Georg Hoesch (hoesch@in.tum.de)                                     */
+/*    - Cyrille Pontvieux (cyrille.pontvieux@edu.univ-fcomte.fr)            */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2004/03/01 18:55:56  rbolze
+ * make Diet objects as LogComponent for LogCentral
+ * DietLogComponent objects can contact LogCentral and transmit log message
+ *
+ ****************************************************************************/
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+#include "ORBMgr.hh"
+#include "debug.hh"
+#include "DietLogComponent.hh"
+
+
+
+/**
+ * Error message - exit with exit_value.
+ */
+#define DLC_ERROR(formatted_msg,exit_value)              \
+  cerr << "DIET ERROR: " << formatted_msg << ".\n"   \
+       << "cannot proceed.\n"; \
+  exit(exit_value)
+
+/**
+ * Warning message.
+ */
+#define DLC_WARNING(formatted_msg)                       \
+  cerr << "DIET WARNING: " << formatted_msg << ".\n" \
+       << "DIET can proceed, but you may encounter unexpected behaviour.\n"
+
+/**
+ * FlushBufferThread
+ */
+FlushBufferThread::FlushBufferThread(DietLogComponent* DLC,
+                                     int outBufferTime) {
+  threadRunning = false;
+  myDLC = DLC;
+
+  outBufferTimeNSec = outBufferTime;
+}
+
+FlushBufferThread::~FlushBufferThread() {
+  // nothing to do
+}
+
+void
+FlushBufferThread::startThread() {
+  if (threadRunning) {
+    return;
+  } else {
+    threadRunning = true;
+    start_undetached();
+  }
+}
+
+void
+FlushBufferThread::stopThread()
+{
+  if (!threadRunning) {
+    return;
+  }
+  threadRunning = false;
+  join(NULL);
+}
+
+void*
+FlushBufferThread::run_undetached(void* params)
+{
+  while (threadRunning) {
+    myDLC->sendOutBuffer();
+
+    sleep(0,outBufferTimeNSec);
+  }
+  return NULL;
+}
+
+/**
+ * PingThread
+ */
+
+PingThread::PingThread(DietLogComponent* DLC)
+{
+  threadRunning = false;
+  myDLC = DLC;
+}
+
+PingThread::~PingThread()
+{
+  // cannot be called directly / has nothing to do
+}
+
+void
+PingThread::startThread()
+{
+  if (threadRunning) {
+    return;
+  }
+  threadRunning = true;
+  start_undetached();
+}
+
+void
+PingThread::stopThread()
+{
+  if (!threadRunning) {
+    return;
+  }
+  threadRunning = false;
+  join(NULL);
+}
+
+void*
+PingThread::run_undetached(void* params)
+{
+  int counter = 0;
+
+  while (threadRunning) {
+    myDLC->ping();
+
+    if (counter == PINGTHREAD_SYNCHRO_FREQUENCY) {
+      myDLC->synchronize();
+      counter = 0;
+    }
+    counter++;
+
+    sleep(PINGTHREAD_SLEEP_SEC, PINGTHREAD_SLEEP_NSEC);
+  }
+  return NULL;
+}
+
+/****************************************************************************/
+
+/**
+ * DietLogComponent
+ */
+
+DietLogComponent::DietLogComponent(const char* name,
+        int outBufferMaxSize) {
+  this->name = CORBA::string_dup(name);
+  this->outBufferMaxSize = outBufferMaxSize;
+  isConnected = false;
+
+  pingThread=NULL;
+
+  // define tags
+  tagCount = 13;
+  tagFlags = createBoolArrayFalse(tagCount);
+  tagNames = new (char*)[tagCount];
+  tagNames[0] = strdup("ADD_SERVICE");
+  tagNames[1] = strdup("ASK_FOR_SED");
+  tagNames[2] = strdup("SED_CHOSEN");
+  tagNames[3] = strdup("BEGIN_SOLVE");
+  tagNames[4] = strdup("END_SOLVE");
+  tagNames[5] = strdup("DATA_STORE");
+  tagNames[6] = strdup("DATA_RELEASE");
+  tagNames[7] = strdup("DATA_TRANSFER_BEGIN");
+  tagNames[8] = strdup("DATA_TRANSFER_END");
+  tagNames[9] = strdup("MEM");
+  tagNames[10] = strdup("LOAD");
+  tagNames[11] = strdup("LATENCY");
+  tagNames[12] = strdup("BANDWIDTH");
+
+  CORBA::Object_ptr myLCCptr;
+
+  try {
+    myLCCptr = ORBMgr::getObjReference(ORBMgr::LOGSERVICE, "LogComponent");
+  } catch(CORBA::SystemException &e) {
+    DLC_ERROR("Could not resolve 'LogService./LogComponent.' from the NS",1);
+  }
+  if (CORBA::is_nil(myLCCptr)) {
+    DLC_ERROR("Could not resolve 'LogService./LogComponent.' from the NS",1);
+  }
+
+  try {
+    myLCC = LogCentralComponent::_narrow(myLCCptr);
+  } catch(CORBA::SystemException &e) {
+    DLC_ERROR("Could not narrow the LogCentralComponent",1);
+  }
+}
+
+int DietLogComponent::run(const char* agentType,
+                          const char* parentName,
+                          int outBufferTime) {
+  // Connect myself to the LogCentral
+  short ret=0;
+
+  char* hostName = new char[256];
+  if(gethostname(hostName,255) != 0) {
+    delete hostName;
+    hostName = strdup("unknownHost");
+  }
+  char* msg;
+  if (parentName != NULL) {
+    msg = (char*) malloc(strlen(agentType) + strlen(parentName) + 2);
+    sprintf(msg, "%s %s", agentType, parentName);
+  } else {
+    msg = strdup(agentType);
+  }
+
+  log_time_t time = getLocalTime();
+  tag_list_t currentTagList;
+  try {
+    ret = myLCC->connectComponent(
+      name,
+      hostName,
+      msg,
+      this->_this(),
+      time,
+      currentTagList
+    );
+  } catch (CORBA::SystemException &e) {
+    delete(msg);
+    delete(hostName);
+    cout << "Error: could not connect to the LogCentral" << endl;
+    DLC_ERROR("SystemException",-1);
+  }
+  delete(hostName);
+  delete(msg);
+
+  if (ret != LS_OK) {
+    ERROR("LogCentral refused connection",ret);
+  }
+
+  setTagFilter(currentTagList);
+
+  pingThread = new PingThread(this);
+  pingThread->startThread();
+
+  if (outBufferMaxSize != 0) {
+    flushBufferThread = new FlushBufferThread(this, outBufferTime);
+    flushBufferThread->startThread();
+  }
+
+  isConnected = true;
+  return 0;
+}
+
+DietLogComponent::~DietLogComponent() {
+  // disconnect LogComponent
+
+  if (pingThread != NULL) {
+    pingThread->stopThread();
+  }
+  if (flushBufferThread != NULL) {
+    flushBufferThread->stopThread();
+  }
+
+  if (isConnected) {
+    try {
+      myLCC->disconnectComponent(name, "");
+    } catch (CORBA::SystemException &e) {
+      isConnected = false;
+      DLC_WARNING("Could not disconnect from the LogCentral");
+      // do nothing - we are already terminating
+    }
+  }
+
+  for (int i=0; i<tagCount; i++) {
+    free(tagNames[i]);
+  }
+  delete tagNames;
+  delete tagFlags;
+}
+
+/**
+ * ComponentConfigurator functions
+ */
+
+void 
+DietLogComponent::setTagFilter(const tag_list_t& tagList) {
+  bool* newList = createBoolArrayFalse(tagCount);
+  bool* oldList;
+  int idx;
+
+  // check for '*'
+  if (containsStar(&tagList)) {
+    for (int i=0; i < tagCount; i++) {
+      newList[i]=true;
+    }
+  } else {
+    // change config configuration
+    for (int i=0; i < (int)tagList.length(); i++) {
+      idx = getTagIndex(tagList[i]);
+      if (idx != -1) {
+        newList[idx]=true;
+      }
+      // else: ignore unknown tags
+    }
+  }
+
+  // set new config
+  dlcMutex.lock();
+  oldList = tagFlags;
+  tagFlags = newList;
+  dlcMutex.unlock();
+  delete oldList;
+}
+
+void
+DietLogComponent::addTagFilter(const tag_list_t& tagList) {
+  int idx;
+
+  dlcMutex.lock();
+  // check for '*'
+  if (containsStar(&tagList)) {
+    for (int i=0; i < tagCount; i++) {
+      tagFlags[i]=true;
+    }
+  } else {
+    // create new configuration
+    for (int i=0; i < (int)tagList.length(); i++) {
+      idx = getTagIndex(tagList[i]);
+      if (idx != -1) {
+        tagFlags[idx]=true;
+      }
+      // else: ignore unknown tags
+    }
+  }
+  dlcMutex.unlock();
+}
+
+void
+DietLogComponent::removeTagFilter(const tag_list_t& tagList) {
+  int idx;
+
+  dlcMutex.lock();
+  // check for '*'
+  if (containsStar(&tagList)) {
+    for (int i=0; i < tagCount; i++) {
+      tagFlags[i]=false;
+    }
+  } else {
+    // change configuration
+    for (int i=0; i < (int)tagList.length(); i++) {
+      idx = getTagIndex(tagList[i]);
+      if (idx != -1) {
+        tagFlags[idx]=false;
+      }
+      // else: ignore unknown tags
+    }
+  }
+  dlcMutex.unlock();
+}
+
+/**
+ * Helper functions
+ */
+
+int DietLogComponent::getTagIndex(const char* tag) {
+  for (int i=0; i < tagCount; i++) {
+    if (strcmp(tag, tagNames[i]) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+bool DietLogComponent::containsStar(const tag_list_t* tagList) {
+  for (int i=0; i < (int)tagList->length(); i++) {
+    if (strcmp((*tagList)[i],"*") == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool*
+DietLogComponent::createBoolArrayFalse(int size) {
+  bool* ba;
+  ba = new bool[size];
+  for (int i=0; i<size; i++) {
+    ba[i] = false;
+  }
+  return ba;
+}
+
+log_time_t
+DietLogComponent::getLocalTime() {
+  struct timeval tv;
+  struct timezone tz;
+  gettimeofday(&tv, &tz);
+  log_time_t ret;
+  ret.sec = tv.tv_sec;
+  ret.msec = tv.tv_usec / 1000;
+  return ret;
+}
+
+
+/**
+ * "Synchronised" functions with errorhandling
+ */
+
+void
+DietLogComponent::ping() {
+  if(isConnected) {
+    try {
+      myLCC->ping(name);
+    } catch (CORBA::SystemException &e) {
+      // looks like we were disconnected
+      handleDisconnect(e);
+    }
+  }
+}
+
+void
+DietLogComponent::synchronize() {
+  log_time_t time = getLocalTime();
+  if(isConnected) {
+    try {
+      myLCC->synchronize(name, time);
+    } catch (CORBA::SystemException &e) {
+      // looks like we were disconnected
+      handleDisconnect(e);
+    }
+  }
+}
+
+void DietLogComponent::sendOutBuffer() {
+  if (isConnected) {
+    dlcMutex.lock();
+
+    if (outBuffer.length() > 0) {
+      try {
+        myLCC->sendBuffer(outBuffer);
+      } catch (CORBA::SystemException &e) {
+        // looks like we were disconnected
+        handleDisconnect(e);
+      }
+      outBuffer.length(0);
+    }
+    dlcMutex.unlock();
+  }
+}
+
+void
+DietLogComponent::log(const char* tag, const char* msg) {
+  log_msg_t logMsg;
+
+  if (isConnected) {
+    logMsg.componentName = CORBA::string_dup(name);
+    logMsg.time = getLocalTime();
+    logMsg.tag = CORBA::string_dup(tag);
+    logMsg.msg = CORBA::string_dup(msg);
+
+    dlcMutex.lock();
+    CORBA::ULong bufsize;
+    bufsize = outBuffer.length();
+    bufsize++;
+    outBuffer.length(bufsize);
+    outBuffer[bufsize-1] = logMsg;
+    dlcMutex.unlock();
+
+    if ((int)bufsize > outBufferMaxSize) {
+      sendOutBuffer();
+    }
+  }
+}
+
+void
+DietLogComponent::handleDisconnect(CORBA::SystemException &e) {
+  DLC_WARNING("Connection to LogCentral failed");
+#ifdef DLC_ERROR_BEHAVIOUR_FATAL
+  DLC_ERROR("Cannot proceed without LogCentral connection\n",1);
+#else
+  DLC_WARNING("LogComponent module stopped - agent will continue without LogCentral");
+  isConnected = false;
+#endif
+}
+
+/**
+ * Diet specific wrappers for the functions above
+ */
+
+/**
+ * pronounce new service
+ */
+
+void
+DietLogComponent::logAddService(const
+				corba_profile_desc_t* serviceDescription) {
+  if (tagFlags[0]) {
+    // FIXME: add service profile ?
+    log(tagNames[0], serviceDescription->path);
+  }
+}
+
+/**
+ * Find SeD and solve problem
+ */
+
+void
+DietLogComponent::logAskForSeD(const corba_request_t* request) {
+  if (tagFlags[1]) {
+    // FIXME: add request parameters (size of request arguments?)
+    log(tagNames[1], request->pb.path);
+  }
+}
+
+void
+DietLogComponent::logSedChosen(const corba_request_t* request,
+			       const corba_response_t* resonse) {
+  if (tagFlags[2]) {
+    // FIXME: add list/outline of chosen servers ?
+    log(tagNames[2], request->pb.path);
+  }
+}
+
+void
+DietLogComponent::logBeginSolve(const char* path,
+				const corba_profile_t* problem) {
+  if (tagFlags[3]) {
+    // FIXME: print problem (argument size?)
+    log(tagNames[3], path);
+  }
+}
+
+void
+DietLogComponent::logEndSolve(const char* path,
+				const corba_profile_t* problem) {
+  if (tagFlags[4]) {
+    log(tagNames[4], path);
+  }
+}
+
+/**
+ * Data transfer functions
+ */
+
+void
+DietLogComponent::logDataRelease(const char* dataID) {
+  if (tagFlags[5]) {
+    log(tagNames[5], dataID);
+  }
+}
+
+void
+DietLogComponent::logDataStore(const char* dataID) {
+  if (tagFlags[6]) {
+    log(tagNames[6], dataID);
+  }
+}
+
+void
+DietLogComponent::logDataBeginTransfer(const char* dataID,
+		     const char* destAgent) {
+  if (tagFlags[7]) {
+    char* s;
+    s = new char[strlen(dataID)+strlen(destAgent)+2];
+    sprintf(s,"%s %s",dataID, destAgent);
+    log(tagNames[7], s);
+    delete(s);
+  }
+}
+
+void
+DietLogComponent::logDataEndTransfer(const char* dataID,
+		     const char* destAgent) {
+  if (tagFlags[8]) {
+    char* s;
+    s = new char[strlen(dataID)+strlen(destAgent)+2];
+    sprintf(s,"%s %s",dataID, destAgent);
+    log(tagNames[8], s);
+    delete(s);
+  }
+}
+
+/**
+ * NWS data
+ */
+void
+DietLogComponent::logMem(double mem) {
+  if (tagFlags[9]) {
+    char buf[20];
+    sprintf(buf,"%f",mem);
+    log(tagNames[9], buf);
+  }
+}
+
+void
+DietLogComponent::logLoad(double load) {
+  if (tagFlags[10]) {
+    char buf[20];
+    sprintf(buf,"%f",load);
+    log(tagNames[10], buf);
+  }
+}
+
+void
+DietLogComponent::logLatency(double latency) {
+  if (tagFlags[11]) {
+    char buf[20];
+    sprintf(buf,"%f",latency);
+    log(tagNames[11], buf);
+  }
+}
+
+void
+DietLogComponent::logBandwidth(double bandwidth) {
+  if (tagFlags[12]) {
+    char buf[20];
+    sprintf(buf,"%f",bandwidth);
+    log(tagNames[12], buf);
+  }
+}
---
+++ src/utils/DietLogComponent.hh
@@ -0,0 +1,386 @@
+/****************************************************************************/
+/* DietLogComponent header - LogService connection for DIET                 */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Georg Hoesch (hoesch@in.tum.de)                                     */
+/*    - Cyrille Pontvieux (cyrille.pontvieux@edu.univ-fcomte.fr)            */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2004/03/01 18:56:03  rbolze
+ * make Diet objects as LogComponent for LogCentral
+ * DietLogComponent objects can contact LogCentral and transmit log message
+ *
+ ****************************************************************************/
+
+#ifndef _DIETLOGCOMPONENT_HH_
+#define _DIETLOGCOMPONENT_HH_
+
+#include "omnithread.h"
+#include "ORBMgr.hh"
+#include "LogComponent.hh"
+#include "common_types.hh"
+#include "response.hh"
+
+#define PINGTHREAD_SYNCHRO_FREQUENCY      60
+#define PINGTHREAD_SLEEP_SEC              1
+#define PINGTHREAD_SLEEP_NSEC             0
+
+/**
+ * This flag configures the behaviour when communication
+ * errors occure during operation. If it is defined, the
+ * component will stop the agent/SeD. If it is undefined,
+ * the agent will print a warning and continue without
+ * sending logmessages.
+ */
+//#define DLC_ERROR_BEHAVIOUR_FATAL
+
+// forward declaraction
+class DietLogComponent;
+
+/**
+ * The FlushBufferThread is an internal thread of the
+ * DietLogComponent(DLC). It flushes the DLC in regular
+ * intervals by calling its sendOutBuffer() function.
+ */
+class FlushBufferThread: public omni_thread
+{
+public:
+  /**
+   * Constructor.
+   * @param DLC a pointer to the DietLogComponent to flush.
+   * @param outBufferTime this value that indicates the maximum
+   * for the messages in the outBuffer. The thread sleeps outBufferTime
+   * nanoseconds between two flush operations.
+   */
+  FlushBufferThread(DietLogComponent* DLC, int outBufferTime);
+
+  /**
+   * Start the thread.
+   */
+  void
+  startThread();
+
+  /**
+   * Stop the thread.
+   * The object will be deleted by the orb.
+   */
+  void
+  stopThread();
+
+protected:
+  /**
+   * The main loop of the thread.
+   * It just sleeps and calls dlc->sendOutBuffer().
+   */
+  void*
+  run_undetached(void* params);
+
+  /**
+   * Destructor. Is called by the orb when the thread is
+   * stopped. Do not call it yourself.
+   */
+  ~FlushBufferThread();
+
+private:
+  /**
+   * Reference to the DLC to flush.
+   */
+  DietLogComponent* myDLC;
+
+  /**
+   * controls the main loop
+   */
+  bool threadRunning;
+
+  /**
+   * time the thread should sleep between two flushes in nanoseconds.
+   */
+  int outBufferTimeNSec;
+};
+
+/**
+ * The PingThread regularly calls functions in the 
+ * DietLogComponent(DLC) to synchronize with the
+ * LogCentral. If this thread is not active, the
+ * LogCentral will disconnect the component after
+ * some time.
+ */
+class PingThread: public omni_thread
+{
+public:
+  /**
+   * Constructor.
+   * @param DLC reference to the DietLogComponent of this thread.
+   */
+  PingThread(DietLogComponent* DLC);
+
+  /**
+   * Start the thread.
+   */
+  void
+  startThread();
+
+  /**
+   * Stop the thread.
+   * The object will be deleted by the orb.
+   */
+  void
+  stopThread();
+
+protected:
+  /**
+   * The main loop of the thread.
+   * It just sleeps and calls ping and/or synchronize of the DLC
+   */
+  void*
+  run_undetached(void* params);
+
+  /**
+   * Destructor. Is called by the orb when the thread is
+   * stopped. Do not call it yourself.
+   */
+  ~PingThread();
+
+private:
+  /**
+   * Reference to this threads DLC.
+   */
+  DietLogComponent* myDLC;
+
+  /**
+   * controls the main loop
+   */
+  bool threadRunning;
+};
+
+/**
+ * The DietLogComponent encapsulates all LogCentral communication
+ * in a DIET specific class. It acts as servant and as local proxy
+ * for the LogCentral at the same time. It offers a number of 
+ * functions for the creation of all possible logmessages. It also
+ * handles errors with the DIET internal mechanisms.
+ *
+ * The implementation of the ComponentConfigurator is done internally
+ * with the dlcMutex, the tagNames[] and the tagFlags[]. The arrays
+ * contain the names of the tags this component offers and their
+ * actual state. The dlcMutex synchronises the access to these arrays
+ * and the outBuffer. To query their status, logXXX functions just
+ * check for the flag with the index that corresponds to their tag.
+ */
+class DietLogComponent:  public POA_ComponentConfigurator,
+        public PortableServer::RefCountServantBase
+{
+public:
+
+  /**
+   * Initialise the DietLogComponent.
+   * @param name The name of this component. Set to "" if this is a SeD.
+   * @param outBufferMaxSize the size of the outBuffer. If the size is 0,
+   * messages will not be cached but sent immediately.
+   */
+  DietLogComponent(const char* name,
+        int outBufferMaxSize);
+
+  /**
+   * Release the memory allocated by this class;
+   */
+  ~DietLogComponent();
+
+  /**
+   * Connect to LogCentral to enable logging. Must be called
+   * after activation.
+   * @param parentName The name of this agent's parent. Pass
+   * NULL or "" if this is a Master agent.
+   * @param outBufferTime The maximum time messages stay in the
+   * outBuffer. After this time, the FlushBufferThread sends them.
+   * The value specified nanoseconds.
+   */
+  int run(const char* agentType,
+          const char* parentName,
+          int outBufferTime);
+
+  /**
+   * Client functions (for DIET)
+   * All these functions
+   * - check if the msg must be sent
+   * - deal with any errors internally
+   * So just envoke these functions, the DLC does the rest.
+   */
+
+  /**
+   * Notifies the change of services on an agent or SeD.
+   * This can only declare one service at a time, so each service
+   * has to be declared in a single message.
+   */
+  void logAddService(const corba_profile_desc_t* serviceProfile);
+
+  /**
+   * Request best SeD for problem
+   */
+  void logAskForSeD(const corba_request_t* request);
+  void logSedChosen(const corba_request_t* request,
+                    const corba_response_t* response);
+
+  /**
+   * Solve a problem
+   * (No ID here, ID exists only in async and is client-specific)
+   */
+  void logBeginSolve(const char* path, const corba_profile_t* problem);
+  void logEndSolve(const char* path, const corba_profile_t* problem);
+
+  /**
+   * Track data movements
+   */
+  void logDataStore(const char* dataID);    // data profile ?
+  void logDataRelease(const char* dataID);
+  // invoked by Sender:
+  void logDataBeginTransfer(const char* dataID,
+			    const char* destAgent);
+  void logDataEndTransfer(const char* dataID,
+			    const char* destAgent);
+
+  /**
+   * NWS values
+   */
+  void logMem(double mem);
+  void logLoad(double load);
+  void logLatency(double latency);
+  void logBandwidth(double bandwidth);
+
+
+  /**
+   * No-User functions
+   */
+  
+  /**
+   * Synchronise with the LogCentral.
+   * Not to be called by the user.
+   */
+  void synchronize();
+
+  /**
+   * Ping the LogCentral. Not to be called by the user.
+   */
+  void ping();
+
+  /**
+   * Flushes the outBuffer. All stored messages are sent.
+   * Should not be called by the user.
+   */
+  void sendOutBuffer();
+
+  /**
+   * create a logmessage and store it in the outBuffer.
+   * Send the buffer if it is full. Not to be called by
+   * the user.
+   */
+  void log(const char* tag, const char* msg);
+
+  /**
+   * Implements the ComponentConfigurator class in LogComponent.idl
+   */
+  void setTagFilter(const tag_list_t& tagList);
+  /**
+   * Implements the ComponentConfigurator class in LogComponent.idl
+   */
+  void addTagFilter(const tag_list_t& tagList);
+  /**
+   * Implements the ComponentConfigurator class in LogComponent.idl
+   */
+  void removeTagFilter(const tag_list_t& tagList);
+
+private:
+  /**
+   * Helper function - allocates an array of bools which are
+   * set to false.
+   */
+  bool* createBoolArrayFalse(int size);
+
+  /**
+   * Checks if a given tag_list_t contains a '*'
+   */
+  bool containsStar(const tag_list_t* tagList);
+
+  /**
+   * Checks if a given tag is stored in the list of available tags
+   * on this component and returns its index in the internal array.
+   * Returns -1 if the tag is not known to this component.
+   */
+  int getTagIndex(const char* tag);
+
+  /**
+   * Takes all necessary actions if a remove procedure call fails
+   * due to a lost connection. It either shuts the whole agent down
+   * or deactivates all communication with the LogCentral.
+   */
+  void handleDisconnect(CORBA::SystemException &e);
+
+  /**
+   * return the local time (including milliseconds)
+   */
+  log_time_t getLocalTime();
+
+
+  /**
+   * connectionstatus to LogCentral
+   */
+  bool isConnected;
+
+  /**
+   * Remote LogCentralComponent object
+   */
+  LogCentralComponent_var myLCC;
+
+  /**
+   * this components name
+   */
+  CORBA::String_var name;
+
+  /**
+   * this DLCs pingThread
+   */
+  PingThread* pingThread;
+
+  /**
+   * this DLCs flushBufferThread
+   */
+  FlushBufferThread* flushBufferThread;
+
+  /**
+   * Synchronises the whole class
+   */
+  mutable omni_mutex dlcMutex;
+
+  /**
+   * The size of the outBuffer. If this value is reached, the
+   * buffer will be send.
+   */
+  int outBufferMaxSize;
+
+  /**
+   * The log_msg_t buffer storing all the messages that have to
+   * be sent.
+   */
+  log_msg_buf_t outBuffer;
+
+  /**
+   * The number of available tags.
+   */
+  int tagCount;
+
+  /**
+   * The names of the available tags
+   */
+  char** tagNames;
+
+  /**
+   * The status of the available tags. If this flag is true,
+   * the tag must be sent, otherwise it must not be sent.
+   */
+  bool* tagFlags;
+};
+
+#endif
