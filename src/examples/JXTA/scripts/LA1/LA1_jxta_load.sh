042e2480c46b2e84db828202ace7ac9a00d02c64
Cédric Tedeschi <cedric.tedeschi@inria.fr>
2004-07-06 13:52:27 +0000

    modify the scripts loading JXTA components (useless settings)

--- src/examples/JXTA/scripts/LA1/LA1_jxta_load.sh
+++ src/examples/JXTA/scripts/LA1/LA1_jxta_load.sh
@@ -2,8 +2,7 @@
 # Launch a LA via JNI with LA1.cfg as configuration file
 
 # To be configured
-export DIET_HOME=
+#export DIET_HOME=
 
-export LD_LIBRARY_PATH=${DIET_HOME}/src/agent/.libs:${LD_LIBRARY_PATH}
 
 java -cp ${DIET_HOME}/src/agent LA ${DIET_HOME}/src/examples/cfgs/LA1.cfg 
--- src/examples/JXTA/scripts/client/client_jxta_load.sh
+++ src/examples/JXTA/scripts/client/client_jxta_load.sh
@@ -2,7 +2,7 @@
 # Launch a JXTA client requesting for MatSUM
 
 # To be configured
-export DIET_HOME=
+#export DIET_HOME=
 
 java -cp .:${DIET_HOME}/src/examples/JXTA:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/saxpath.jar:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTAClient MatSUM
 
