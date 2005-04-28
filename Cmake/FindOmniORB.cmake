9e12d81e60103fc1b0925c526e6d2f763e5064fb
Eric Boix <eboix@ens-lyon.fr>
2005-04-28 12:35:58 +0000

    Added the cmake equivalent of autotool's acmacros/ directory.--- Injay 2461

---
+++ Cmake/FindOmniORB.cmake
@@ -0,0 +1,63 @@
+#
+# Find the omniORB libraries and include dir
+#
+ 
+# OMNIORB4_INCLUDE_DIR  - Directories to include to use omniORB
+# OMNIORB4_LIBRARIES    - Files to link against to use omniORB
+# OMNIORB4_FOUND        - If false, don't try to use omniORB
+# OMNIORB4_DIR          - (optional) Suggested installation directory to search
+#
+# OMNIORB4_DIR can be used to make it simpler to find the various include
+# directories and compiled libraries when omniORB was not installed in the
+# usual/well-known directories (e.g. because you made an in tree-source
+# compilation or because you installed it in an "unusual" directory).
+# Just set OMNIORB4_DIR it to your specific installation directory
+#
+ 
+FIND_PATH( OMNIORB4_omniORB4_SUBDIR omniORB4/CORBA.h
+  ${OMNIORB4_DIR}/include
+)
+
+#### FIXME: what are the required libraries for proper usage ????
+FIND_LIBRARY(  OMNIORB4_LIBRARY_omniORB4 omniORB4
+  ${OMNIORB4_DIR}/lib
+)
+
+FIND_LIBRARY( OMNIORB4_LIBRARY_omnithread omnithread
+  ${OMNIORB4_DIR}/lib
+)
+
+FIND_LIBRARY( OMNIORB4_LIBRARY_COS4 COS4
+  ${OMNIORB4_DIR}/lib
+)
+
+FIND_LIBRARY( OMNIORB4_LIBRARY_COSDynamic4 COSDynamic4
+  ${OMNIORB4_DIR}/lib
+)
+
+IF( OMNIORB4_omniORB4_SUBDIR )
+IF( OMNIORB4_LIBRARY_omniORB4 )
+IF( OMNIORB4_LIBRARY_omnithread )
+IF( OMNIORB4_LIBRARY_COS4 )
+IF( OMNIORB4_LIBRARY_COSDynamic4 )
+
+  SET( OMNIORB4_FOUND "YES" )
+  SET( OMNIORB4_INCLUDE_DIR
+    ${OMNIORB4_omniORB4_SUBDIR} )
+  SET( OMNIORB4_LIBRARIES
+    ${OMNIORB4_LIBRARY_omniORB4}
+    ${OMNIORB4_LIBRARY_omnithread}
+    ${OMNIORB4_LIBRARY_COS4}
+    ${OMNIORB4_LIBRARY_COSDynamic4} )
+
+ENDIF( OMNIORB4_LIBRARY_COSDynamic4 )
+ENDIF( OMNIORB4_LIBRARY_COS4 )
+ENDIF( OMNIORB4_LIBRARY_omnithread )
+ENDIF( OMNIORB4_LIBRARY_omniORB4 )
+ENDIF( OMNIORB4_omniORB4_SUBDIR )
+
+IF( NOT OMNIORB4_FOUND )
+  SET( OMNIORB4_DIR "" CACHE PATH "Root of omniORB instal tree (optional)." )
+  MARK_AS_ADVANCED( OMNIORB4_DIR )
+ENDIF( NOT OMNIORB4_FOUND )
+
