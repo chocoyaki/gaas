40a3fe43d86b4d5db757dd7c2030cd86de18e141
Alan Su <su.alan@gmail.com>
2005-08-31 15:01:10 +0000

    New plugin scheduling interface: implementation of the DIET-internal estimation vector access functions

---
+++ src/utils/est_internal.cc
@@ -0,0 +1,189 @@
+#ifndef OLD_EV
+#include "DIET_data.h"
+
+#include "debug.hh"
+#include "est_internal.hh"
+
+#define EV_ELT(e,i) (((e)->estValues)[(i)])
+#define EV_LEN(e) ((e)->estValues.length())
+#define EV_SETLEN(e,l) ((e)->estValues.length(l))
+
+int
+diet_est_set_internal(estVector_t ev, int tag, double value)
+{
+  if (ev == NULL) {
+    ERROR(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR(__FUNCTION__ << ": tag must be non-negative (" << tag << ")\n", -1);
+  }
+
+  for (unsigned int valIter = 0 ; valIter < EV_LEN(ev) ; valIter++) {
+    if (tag == (EV_ELT(ev, valIter)).v_tag &&
+        (EV_ELT(ev, valIter)).v_idx < 0) {
+      (EV_ELT(ev, valIter)).v_value = value;
+      return (1);
+    }
+  }
+
+  EV_SETLEN(ev, EV_LEN(ev) + 1);
+  (EV_ELT(ev, EV_LEN(ev)-1)).v_tag = tag;
+  (EV_ELT(ev, EV_LEN(ev)-1)).v_idx = EST_IDX_SCALAR;
+  (EV_ELT(ev, EV_LEN(ev)-1)).v_value = value;
+  
+  return (1);
+}
+
+double
+diet_est_get_internal(estVectorConst_t ev, int tag, double errVal)
+{
+  if (ev == NULL) {
+    ERROR(__FUNCTION__ << ": NULL estimation vector", errVal);
+  }
+  if (tag < 0) {
+    ERROR(__FUNCTION__ <<
+          ": tag must be non-negative (" <<
+          tag <<
+          ")\n",
+          errVal);
+  }
+
+  for (unsigned int valIter = 0 ; valIter < EV_LEN(ev) ; valIter++) {
+    if (tag == (EV_ELT(ev, valIter)).v_tag &&
+        (EV_ELT(ev, valIter)).v_idx < 0) {
+      return ((EV_ELT(ev, valIter)).v_value);
+    }
+  }
+
+  return (errVal);
+}
+
+int
+diet_est_defined_internal(estVectorConst_t ev, int tag)
+{
+  if (ev == NULL) {
+    ERROR(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR(__FUNCTION__ << ": tag must be non-negative (" << tag << ")\n", -1);
+  }
+
+//   cout << "AS: [" << __FUNCTION__ << "] num values = " << EV_LEN(ev) << endl;
+
+  for (unsigned int valIter = 0 ; valIter < EV_LEN(ev) ; valIter++) {
+    if (tag == (EV_ELT(ev, valIter)).v_tag &&
+        (EV_ELT(ev, valIter)).v_idx < 0) {
+      return (1);
+    }
+  }
+
+  return (0);
+}
+
+int
+diet_est_array_size_internal(estVectorConst_t ev, int tag)
+{
+  if (ev == NULL) {
+    ERROR(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR(__FUNCTION__ << ": tag must be non-negative (" << tag << ")\n", -1);
+  }
+
+  int maxIdx = -1;
+  for (unsigned int valIter = 0 ; valIter < EV_LEN(ev) ; valIter++) {
+    if (tag == (EV_ELT(ev, valIter)).v_idx &&
+        (EV_ELT(ev, valIter)).v_idx > maxIdx) {
+      maxIdx = (EV_ELT(ev, valIter)).v_idx;
+    }
+  }
+
+  return (maxIdx + 1);
+}
+
+int
+diet_est_array_set_internal(estVector_t ev, int tag, int idx, double value)
+{
+  if (ev == NULL) {
+    ERROR(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR(__FUNCTION__ << ": tag must be non-negative (" << tag << ")\n", -1);
+  }
+  if (idx < 0) {
+    ERROR(__FUNCTION__ << ": idx must be non-negative (" << idx << ")\n", -1);
+  }
+
+  for (unsigned int valIter = 0 ; valIter < EV_LEN(ev) ; valIter++) {
+    if (tag == (EV_ELT(ev, valIter)).v_tag &&
+        (EV_ELT(ev, valIter)).v_idx == idx) {
+      (EV_ELT(ev, valIter)).v_value = value;
+      return (1);
+    }
+  }
+
+  EV_SETLEN(ev, EV_LEN(ev) + 1);
+  (EV_ELT(ev, EV_LEN(ev)-1)).v_tag = tag;
+  (EV_ELT(ev, EV_LEN(ev)-1)).v_idx = idx;
+  (EV_ELT(ev, EV_LEN(ev)-1)).v_value = value;
+
+  return (1);
+}
+
+double
+diet_est_array_get_internal(estVectorConst_t ev,
+                            int tag,
+                            int idx,
+                            double errVal)
+{
+  if (ev == NULL) {
+    ERROR(__FUNCTION__ << ": NULL estimation vector", errVal);
+  }
+  if (tag < 0) {
+    ERROR(__FUNCTION__ <<
+          ": tag must be non-negative (" <<
+          tag <<
+          ")\n",
+          errVal);
+  }
+  if (idx < 0) {
+    ERROR(__FUNCTION__ <<
+          ": idx must be non-negative (" <<
+          idx <<
+          ")\n",
+          errVal);
+  }
+
+  for (unsigned int valIter = 0 ; valIter < EV_LEN(ev) ; valIter++) {
+    if (tag == (EV_ELT(ev, valIter)).v_tag &&
+        (EV_ELT(ev, valIter)).v_idx == idx) {
+      return ((EV_ELT(ev, valIter)).v_value);
+    }
+  }
+
+  return (errVal);
+}
+
+int
+diet_est_array_defined_internal(estVectorConst_t ev, int tag, int idx)
+{
+  if (ev == NULL) {
+    ERROR(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR(__FUNCTION__ << ": tag must be non-negative (" << tag << ")\n", -1);
+  }
+  if (idx < 0) {
+    ERROR(__FUNCTION__ << ": idx must be non-negative (" << idx << ")\n", -1);
+  }
+
+  for (unsigned int valIter = 0 ; valIter < EV_LEN(ev) ; valIter++) {
+    if (tag == (EV_ELT(ev, valIter)).v_tag &&
+        (EV_ELT(ev, valIter)).v_idx == idx) {
+      return (1);
+    }
+  }
+
+  return (0);
+}
+#endif /* OLD_EV */
---
+++ src/utils/est_internal.hh
@@ -0,0 +1,31 @@
+#ifndef OLD_EV
+#ifndef _EST_INTERNAL_HH_
+#define _EST_INTERNAL_HH_
+
+/*
+** Set of #define's for future "rules" for the scalar
+** values of estimation values.  The first
+** (EST_IDX_SCALAR) denotes a scalar value that does
+** not have a rule associated with it.
+**
+** (All new rule values should be negative, to avoid
+** colliding with the values for valid array values!)
+*/
+#define EST_IDX_SCALAR -1
+
+int diet_est_set_internal(estVector_t ev, int tag, double value);
+double diet_est_get_internal(estVectorConst_t ev, int tag, double errVal);
+int diet_est_defined_internal(estVectorConst_t ev, int tag);
+int diet_est_array_size_internal(estVectorConst_t ev, int tag);
+int diet_est_array_set_internal(estVector_t ev,
+                                int tag,
+                                int idx,
+                                double value);
+double diet_est_array_get_internal(estVectorConst_t ev,
+                                   int tag,
+                                   int idx,
+                                   double errVal);
+int diet_est_array_defined_internal(estVectorConst_t ev, int tag, int idx);
+
+#endif /* _EST_INTERNAL_HH_ */
+#endif /* OLD_EV */
