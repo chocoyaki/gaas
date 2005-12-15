006df34a37fdd38f464adc928c4a07be49759588
Peter Frauenkron <peter.frauenkron@gmail.com>
2005-12-15 09:42:36 +0000

    CoRI example added Peter Frauenkron

---
+++ src/examples/BLAS/client_scheduler.c
@@ -0,0 +1,330 @@
+/****************************************************************************/
+/* DIET client for BLAS functions                                           */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Philippe COMBES (Philippe.Combes@ens-lyon.fr)                       */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2005/12/15 09:42:36  pfrauenk
+ * CoRI example added Peter Frauenkron
+ *
+ * Revision 1.8  2005/04/25 09:03:36  hdail
+ * Use hard-coded row and column orderings as input to BLAS service so we obtain
+ * dependable performance.  Later we need to add more advanced example that allows
+ * user to pass in file-based matrices with information on their ordering given on
+ * command line.
+ *
+ * Revision 1.7  2003/07/25 20:37:36  pcombes
+ * Separate the DIET API (slightly modified) from the GridRPC API (version of
+ * the draft dated to 07/21/2003)
+ *
+ * Revision 1.6  2003/04/10 13:33:21  pcombes
+ * Apply new Coding Standards.
+ *
+ * Revision 1.5  2003/02/07 17:05:23  pcombes
+ * Add SqMatSUM_opt with the new convertor API.
+ * Use diet_free_data to properly free user's data.
+ *
+ * Revision 1.4  2003/01/23 19:13:44  pcombes
+ * Update to API 0.6.4
+ *
+ * Revision 1.3  2003/01/17 18:05:37  pcombes
+ * Update to API 0.6.3
+ *
+ * Revision 1.2  2002/12/12 18:17:04  pcombes
+ * Small bug fixes on prints (special thanks to Jean-Yves)
+ ****************************************************************************/
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#include "DIET_client.h"
+
+#define print_matrix(mat, m, n, rm)        \
+  {                                        \
+    size_t i, j;                           \
+    printf("%s (%s-major) = \n", #mat,     \
+           (rm) ? "row" : "column");       \
+    for (i = 0; i < (m); i++) {            \
+      for (j = 0; j < (n); j++) {          \
+        if (rm)                            \
+	  printf("%3f ", (mat)[j + i*(n)]);\
+        else                               \
+	  printf("%3f ", (mat)[i + j*(m)]);\
+      }                                    \
+      printf("\n");                        \
+    }                                      \
+    printf("\n");                          \
+  }
+
+
+int
+empty_line()
+{
+  while (fgetc(stdin) != '\n');
+  return 1;
+}
+
+#define NB_PB 5
+static const char* PB[NB_PB] =
+  {"dgemm", "SqMatSUM", "SqMatSUM_opt", "MatPROD", "MatScalMult"};
+
+
+/* argv[1]: client config file path
+ * argv[2]: one of the strings above */
+
+int
+main(int argc, char* argv[])
+{
+  char* path = NULL;
+  diet_profile_t* profile = NULL;
+
+  size_t i, j, m, n, k;
+  double alpha, beta;
+  double* A = NULL;
+  double* B = NULL;
+  double* C = NULL;
+  diet_matrix_order_t oA, oB, oC;
+
+  srand(time(NULL));
+  
+  /*oA = (rand() & 1) ? DIET_ROW_MAJOR : DIET_COL_MAJOR;
+  oB = (rand() & 1) ? DIET_ROW_MAJOR : DIET_COL_MAJOR;
+  oC = (rand() & 1) ? DIET_ROW_MAJOR : DIET_COL_MAJOR;*/
+  oA = DIET_ROW_MAJOR;
+  oB = DIET_COL_MAJOR;
+  oC = DIET_ROW_MAJOR;
+
+ if (argc != 3) {
+   fprintf(stderr, "Usage: %s <file.cfg> [%s|%s|%s|%s|%s]\n",
+	   argv[0], PB[0], PB[1], PB[2], PB[3], PB[4]);
+    return 1;
+  }
+  path = argv[2];
+
+  if (diet_initialize(argv[1], argc, argv)) {
+    fprintf(stderr, "DIET initialization failed !\n");
+    return 1;
+  }
+
+
+  /*********************
+   * dgemm
+   *********************/
+
+  if (!strcmp(path, PB[0])) {
+  
+    printf("%s: C = alpha.A*B + beta.C, with A[m,k], B[k,n] and C[m,n]\n", path);
+    printf("A, B, and C are generated randomly, from entered m, n and k.\n");
+    printf("Please enter m: ");
+    while (((fscanf(stdin, "%d", &m) != 1) && empty_line()) || (m <= 0))
+      printf("m is a postive integer - Please enter m: ");
+    printf("Please enter n: ");
+    while (((fscanf(stdin, "%d", &n) != 1) && empty_line()) || (n <= 0))
+      printf("n is a postive integer - Please enter n: ");
+    printf("Please enter k: ");
+    while (((fscanf(stdin, "%d", &k) != 1) && empty_line()) || (k <= 0))
+      printf("k is a postive integer - Please enter k: ");
+    printf("Please enter alpha: ");
+    while ((fscanf(stdin, "%lf", &alpha) != 1) && empty_line())
+      printf("alpha is a double - Please enter alpha: ");
+    printf("Please enter beta: ");
+    while ((fscanf(stdin, "%lf", &beta) != 1) && empty_line())
+      printf("beta is a double - Please enter beta: ");
+  
+    // Fill A, B and C randomly ...
+    A = calloc(m*k, sizeof(double));
+    B = calloc(k*n, sizeof(double));
+    C = calloc(m*n, sizeof(double));
+    for (i = j = 0; i < m * k; i++) A[i] = 1.0 + j++;
+    for (i = 0; i < k * n; i++)     B[i] = 1.0 + j++;
+    for (i = 0; i < m * n; i++)     C[i] = 1.0 + j++;
+
+    profile = diet_profile_alloc(path, 3, 4, 4);
+    diet_scalar_set(diet_parameter(profile,0), &alpha,
+		    DIET_VOLATILE, DIET_DOUBLE);
+    diet_matrix_set(diet_parameter(profile,1), A,
+		    DIET_VOLATILE, DIET_DOUBLE, m, k, oA);
+    diet_matrix_set(diet_parameter(profile,2), B,
+		    DIET_VOLATILE, DIET_DOUBLE, k, n, oB);
+    diet_scalar_set(diet_parameter(profile,3), &beta,
+		    DIET_VOLATILE, DIET_DOUBLE);
+    diet_matrix_set(diet_parameter(profile,4), C,
+		    DIET_VOLATILE, DIET_DOUBLE, m, n, oC);
+
+    print_matrix(A, m, k, (oA == DIET_ROW_MAJOR));
+    print_matrix(B, k, n, (oB == DIET_ROW_MAJOR));
+    print_matrix(C, m, n, (oC == DIET_ROW_MAJOR));
+
+  /*********************
+   * SqMatSUM_opt
+   *********************/
+
+  } else if (!strcmp(path, PB[2])) {
+    
+    printf("%s: C = A + C, with A[m,m]\n", path);
+    printf("A and C are generated randomly, from entered m.\n");
+    printf("Please enter m: ");
+    while (((fscanf(stdin, "%d", &m) != 1) && empty_line()) || (m <= 0))
+      printf("m is a positive integer - Please enter m: ");
+    n = m;
+    
+    // Fill A, B and C randomly ...
+    A = calloc(m*m, sizeof(double));
+    C = calloc(m*m, sizeof(double));
+    for (i = j = 0; i < m * m; i++) A[i] = 1.0 + j++;
+    for (i = 0; i < m * m; i++)     C[i] = 1.0 + j++;
+
+    profile = diet_profile_alloc(path, 0, 1, 1);
+    diet_matrix_set(diet_parameter(profile,0),
+		    A, DIET_VOLATILE, DIET_DOUBLE, m, m, oA);
+    diet_matrix_set(diet_parameter(profile,1),
+		    C, DIET_VOLATILE, DIET_DOUBLE, m, m, oC);
+    
+    print_matrix(A, m, m, (oA == DIET_ROW_MAJOR));
+    print_matrix(C, m, m, (oC == DIET_ROW_MAJOR));
+
+
+    
+  /*********************
+   * SqMatSUM and MatPROD
+   *********************/
+
+  } else if (!(strcmp(path, PB[1]) && strcmp(path, PB[3]))) {
+
+    if (!strcmp(path, PB[1])) {
+      printf("%s: C = A + B, with A[m,m] and B[m,m]\n", path);
+      printf("A and B are generated randomly, from entered m.\n");
+    } else {
+      printf("%s: C = A * B, with A[m,k] and B[k,n]\n", path);
+      printf("A and B are generated randomly, from entered m, n and k.\n");
+    }
+    printf("Please enter m: ");
+    while (((fscanf(stdin, "%d", &m) != 1) && empty_line()) || (m <= 0))
+      printf("m is a positive integer - Please enter m: ");
+    if (!strcmp(path, PB[1])) {
+      k = m;
+      n = m;
+    } else {
+      printf("Please enter n: ");
+      while (((fscanf(stdin, "%d", &n) != 1) && empty_line()) || (n <= 0))
+	printf("n is a positive integer - Please enter n: ");
+      printf("Please enter k: ");
+      while (((fscanf(stdin, "%d", &k) != 1) && empty_line()) || (k <= 0))
+	printf("k is a positive integer - Please enter k: ");
+    }
+
+    // Fill A, B and C randomly ...
+    A = calloc(m*k, sizeof(double));
+    B = calloc(k*n, sizeof(double));
+    for (i = j = 0; i < m * k; i++) A[i] = 1.0 + j++;
+    for (i = 0; i < k * n; i++)     B[i] = 1.0 + j++;
+
+    profile = diet_profile_alloc(path, 1, 1, 2);
+    diet_matrix_set(diet_parameter(profile,0),
+		    A, DIET_VOLATILE, DIET_DOUBLE, m, k, oA);
+    diet_matrix_set(diet_parameter(profile,1),
+		    B, DIET_VOLATILE, DIET_DOUBLE, k, n, oB);
+    diet_matrix_set(diet_parameter(profile,2),
+		    NULL, DIET_VOLATILE, DIET_DOUBLE, m, n, oC);
+
+    print_matrix(A, m, k, (oA == DIET_ROW_MAJOR));
+    print_matrix(B, k, n, (oB == DIET_ROW_MAJOR));
+
+
+    
+  /*********************
+   * MatScalMult
+   *********************/
+
+  } else if (!strcmp(path, PB[4])) {
+  
+    printf("%s: C = alpha.C, with C[m,n]\n", path);
+    printf("C is generated randomly, from entered m and n.\n");
+    printf("Please enter m: ");
+    while (((fscanf(stdin, "%d", &m) != 1) && empty_line()) || (m <= 0))
+      printf("m is a positive integer - Please enter m: ");
+    printf("Please enter n: ");
+    while (((fscanf(stdin, "%d", &n) != 1) && empty_line()) || (n <= 0))
+      printf("n is a positive integer - Please enter n: ");
+    printf("Please enter alpha: ");
+    while ((fscanf(stdin, "%lf", &alpha) != 1) && empty_line())
+      printf("alpha is a double - Please enter alpha: ");
+
+    // Fill A, B and C randomly ...
+    C = calloc(m*n, sizeof(double));
+    for (i = j = 0; i < m * n; i++) C[i] = 1.0 + j++;
+
+    profile = diet_profile_alloc(path, 0, 1, 1);
+    diet_scalar_set(diet_parameter(profile,0), &alpha, DIET_VOLATILE, DIET_DOUBLE);
+    diet_matrix_set(diet_parameter(profile,1),
+		    C, DIET_VOLATILE, DIET_DOUBLE, m, n, oC);
+
+    print_matrix(C, m, n, (oC == DIET_ROW_MAJOR));
+
+
+    
+  /*********************
+   * Dummy pb (no server offers the correspunding service)
+   *********************/
+
+  } else {
+
+    alpha = 0.0;
+    m = n = 0;
+    profile = diet_profile_alloc(path, -1, 0, 0);
+    diet_scalar_set(diet_parameter(profile,0), &alpha,
+		    DIET_VOLATILE, DIET_DOUBLE);
+  }
+  
+
+  /*********************
+   * DIET Call
+   *********************/
+#if HAVE_CORI
+timeval tim;
+  gettimeofday(&tim, NULL);
+  double t1=tim.tv_sec+(tim.tv_usec/1000000.0);
+  //clock started
+#endif //HAVE_CORI
+
+  if (!diet_call(profile)) {
+    if (!(strcmp(path, PB[1]) && strcmp(path, PB[3]))) {
+      // C is OUT and thus must be set
+      diet_matrix_get(diet_parameter(profile,2), &C, NULL, NULL, NULL, &oC);
+      print_matrix(C, m, n, (oC == DIET_ROW_MAJOR));
+      diet_free_data(diet_parameter(profile,2));
+      C = NULL;
+    } else {
+      print_matrix(C, m, n, (oC == DIET_ROW_MAJOR));
+    }
+  }
+
+  if (A) free(A);
+  if (B) free(B);
+  if (C) free(C);
+  
+  diet_profile_free(profile);
+  
+  diet_finalize();
+#if HAVE_CORI
+  //stop clock
+gettimeofday(&tim, NULL);
+  double t2=tim.tv_sec+(tim.tv_usec/1000000.0);
+  double elapsed_time= t2-t1;
+  cerr<<"time to accomplish this task: "<<elapsed_time<<endl;
+  ofstream outfile;
+outfile.open ("./results.res", ofstream::out);
+ outfile<<elapsed_time<<endl;
+ 
+#endif //HAVE_CORI
+  return 0;
+}
+
---
+++ src/examples/BLAS/server_scheduler.c
@@ -0,0 +1,435 @@
+/****************************************************************************/
+/* DIET server for BLAS functions                                           */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Philippe COMBES (Philippe.Combes@ens-lyon.fr)                       */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2005/12/15 09:42:36  pfrauenk
+ * CoRI example added Peter Frauenkron
+ *
+ * Revision 1.10  2005/06/24 15:03:59  ycaniou
+ * NB_SRV=5 services and diet_service_table_init(3) corrected
+ *
+ * Revision 1.9  2003/08/09 17:32:47  pcombes
+ * Update to the new diet_profile_desc_t.
+ *
+ * Revision 1.8  2003/04/10 13:33:21  pcombes
+ * Apply new Coding Standards.
+ *
+ * Revision 1.7  2003/02/07 17:05:23  pcombes
+ * Add SqMatSUM_opt with the new convertor API.
+ * Use diet_free_data to properly free user's data.
+ *
+ * Revision 1.6  2003/01/23 19:13:44  pcombes
+ * Update to API 0.6.4
+ *
+ * Revision 1.5  2003/01/17 18:05:37  pcombes
+ * Update to API 0.6.3
+ *
+ * Revision 1.3  2002/12/18 12:11:42  jylexcel
+ * SqMatSUM bug fix related to the use of convertors. Philippe+JY.
+ *
+ * Revision 1.2  2002/12/12 18:17:04  pcombes
+ * Small bug fixes on prints (special thanks to Jean-Yves)
+ ****************************************************************************/
+
+
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#include "DIET_server.h"
+
+#if HAVE_CORI
+static void
+performanceFn(diet_profile_t* pb, estVector_t perfValues);
+#endif
+
+/**
+ * dgemm_ prototype
+ */
+extern void
+dgemm_(char*   tA,    char* tB,  int* m,   int* n,    int* k,
+       double* alpha, double* A, int* lda, double* B, int* ldb,
+       double* beta,  double* C, int* ldc);
+
+
+#define print_matrix(mat, m, n, rm)        \
+  {                                        \
+    size_t i, j;                           \
+    printf("%s (%s-major) = \n", #mat,     \
+           (rm) ? "row" : "column");       \
+    for (i = 0; i < (m); i++) {            \
+      for (j = 0; j < (n); j++) {          \
+        if (rm)                            \
+	  printf("%3f ", (mat)[j + i*(n)]);\
+        else                               \
+	  printf("%3f ", (mat)[i + j*(m)]);\
+      }                                    \
+      printf("\n");                        \
+    }                                      \
+    printf("\n");                          \
+  }
+
+
+/* This server can offer 5 services (chosen by user at launch time): */
+/*   - T = matrix translation                                        */
+/*   - MatSUM = matrix addition (and thus SqMatSUM and SqMatSUM_opt) */
+/*   - MatPROD = matrix product                                      */
+
+#define NB_SRV 5
+static const char* SRV[NB_SRV] =
+  {"dgemm", "MatPROD", "SqMatSUM", "SqMatSUM_opt", "MatScalMult"};
+
+
+/*
+ * SOLVE FUNCTION for dgemm_
+ * C MUST BE COLUMN-MAJOR
+ */
+
+int
+solve_dgemm(diet_profile_t* pb)
+{
+  char    tA, tB;
+  diet_matrix_order_t oA, oB, oC;
+  size_t  i, k, k_, m, m_, n, n_;
+  double* alpha = NULL;
+  double* beta  = NULL;
+  double* A = NULL;
+  double* B = NULL;
+  double* C = NULL;
+  int     IsSqMatSUM = 0;
+
+  diet_matrix_get(diet_parameter(pb,0), &A, NULL, &m, &k, &oA);
+  tA = (oA == DIET_ROW_MAJOR) ? 'T' : 'N';
+  diet_matrix_get(diet_parameter(pb,1), &B, NULL, &k_, &n, &oB);
+  tB = (oB == DIET_ROW_MAJOR) ? 'T' : 'N';
+  diet_scalar_get(diet_parameter(pb,2), &alpha, NULL);
+  diet_scalar_get(diet_parameter(pb,3), &beta,  NULL);
+  diet_matrix_get(diet_parameter(pb,4), &C, NULL, &m_, &n_, &oC);
+  
+  /* A and B NULL => MatScalMult, then m and n are wrong
+   *  only B NULL => SqMatSUM, then set B to identity (and k_ for later test) */
+  if (!A && !B) {
+    m = m_;
+    n = n_;
+    k = (m > n) ? m : n;
+    k_ = k;
+  }
+  if ((IsSqMatSUM = (A && !B))) {
+    k_ = k;
+    n = n_;
+    if (m != n || n != k || m != k) {
+      fprintf(stderr, "dgemm Error: only square matrices can be summed.\n");
+      return 1;
+    }
+    B = (double*) calloc(m * m, sizeof(double));
+    for (i = 0; i < m; i++) {
+      B[i + m * i] = 1.0;
+    }
+  }
+
+  if ((k_ != k) || (m_ != m) || (n_ != n)) {
+    fprintf(stderr, "dgemm Error: invalid matrix dimensions: ");
+    fprintf(stderr, "%dx%d = %dx%d * %dx%d\n", m_, n_, m, k, k_, n);
+    return 1;
+  }
+
+  // DEBUG
+  
+  printf("Solving dgemm_ ...");
+  if (oC == DIET_ROW_MAJOR) {
+    tA = (tA == 'T') ? 'N' : 'T';
+    tB = (tB == 'T') ? 'N' : 'T';
+    fprintf(stderr,
+	    "dgemm args : m=%d, n=%d, k=%d, alpha=%f, beta=%f, tA=%c, tB=%c\n",
+	    n, m, k, *alpha, *beta, tB, tA);
+    dgemm_(&tB, &tA, &n, &m, &k, alpha,
+	   B, (tB == 'T') ? &k : &n,
+	   A, (tA == 'T') ? &m : &k,
+	   beta, C, &n);  
+  } else {
+    fprintf(stderr,
+	    "dgemm args : m=%d, n=%d, k=%d, alpha=%f, beta=%f, tA=%c, tB=%c\n",
+	    m, n, k, *alpha, *beta, tA, tB);
+    dgemm_(&tA, &tB, &m, &n, &k, alpha,
+	   A, (tA == 'T') ? &k : &m,
+	   B, (tB == 'T') ? &n : &k,
+	   beta, C, &m);
+  }
+  printf(" done.\n");
+
+  diet_free_data(diet_parameter(pb,0));
+  diet_free_data(diet_parameter(pb,1));
+  diet_free_data(diet_parameter(pb,2));
+  diet_free_data(diet_parameter(pb,3));
+
+  if (IsSqMatSUM) {
+    free(B);
+  }
+
+  return 0;
+}
+
+/*
+ * MAIN
+ */
+
+int
+main(int argc, char* argv[])
+{
+  int res;
+  diet_profile_desc_t* profile = NULL;
+  diet_convertor_t* cvt = NULL;
+  diet_arg_t* arg = NULL;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <file.cfg>\n", argv[0]);
+    return 1;
+  }
+
+  diet_service_table_init(NB_SRV);
+
+  cvt = diet_convertor_alloc("BLAS/dgemm", 3, 4, 4);
+
+
+  /*
+   * Adding dgemm
+   */
+  /* Set profile */
+  profile = diet_profile_desc_alloc(SRV[0], 3, 4, 4);
+  diet_generic_desc_set(diet_param_desc(profile,0),
+			DIET_SCALAR, DIET_DOUBLE); // alpha
+  diet_generic_desc_set(diet_param_desc(profile,1),
+			DIET_MATRIX, DIET_DOUBLE); // A
+  diet_generic_desc_set(diet_param_desc(profile,2),
+			DIET_MATRIX, DIET_DOUBLE); // B
+  diet_generic_desc_set(diet_param_desc(profile,3),
+			DIET_SCALAR, DIET_DOUBLE); // beta
+  diet_generic_desc_set(diet_param_desc(profile,4),
+			DIET_MATRIX, DIET_DOUBLE); // C
+  /* Set convertor */
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,0), 1, NULL);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,1), 2, NULL);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,2), 0, NULL);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,3), 3, NULL);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,4), 4, NULL);
+  /* Add */
+  if (diet_service_table_add(profile, cvt, solve_dgemm)) return 1;
+  diet_profile_desc_free(profile);
+
+
+  /*
+   * Adding MatPROD
+   */
+  /* Set profile */
+  profile = diet_profile_desc_alloc(SRV[1], 1, 1, 2);
+  diet_generic_desc_set(diet_param_desc(profile,0), DIET_MATRIX, DIET_DOUBLE);
+  diet_generic_desc_set(diet_param_desc(profile,1), DIET_MATRIX, DIET_DOUBLE);
+  diet_generic_desc_set(diet_param_desc(profile,2), DIET_MATRIX, DIET_DOUBLE);
+  /* Set convertor */
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,0), 0, NULL);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,1), 1, NULL);
+  {
+    double alpha = 1.0;
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    diet_scalar_set(arg, &alpha, DIET_VOLATILE, DIET_DOUBLE);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,2), -1, arg);
+  {
+    double beta = 0.0;
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    diet_scalar_set(arg, &beta, DIET_VOLATILE, DIET_DOUBLE);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,3), -1, arg);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,4),  2, NULL);
+  /* Add */
+  if (diet_service_table_add(profile, cvt, solve_dgemm)) return 1;
+  diet_profile_desc_free(profile);
+
+
+  /*
+   * Adding SqMatSUM (IN, IN, OUT)
+   */
+  /* Set profile */
+  profile = diet_profile_desc_alloc(SRV[2], 1, 1, 2);
+  diet_generic_desc_set(diet_param_desc(profile,0), DIET_MATRIX, DIET_DOUBLE);
+  diet_generic_desc_set(diet_param_desc(profile,1), DIET_MATRIX, DIET_DOUBLE);
+  diet_generic_desc_set(diet_param_desc(profile,2), DIET_MATRIX, DIET_DOUBLE);
+  /* Set convertor
+     NB: the difficulty here is that the INOUT parameter of the dgemm
+         corresponds to the second IN AND the OUT parameter of the profile. */
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,0), 0, NULL);
+  {
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    diet_matrix_set(arg, NULL, DIET_VOLATILE,
+		    DIET_DOUBLE, 0, 0, DIET_COL_MAJOR);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,1), -1, arg);
+  {
+    double alpha = 1.0;
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    diet_scalar_set(arg, &alpha, DIET_VOLATILE, DIET_DOUBLE);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,2), -1, arg);
+  // beta is the same
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,3), -1, arg);
+  // Use all arguments of diet_arg_cvt_set, since out_arg_idx == 2 (the OUT
+  // parameter of the profile) and in_arg_idx == 1 (the second IN parameter).
+  diet_arg_cvt_set(diet_arg_conv(cvt,4), DIET_CVT_IDENTITY, 1, NULL, 2);
+  /* Add */
+  if (diet_service_table_add(profile, cvt, solve_dgemm)) return 1;
+  diet_profile_desc_free(profile);
+
+
+  /*
+   * Adding SqMatSUM_opt (IN, INOUT)
+   */
+  /* Set profile */
+  profile = diet_profile_desc_alloc(SRV[3], 0, 1, 1);
+  diet_generic_desc_set(diet_param_desc(profile,0), DIET_MATRIX, DIET_DOUBLE);
+  diet_generic_desc_set(diet_param_desc(profile,1), DIET_MATRIX, DIET_DOUBLE);
+  /* Set convertor */
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,0), 0, NULL);
+  {
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    diet_matrix_set(arg, NULL, DIET_VOLATILE,
+		    DIET_DOUBLE, 0, 0, DIET_COL_MAJOR);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,1), -1, arg);
+  {
+    double alpha = 1.0;
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    diet_scalar_set(arg, &alpha, DIET_VOLATILE, DIET_DOUBLE);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,2), -1, arg);
+  // beta is the same
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,3), -1, arg);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,4), 1, NULL);
+  /* Add */
+  if (diet_service_table_add(profile, cvt, solve_dgemm)) return 1;
+  diet_profile_desc_free(profile);
+
+
+  /*
+   * Adding MatScalMult
+   */
+  /* Set profile */
+  profile = diet_profile_desc_alloc(SRV[4], 0, 1, 1);
+  // beta and C
+  diet_generic_desc_set(diet_param_desc(profile,0), DIET_SCALAR, DIET_DOUBLE);
+  diet_generic_desc_set(diet_param_desc(profile,1), DIET_MATRIX, DIET_DOUBLE);
+  /* Set convertor */
+  {
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    // 1x1 matrix (to force dimensions set, because 0 is ignored)
+    diet_matrix_set(arg, NULL, DIET_VOLATILE, DIET_DOUBLE, 1, 1, DIET_COL_MAJOR);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,0), -1, arg);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,1), -1, arg);
+  {
+    double alpha = 0.0;
+    arg = (diet_arg_t*) calloc(1, sizeof(diet_arg_t));
+    diet_scalar_set(arg, &alpha, DIET_VOLATILE, DIET_DOUBLE);
+  }
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,2), -1, arg);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,3),  0, NULL);
+  diet_arg_cvt_short_set(diet_arg_conv(cvt,4),  1, NULL);
+
+  /* Add a plugin schedular for this case */
+#if HAVE_CORI
+/* new section of the profile: aggregator */
+    diet_aggregator_desc_t *agg;
+    agg = diet_profile_desc_aggregator(profile);
+
+    /* install our custom performance function */
+    diet_service_use_perfmetric(performanceFn);
+
+    /* for this service, use a priority scheduler */
+    diet_aggregator_set_type(agg, DIET_AGG_PRIORITY);
+    //diet_aggregator_priority_minuser(agg, 0);
+    diet_aggregator_priority_max(agg, 0);
+    
+#endif //HAVE_CORI
+
+
+  /* Add */
+  if (diet_service_table_add(profile, cvt, solve_dgemm)) return 1;
+  diet_profile_desc_free(profile);
+
+  /* The same cvt has been used for all services, free it now */
+  diet_convertor_free(cvt);
+  
+  res = diet_SeD(argv[1], argc, argv);
+  // Not reached
+  return res;
+}
+
+
+/***********PLUGIN - CORI *************************/
+#if HAVE_CORI
+
+/**
+ * thevector: is the vector where to add the new value
+ * valToAdd: the val to add
+ * POST: the 'valToAdd' must be the last value in 'thevector'
+ *       th vector size must be maximum 'size'
+*/
+ static vector <double> *vector_values = NULL;
+
+void add_new_value(vector <double> *thevector, double valToAdd, int size){
+  vector<double>::iterator iter = thevector->begin();
+  
+  thevector->push_back(valToAdd);
+
+  if (thevector->size()>size)
+    thevector->erase(iter);
+}
+
+
+/*
+** performanceFn: the performance function to use in the DIET
+**   plugin scheduling facility
+** The SeD must maintain a vector with the last executions
+** the scalar of user defined tag must be the value of the eldest
+** value. The size of the vector is (finally -after enough executions)
+** the number of CPUs. In this manner, scheduling takes care of NBCPU 
+** without occuping only the SeD with the most CPUs
+*/
+
+static void
+performanceFn(estVector_t perfValues)
+{
+
+  CORIMgr::diet_estimate_cori(*perfValues,EST_NBCPU, EST_COLL_EASY,NULL);
+  
+  /*store the timestamp since last execution */
+  diet_estimate_lastexec(perfValues, pb);
+  if (vector_values==NULL)
+    vector_values=new  vector <double>;
+
+ // stock the value in vector_values (the most recent is on index 0)
+  add_new_value(vector_values, diet_est_get(perfValues,EST_TIMESINCELASTSOLVE,-1))
+
+ 
+  //if the stocked number of executions is less then the number of CPUs, then
+  // stock in the perfValues on tag userdefine only the number of cpu + 10000
+  // this is to prevent that an low number-CPU-SeD receive too fast a new task
+  if (vector_values.size()<diet_est_get(perfValues,EST_NBCPU,1)){
+    diet_est_set(perfValues,0,diet_est_get(perfValues,EST_NBCPU,1)-vector_values.size()+10000);   
+  }
+  else{
+    vector<double>::iterator theIterator = vector_values.begin();
+     //stock the last element of vector_values in the scalar of perfValues
+    diet_est_set(perfValues,0,*iter);   
+  }
+
+}
+#endif //HAVE_CORI
