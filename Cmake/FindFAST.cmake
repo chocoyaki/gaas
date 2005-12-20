5a31731cb68ac4601002f275a18025ef45ba9030
Eric Boix <eboix@ens-lyon.fr>
2005-12-20 15:06:35 +0000

    * CMakeLists.txt:   - additional DIET_USE_BLAS switch added   - FAST is now handled when DIET_USE_FAST is ON:     -- Apparently only UtilsCommon requires linking against fast libraries,        allthough files like src/utils/Parsers.cc (UtilsCommon library)        or src/agent/AgentImpl.cc seem to depend on the HAVE_FAST symbol !?     -- CMAKE_HAVE_FAST is no longer used (trick for communicating with        Cmake/Diet_config.h.in in order to define HAVE_FAST)   - src/utils/CMakeLists.txt changed accordingly * FindFAST.cmake (really coarse) package detection added. * Cmake/DIET_config.h.in: the following preprocessing symbols were removed:    HAVE_FAST, __FAST_0_4__, __FAST_0_8__, PACKAGE_NAME, PACKAGE_STRING,    PACKAGE_TARNAME, PACKAGE_VERSION, VERSION:  - all the symbols defined by autoconf's AC_INIT are not defined anymore.    This is because those default symbols collide with the ones declared    in omniORB include files. Some of those symbols (PACKAGE_TARNAME,    PACKAGE_VERSION) were never used in diet sources. For some others    (PACKAGE_NAME, PACKAGE_STRING) the occurences seemed to be limited to    the autotools configuration files.  - VERSION preprocessing symbol removed: this symbol is only used in the    documentation (refer to Tutorial.tex or ProgrammersGuide.tex).  - FAST related symbols (HAVE_FAST, __FAST_0_4__, __FAST_0_8__) are now    handled by the main CMakeLists.txt.   ---Injay2461

---
+++ Cmake/FindFAST.cmake
@@ -0,0 +1,61 @@
+#
+# This module checks wether FAST (Fast Agent System Timers, refer to 
+# http://graal.ens-lyon.fr/FAST/ ) tools are installed. When they are it
+# determines the location of the FAST libraries and include files.
+# This code sets the following variables:
+#   - FAST_FOUND set to either "YES" or "NO"
+#   - FAST_LIBRARIES full path to the FAST libraries (and depending libraries)
+#     additional linker flags in a un*x fashion (i.e. basically the result of
+#     the command "fast-config --libs fast").
+#   - FAST_INCLUDE_DIR full path to the FAST include directories (and depending
+#     package directories) preprocessing flags in a un*x fashion (i.e.
+#     basically the result of the command "fast-config --cflags fast"
+#   - FAST_VERSION essentially the result of the command 
+#     "fast-config --version fast"
+
+FIND_PROGRAM( FAST_CONFIG_EXECUTABLE
+  NAMES
+  fast-config
+  PATH
+  ${CYGWIN_INSTALL_PATH}/bin
+  /bin
+  /usr/bin
+  /usr/local/bin
+  /sbin
+  DOC "Path to program fast-config (FAST tools)"
+)
+
+IF( FAST_CONFIG_EXECUTABLE )
+  EXEC_PROGRAM( ${FAST_CONFIG_EXECUTABLE}
+    ARGS "--libs fast"
+    OUTPUT_VARIABLE TEMP_OUTPUT )
+  SET( FAST_LIBRARIES ${TEMP_OUTPUT}
+    CACHE STRING "FAST libraries link directives" )
+  EXEC_PROGRAM( ${FAST_CONFIG_EXECUTABLE}
+    ARGS "--cflags fast"
+    OUTPUT_VARIABLE TEMP_OUTPUT )
+  SET( FAST_INCLUDE_DIR ${TEMP_OUTPUT}
+    CACHE STRING "FAST preprocessor include directives" )
+  # fast.m4 elaborates a lot more on the version number of FAST as obtained
+  # through "fast-config --vesion fast": it extracts major and minor version
+  # number but alas the parsing requires some shell functionality... For the
+  # time being, we will skip this part:
+  EXEC_PROGRAM( ${FAST_CONFIG_EXECUTABLE}
+    ARGS "--version fast"
+    OUTPUT_VARIABLE TEMP_OUTPUT )
+  SET( FAST_VERSION ${TEMP_OUTPUT}
+    CACHE STRING "FAST version number" )
+  IF( FAST_LIBRARIES AND FAST_INCLUDE_DIR )
+    MARK_AS_ADVANCED(
+      FAST_CONFIG_EXECUTABLE
+      FAST_LIBRARIES
+      FAST_INCLUDE_DIR
+      FAST_VERSION )
+    SET( FAST_FOUND "YES" )
+  ELSE( FAST_LIBRARIES AND FAST_INCLUDE_DIR )
+    SET( FAST_FOUND "NO" )
+  ENDIF( FAST_LIBRARIES AND FAST_INCLUDE_DIR )
+ELSE( FAST_CONFIG_EXECUTABLE )
+  SET( FAST_FOUND "NO" )
+ENDIF( FAST_CONFIG_EXECUTABLE )
+
