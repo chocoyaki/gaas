a94cb186389442b6ab66909359366d6c46501757
Eric Boix <eboix@ens-lyon.fr>
2006-03-07 12:49:17 +0000

    - Examples are not build by default anymore.     - Cmake/FindOmniORB.cmake: omniORB libraries COS4 and COSDynamic4 are not mandatory       anymore (but optional). By default linking against omniORB now links againt       omniORB4, omnithread, and omniDynamic4 libraries. This should enable cmake usage       on Debian packaging.  ---  Injay 2461

--- Cmake/FindOmniORB.cmake
+++ Cmake/FindOmniORB.cmake
@@ -14,6 +14,15 @@
 # compilation or because you installed it in an "unusual" directory).
 # Just set OMNIORB4_DIR it to your specific installation directory
 #
+#### The list of required libraries for omniORB is not so clear. See
+# http://www.omniorb-support.com/pipermail/omniorb-list/2005-May/026666.html
+# The answer should basically be in
+#    - omniORB-4.0.X/README.*
+#    - omniORB-4.0.X/readmes/*
+# but doesn't seem that obvious to newbies.
+# The logic we took is here to impose the detection of omniORB4, omnithread,
+# and omniDynamic4 libraries. Optionaly we search for libraries COS4 and
+#
  
 FIND_PATH( OMNIORB4_INCLUDE_DIR omniORB4/CORBA.h
   PATHS
@@ -23,14 +32,6 @@ FIND_PATH( OMNIORB4_INCLUDE_DIR omniORB4/CORBA.h
 )
 
 SET( OMNIORB4_DEFAULT_LIB_PATH /usr/lib /usr/local/lib )
-#### For the list of required libraries for omniORB see
-# http://www.omniorb-support.com/pipermail/omniorb-list/2005-May/026666.html
-# Basically, look at
-#  - omniORB-4.0.5/README.*
-#  - omniORB-4.0.5/readmes/*
-# Platfrom dependencies might (only?) happen for Win32/VC++ (sigh):
-# "Because of a bug in VC++, unless you require the dynamic interfaces, it
-#  is suggested that you use a smaller stub library called msvcstub.lib."
 
 FIND_LIBRARY(  OMNIORB4_LIBRARY_omniORB4 omniORB4
   PATHS ${OMNIORB4_DIR}/lib
@@ -42,12 +43,17 @@ FIND_LIBRARY( OMNIORB4_LIBRARY_omnithread omnithread
         ${OMNIORB4_DEFAULT_LIB_PATH}
 )
 
-FIND_LIBRARY( OMNIORB4_LIBRARY_COS4 COS4
+FIND_LIBRARY( OMNIORB4_LIBRARY_omniDynamic4 omniDynamic4
   PATHS ${OMNIORB4_DIR}/lib
         ${OMNIORB4_DEFAULT_LIB_PATH}
 )
 
-FIND_LIBRARY( OMNIORB4_LIBRARY_COSDynamic4 COSDynamic4
+FIND_LIBRARY( OMNIORB4_LIBRARY_COS4 COS4      # Optional library
+  PATHS ${OMNIORB4_DIR}/lib
+        ${OMNIORB4_DEFAULT_LIB_PATH}
+)
+
+FIND_LIBRARY( OMNIORB4_LIBRARY_COSDynamic4 COSDynamic4 # Optional library
   PATHS ${OMNIORB4_DIR}/lib
         ${OMNIORB4_DEFAULT_LIB_PATH}
 )
@@ -63,8 +69,7 @@ FIND_PROGRAM(OMNIORB4_IDL_COMPILER
 IF( OMNIORB4_INCLUDE_DIR )
 IF( OMNIORB4_LIBRARY_omniORB4 )
 IF( OMNIORB4_LIBRARY_omnithread )
-IF( OMNIORB4_LIBRARY_COS4 )
-IF( OMNIORB4_LIBRARY_COSDynamic4 )
+IF( OMNIORB4_LIBRARY_omniDynamic4 )
 IF( OMNIORB4_IDL_COMPILER )
 
   SET( OMNIORB4_FOUND "YES" )
@@ -72,19 +77,35 @@ IF( OMNIORB4_IDL_COMPILER )
   MARK_AS_ADVANCED( OMNIORB4_INCLUDE_DIR )
   MARK_AS_ADVANCED( OMNIORB4_LIBRARY_omniORB4 )
   MARK_AS_ADVANCED( OMNIORB4_LIBRARY_omnithread )
+  MARK_AS_ADVANCED( OMNIORB4_LIBRARY_omniDynamic4 )
   MARK_AS_ADVANCED( OMNIORB4_LIBRARY_COS4 )
   MARK_AS_ADVANCED( OMNIORB4_LIBRARY_COSDynamic4 )
   MARK_AS_ADVANCED( OMNIORB4_IDL_COMPILER )
   SET( OMNIORB4_LIBRARIES
     ${OMNIORB4_LIBRARY_omniORB4}
     ${OMNIORB4_LIBRARY_omnithread}
-    ${OMNIORB4_LIBRARY_COS4}            # mandatory on FC2 and graal
-    ${OMNIORB4_LIBRARY_COSDynamic4}     # mandatory on FC2 and graal
+    ${OMNIORB4_LIBRARY_omniDynamic4}
   )
 
+  # We do not require the existence of the libraries COS4 and COSDynamic4
+  # since they exist in some distribution packages (e.g. Fedora Core) and
+  # not in others (e.g. Debian). When present we add them to the list of
+  # libraries we link against...
+  IF( OMNIORB4_LIBRARY_COS4 )
+    SET( OMNIORB4_LIBRARIES
+      ${OMNIORB4_LIBRARIES}
+      ${OMNIORB4_LIBRARY_COS4}
+    )
+  ENDIF( OMNIORB4_LIBRARY_COS4 )
+  IF( OMNIORB4_LIBRARY_COSDynamic4 )
+    SET( OMNIORB4_LIBRARIES
+      ${OMNIORB4_LIBRARIES}
+      ${OMNIORB4_LIBRARY_omnithread}
+    )
+  ENDIF( OMNIORB4_LIBRARY_COSDynamic4 )
+
 ENDIF( OMNIORB4_IDL_COMPILER )
-ENDIF( OMNIORB4_LIBRARY_COSDynamic4 )
-ENDIF( OMNIORB4_LIBRARY_COS4 )
+ENDIF( OMNIORB4_LIBRARY_omniDynamic4 )
 ENDIF( OMNIORB4_LIBRARY_omnithread )
 ENDIF( OMNIORB4_LIBRARY_omniORB4 )
 ENDIF( OMNIORB4_INCLUDE_DIR )
