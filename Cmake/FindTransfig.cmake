e82ede1259edb9148965227074892a8334d86606
Eric Boix <eboix@ens-lyon.fr>
2006-05-29 19:09:41 +0000

    Transfig (fig2dev) non detection made more explicit --- Injay2461

--- Cmake/FindTransfig.cmake
+++ Cmake/FindTransfig.cmake
@@ -1,6 +1,10 @@
 #
-# this module looks for dia the gtk+ based diagram creation program
-# refer to http://www.gnome.org/projects/dia/
+# This module looks for transfig's fig2dev conversion utility
+# (fig stands for "Facility for Interactive Generation of figures")
+#
+# The following variables are set:
+# FIG2DEV               - Path to fig2dev utility
+# TRANSFIG_FOUND        - When false, don't try to use TRANSFIG
 #
  
 FIND_PROGRAM(FIG2DEV
@@ -13,7 +17,9 @@ FIND_PROGRAM(FIG2DEV
   /usr/local/bin
   /sbin
 )
-MARK_AS_ADVANCED(
-  FIG2DEV
-)
 
+SET( TRANSFIG_FOUND FALSE )
+IF( FIG2DEV )
+  SET( TRANSFIG_FOUND TRUE )
+  MARK_AS_ADVANCED( FIG2DEV )
+ENDIF( FIG2DEV )
