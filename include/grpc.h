75101de7271b652b6af0c4008adfc82b77c14d3a
Eddy Caron <eddy.caron@ens-lyon.fr>
2006-06-29 12:50:03 +0000

    Just an include to DIET_grpc.h, thus no modification in GridRPC GGF client

---
+++ include/grpc.h
@@ -0,0 +1,15 @@
+/****************************************************************************/
+/* GridRPC compliancy for the client interface of DIET                      */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Eddy CARON (Eddy.Caron@ens-lyon.fr)				    */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2006/06/29 12:50:03  ecaron
+ * Just an include to DIET_grpc.h, thus no modification in GridRPC GGF client
+ *
+ ****************************************************************************/
+#include <DIET_grpc.h>
