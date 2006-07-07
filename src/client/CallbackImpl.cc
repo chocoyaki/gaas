9b184a92d6f18112aff20a81bb7d920f5a08e25f
Abdelkader Amar <abdelkader.amar@ens-lyon.fr>
2006-07-07 09:29:03 +0000

    Modify the solveResults prototype: adding the parameter solve_res that represents the execution status of the asynchronous request.

--- src/client/CallbackImpl.cc
+++ src/client/CallbackImpl.cc
@@ -8,6 +8,10 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
+ * Revision 1.6  2006/07/07 09:29:03  aamar
+ * Modify the solveResults prototype: adding the parameter solve_res
+ * that represents the execution status of the asynchronous request.
+ *
  * Revision 1.5  2003/10/13 13:02:22  cpera
  * Replace long by CORBA::Long.
  *
@@ -47,16 +51,23 @@ CORBA::Long CallbackImpl::ping()
 }
 
 CORBA::Long CallbackImpl::notifyResults(const char * path,
-    const corba_profile_t& pb,
-    CORBA::Long reqID)
+					const corba_profile_t& pb,
+					CORBA::Long reqID)
 {
-  return 0;
+  /*
+  if (solve_res != GRPC_NO_ERROR)
+    CallAsyncMgr::Instance()->setReqErrorCode(reqID, solve_res);
+  */
 }
 
-CORBA::Long CallbackImpl::solveResults(const char * path, 
-    const corba_profile_t& pb,
-    CORBA::Long reqID)
+CORBA::Long
+CallbackImpl::solveResults(const char * path, 
+			   const corba_profile_t& pb,
+			   CORBA::Long reqID,
+			   CORBA::Long solve_res)
 {
   // notify solve result availability
+  if (solve_res != GRPC_NO_ERROR)
+    CallAsyncMgr::Instance()->setReqErrorCode(reqID, solve_res);
   return CallAsyncMgr::Instance()->notifyRst(int(reqID), &(const_cast<corba_profile_t&>(pb)));
 }
--- src/client/CallbackImpl.hh
+++ src/client/CallbackImpl.hh
@@ -8,6 +8,10 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
+ * Revision 1.4  2006/07/07 09:29:03  aamar
+ * Modify the solveResults prototype: adding the parameter solve_res
+ * that represents the execution status of the asynchronous request.
+ *
  * Revision 1.3  2003/10/13 13:02:22  cpera
  * Replace long by CORBA::Long.
  *
@@ -34,11 +38,12 @@ public:
   virtual ~CallbackImpl();
   virtual CORBA::Long ping();
   virtual CORBA::Long notifyResults(const char * path,
-                            const corba_profile_t& pb,
-                            CORBA::Long reqID);
+				    const corba_profile_t& pb,
+				    CORBA::Long reqID);
   virtual CORBA::Long solveResults(const char * path, 
-                            const corba_profile_t& pb,
-                            CORBA::Long reqID);
+				   const corba_profile_t& pb,
+				   CORBA::Long reqID,
+				   CORBA::Long solve_res);
 };
 
 #endif
