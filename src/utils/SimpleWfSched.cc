8b09b49a125a4938ccffbde3f087ca1588b37114
Abdelkader Amar <abdelkader.amar@ens-lyon.fr>
2006-07-10 11:11:08 +0000

    Adding time and rescheduling management

--- src/utils/SimpleWfSched.cc
+++ src/utils/SimpleWfSched.cc
@@ -10,6 +10,9 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
+ * Revision 1.2  2006/07/10 11:11:08  aamar
+ * Adding time and rescheduling management
+ *
  * Revision 1.1  2006/04/14 14:04:51  aamar
  * The basis scheduler (random scheduling).
  *
@@ -33,11 +36,37 @@ SimpleWfSched::~SimpleWfSched() {
  */
 void
 SimpleWfSched::execute() {
+  if (response != NULL) { // if the runtime use the MA
+    cout << "---------------------" << endl;
+    cout << "Display the response " << endl;
+    for (unsigned int ix=0; 
+	 ix < (*(this->response)).wfn_seq_resp.length(); 
+	 ix++) {
+      corba_response_t cr = (*(this->response)).wfn_seq_resp[ix].response;
+      for (unsigned int jx=0; jx < cr.servers.length(); jx++) {
+	corba_server_estimation_t cs_estim = cr.servers[jx];
+	for (unsigned kx =0; kx < cs_estim.estim.estValues.length(); kx++) {
+	  cout << "ix = " << ix <<
+	    ", jx = " << jx <<
+	    ", kx = " << kx << 
+	    ", v_tag = " << cs_estim.estim.estValues[kx].v_tag << 
+	    ", v_idx = " << cs_estim.estim.estValues[kx].v_idx << 
+	    ", v_value = " << cs_estim.estim.estValues[kx].v_value << endl;
+	}
+      }
+    }
+  } // end if
+
+  cout << "---------------------" << endl;
   Node * n = NULL;
   cout << "The simple scheduler starts execution " << endl;
   cout << "Linking the dag nodes ... " << endl;
   myDag->linkAllPorts();
   cout << "linking is done " << endl;
+
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  myDag->setTheBeginning(tv);
 
   while (true) {
     // get the ready nodes
@@ -46,6 +75,15 @@ SimpleWfSched::execute() {
     for (uint ix=0; ix<len; ix++) {
       n = readyNodes[ix];
       n->start();
+    }
+    /*
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << myDag -> toXML();
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    */
+    if ((this->myCltReoMan != NULL) && (!myDag->checkScheduling())) {
+      cout << "the scheduling is incorrect" << endl << 
+	"try to reschedule" << endl;
     }
     if (readyNodes.size() == 0) {
       if (myDag->isDone())
