2d462cd12a4882defcf58ba6cb29935a7a229490
Eric Boix <eboix@ens-lyon.fr>
2006-10-17 17:51:12 +0000

    TestORBMgrInit.cxx and OmniDaemon.bash are now configurable dynamically    out of cmake's collected results. --- Injay2461

--- src/Testing/TestORBMgrInit.cxx
+++ src/Testing/TestORBMgrInit.cxx
@@ -28,7 +28,6 @@ char* c_string( const string & s)
 
 
 /*
- * Converts the string arguments to C-style arrays and invoke the
  * CORBA::ORB_init() through DIET::ORBMgr::init()
  * @OmniNamesHost Suggested hostname
  * @OmniNamesPort Suggested port
@@ -36,96 +35,102 @@ char* c_string( const string & s)
 bool ORBMgrInit( string OmniNamesHost, 
                  string OmniNamesPort )
 {
-  int    TempArgc = 2;
-  char** TempArgv;
-  TempArgv = (char**)malloc( TempArgc * sizeof(char*) );
+   /////// Prepare the arguments CORBA::ORB_init( ) invocation
+   int    TempArgc = 2;
+   char** TempArgv;
+   TempArgv = (char**)malloc( TempArgc * sizeof(char*) );
+ 
+   // First argument:
+   TempArgv[ 0 ] = "-ORBInitRef";
+   
+   // Second argument is of the form NameService=corbaname::<host>:<port>:
+   string EndPointArg = "NameService=corbaname::";
+   if( OmniNamesHost.length() )
+     EndPointArg += OmniNamesHost;
+   EndPointArg += ":";
+   if( OmniNamesPort.length() )
+     EndPointArg += OmniNamesPort;
+   TempArgv[ 1 ] = c_string( EndPointArg );
 
-  // First argument:
-  TempArgv[ 0 ] = "-ORBInitRef";
+   const char* options[][2] =
+   {
+      { "traceLevel", "10" },
+      { "inConScanPeriod", "0" },
+      { "outConScanPeriod", "0" },
+      { "maxGIOPConnectionPerServer", "50" },
+      { "giopMaxMsgSize","33554432"},
+      { 0, 0 }
+   }; 
+
+   //////// Try to initialize the ORB server:
+   try
+   {
+
+      CORBA::ORB_ptr orb =  CORBA::ORB_init( TempArgc, TempArgv,
+                                             "omniORB4", options );
   
-  // Second argument:
-  string EndPointArg = "NameService=corbaname::";
-  if( OmniNamesHost.length() )
-    EndPointArg += OmniNamesHost;
-  EndPointArg += ":";
+      if( CORBA::is_nil( orb ) ) {
+         std::cout << "Return orb is nil ? :" << std::endl;
+         return false;
+      }
+      std::cout << "Return orb is valid." << std::endl;
+  
+      // Just make sure the orb is valid by listing initial services:
+      CORBA::ORB::ObjectIdList *idlist = orb->list_initial_services();
+      if ( !idlist )
+      {
+         std::cout << "Empty list of initial services." << std::endl;
+         CORBA::release( orb );
+         return false;
+      }
 
-  if( OmniNamesPort.length() )
-    EndPointArg += OmniNamesPort;
-  TempArgv[ 1 ] = c_string( EndPointArg );
-  std::cout << TempArgc << TempArgv[0] << TempArgv[ 1 ] <<"XXXXX\n"; 
-  // Eventually try to initialize the ORB server:
-  try
-  {
-     const char* options[][2] = { { "traceLevel", "40" }, { 0, 0 } }; 
-    // = {{"inConScanPeriod","0"},{"outConScanPeriod","0"},
-    //    {"maxGIOPConnectionPerServer","50"},
-    //    //{"giopMaxMsgSize","33554432"},
-    //    {0,0}};
-
-    CORBA::ORB_ptr orb =  CORBA::ORB_init( TempArgc, TempArgv, "omniORB4",
-options);
-
-    if( CORBA::is_nil( orb ) ) {
-       std::cout << "Return orb is nil ? :" << std::endl;
-       return false;
-    }
-    std::cout << "Return orb is valid." << std::endl;
-
-    CORBA::ORB::ObjectIdList *idlist = orb->list_initial_services();
-    if ( !idlist )
-    {
-       std::cout << "Empty Id list." << std::endl;
-       CORBA::release( orb );
-       return false;
-    }
-    std::cout << "Lenght of Id list: "  << idlist->length() << std::endl;
-    std::cout << "Element of Id list: " <<  std::endl;
-    for( unsigned int i = 0; i < idlist->length(); i++ )
-    {
-       std::cout << "    "  << (*idlist)[i] << std::endl;
-    }
-    
-    std::cout << "ZOOOOOOOOOOO" 
-        << orb->resolve_initial_references((*idlist)[0])
-        <<  endl;
-    std::cout << (*idlist)[1]  << "  " 
-        << orb->resolve_initial_references((*idlist)[1])
-        <<  endl;
-    CORBA::release( orb );
-    return true;
-  }
-  catch( CORBA::INITIALIZE& ex )
-  {
-    // Just to illustrate the syntax of minor handling:
-    if (ex.minor() == omni::INITIALIZE_TransportError )
-      std::cout << "CORBA exception TransportError." <<  endl;
-    else if (ex.minor() == omni::INITIALIZE_InvalidORBInitArgs )
-      std::cout << "CORBA exception InvalidORBInitArgs." <<  endl;
-    else if (ex.minor() == omni::INITIALIZE_FailedBOAInit )
-      std::cout << "CORBA exception FailedBOAInit." <<  endl;
-    else if (ex.minor() == omni::INITIALIZE_FailedORBInit )
-      std::cout << "CORBA exception FailedORBInit." <<  endl;
-    else if (ex.minor() == omni::INITIALIZE_FailedPOAInit )
-      std::cout << "CORBA exception FailedPOAInit." <<  endl;
-    else
-       std::cout << "CORBA exception generic Initialize." <<  endl;
-    return false;
-  }
-  catch( CORBA::Exception& )
-  {
-    std::cout << "Caught CORBA::Exception." << endl;
-    return false;
-  }
-  return false;
+      // Display the collected information to make sur orb is OK:
+      std::cout << "List of initial services:" << std::endl;
+      for( unsigned int i = 0; i < idlist->length(); i++ )
+      {
+         std::cout << "    "  << (*idlist)[i] << std::endl;
+      }
+      
+      // The orb looks ok indeed !
+      CORBA::release( orb );
+      return true;
+   }
+   catch( CORBA::INITIALIZE& ex )
+   {
+      // Just to illustrate the syntax of minor handling:
+      if (ex.minor() == omni::INITIALIZE_TransportError )
+        std::cout << "CORBA exception TransportError." <<  endl;
+      else if (ex.minor() == omni::INITIALIZE_InvalidORBInitArgs )
+        std::cout << "CORBA exception InvalidORBInitArgs." <<  endl;
+      else if (ex.minor() == omni::INITIALIZE_FailedBOAInit )
+        std::cout << "CORBA exception FailedBOAInit." <<  endl;
+      else if (ex.minor() == omni::INITIALIZE_FailedORBInit )
+        std::cout << "CORBA exception FailedORBInit." <<  endl;
+      else if (ex.minor() == omni::INITIALIZE_FailedPOAInit )
+        std::cout << "CORBA exception FailedPOAInit." <<  endl;
+      else
+         std::cout << "CORBA exception generic Initialize." <<  endl;
+      return false;
+   }
+   catch( CORBA::Exception& )
+   {
+      std::cout << "Caught CORBA::Exception." << endl;
+      return false;
+   }
+   return false;
 }
 
 int TestORBMgrInit( int argc, char* argv[] )
 {
-  std::cout << "Testing the existence of an ORB server "
-               " @DIET_CMAKE_FQDN_HOSTNAME@"
-            << std::endl;
-  std::cout << ORBMgrInit( "frog.lip.ens-lyon.fr", "28009" ) << std::endl;
-  //std::cout << ORBMgrInit( "localhost", "11112809" ) << std::endl;
-
-  return EXIT_SUCCESS;
+   string OmniNamesHost = DIET_CTEST_FQDN_HOSTNAME;
+   string OmniNamesPort = DIET_CTEST_OMNIORB_MAPPER_PORT;
+   std::cout << "Testing the presence of an ORB server on "
+             << OmniNamesHost << " on port " << OmniNamesPort   
+             << std::endl;
+   bool orbMgrSuccess = ORBMgrInit( OmniNamesHost, OmniNamesPort );
+   if( ! orbMgrSuccess )
+   {
+      return EXIT_FAILURE;
+   }
+   return EXIT_SUCCESS;
 }
