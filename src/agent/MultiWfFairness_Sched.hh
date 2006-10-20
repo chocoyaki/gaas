059a3ffaaf831581eb1e349e710504837cfc26bb
Abdelkader Amar <abdelkader.amar@ens-lyon.fr>
2006-10-20 09:05:29 +0000

    The fairness on finish time scheduler for the MA DAG.

---
+++ src/agent/MultiWfFairness_Sched.hh
@@ -0,0 +1,165 @@
+/****************************************************************************/
+/* [Documentation Here!]                                                    */
+/*                                                                          */
+/* Author(s):                                                               */
+/* - Abdelkader AMAR (Abdelkader.Amar@ens-lyon.fr)                          */
+/*                                                                          */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id :
+ * $Log :
+ ****************************************************************************/
+
+#ifndef _MULTIWFFAIRNESS_SCHED_HH_
+#define _MULTIWFFAIRNESS_SCHED_HH_
+
+#include <map>
+#include <vector>
+
+#include "Dag.hh"
+#include "AbsWfMetaSched.hh"
+
+
+using namespace std;
+
+class DagState;
+class NodeState;
+
+class MultiWfFairness_Sched : public AbsWfMetaSched {
+public:
+  MultiWfFairness_Sched();
+  
+  virtual ~MultiWfFairness_Sched();
+
+  /**
+   * set the scheduler used by the MA DAG
+   * @param sched the base scheduler to be used
+   */
+  virtual void 
+  setSched(MaDag_sched * sched);
+
+  /**
+   * Workflow submission function. 
+   * @param wf_desc workflow string description
+   * @param dag_id the dag ID
+   */
+  virtual wf_sched_response_t * 
+  submit_wf (const corba_wf_desc_t& wf_desc, int dag_id,
+	     MasterAgent_var parent);
+
+  /**
+   * Remove a client from myClients map
+   * @param dag id
+   */
+  virtual void
+  removeClient(const string dag);
+
+  /**
+   * Set the node state as done
+   * @param dagId the DAG identifier
+   * @param nodeId the node identifier in the DAG
+   */
+  virtual void 
+  setNodeAsDone(const char* dagId, const char* nodeId);
+
+protected:
+  /**
+   * The scheduling of each dag using a scheduling algorithm <R>
+   */
+  map<Dag*, wf_node_sched_seq_t> sOwn;
+
+  /**
+   * The scheduling using the multiworkflow algorithm
+   */
+  map<Node*, wf_node_sched_t> sMulti;
+
+  /**
+   * Save the state of dags
+   * See the class DagState
+   */
+  map<Dag*, DagState> dagsState;
+
+  /**
+   * Save the state of nodes
+   * See the class NodeState
+   */
+  map<Node*, NodeState> nodesState;
+
+  /**
+   * Vector of unexecuted dags
+   * must be sorted
+   */
+  vector<Dag *> U;
+
+  /**
+   * Init the scheduling
+   * 1. schedule each dag
+   * 2. mark each dag as unexecuted, initialize the slowdown
+   */
+  virtual 
+  void init();
+
+  /**
+   * The main scheduling function
+   */
+  void
+  fairnessOnFinishTime(const wf_response_t * wf_response);
+
+  Node *
+  get1stReadyNode(vector<Node*>& v);
+
+
+  void
+  sortU();
+};
+
+class DagState {
+public:
+  DagState();
+
+  /**
+   * true if the dag is marked as executed otherwise false
+   *
+   */
+  bool executed;
+
+  /**
+   * the makespan of the DAG scheduled alone
+   */
+  double makespan;
+
+  /**
+   * Dag slowdown
+   */
+  double slowdown;
+  
+  /**
+   * The number of executed (or scheduled) nodes
+   */
+  unsigned int executedNodes;
+};
+
+class NodeState {
+public:
+  NodeState();
+
+  /**
+   * true if the dag is marked as executed otherwise false
+   */
+  bool executed;
+
+  /**
+   * Node finish time based on the multi-workflow algorithm
+   */
+  double multiFT;
+
+  /**
+   * Node finish time when the dag is scheduled alone
+   */
+  double ownFT;
+};
+
+#endif   /* not defined _MULTIWFFAIRNESS_SCHED_HH */
+
+
+
