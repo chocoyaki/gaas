810244397e36f83f13fd2581e1d000783494319e
Abdelkader Amar <abdelkader.amar@ens-lyon.fr>
2006-11-27 14:20:07 +0000

    Xerces version detection corrected.

--- Cmake/FindXerces.cmake
+++ Cmake/FindXerces.cmake
@@ -8,10 +8,10 @@
 # XERCES_FOUND         - When false, don't try to use Xerces
 # XERCES_INCLUDE_DIR   - Directories to include to use Xerces
 # XERCES_LIBRARIES     - Files to link against to use Xerces
+# XERCES_DIR           - (optional) Suggested installation directory to search
 # XERCES_VERSION_MAJOR - Major version number
 # XERCES_VERSION_MINOR - Minor version number
-# XERCES_VERSION_REVISION - Revision version number
-# XERCES_DIR           - (optional) Suggested installation directory to search
+# XERCES_VERSION       - Full xerces version (major and minor version numbers)
 #
 # XERCES_DIR can be used to make it simpler to find the various include
 # directories and compiled libraries when Xerces was not installed in the
@@ -46,34 +46,6 @@ IF( XERCES_LIBRARY )
   MARK_AS_ADVANCED ( XERCES_DIR )
   MARK_AS_ADVANCED ( XERCES_INCLUDE_DIR )
   MARK_AS_ADVANCED ( XERCES_LIBRARY )
-  # Optionaly, extract the the version number from the acconfig.h file:
-  IF( EXISTS ${XERCES_INCLUDE_DIR}/xercesc/util/XercesVersion.hpp )
-    FILE( READ ${XERCES_INCLUDE_DIR}/xercesc/util/XercesVersion.hpp 
-          XERCES_VERSION_HPP )
-    STRING( REGEX MATCH "[^ ]#define[\t ]+XERCES_VERSION_MAJOR[\t ]+[0-9]"
-            XERCES_VERSION_MAJOR "${XERCES_VERSION_HPP}" )
-    STRING( REGEX REPLACE ".*XERCES_VERSION_MAJOR[\t ]([0-9]).*" "\\1"
-            XERCES_VERSION_MAJOR "${XERCES_VERSION_MAJOR}")
-    STRING( REGEX MATCH "[^ ]#define[\t ]+XERCES_VERSION_MINOR[\t ]+[0-9]"
-            XERCES_VERSION_MINOR "${XERCES_VERSION_HPP}" )
-    STRING( REGEX REPLACE ".*XERCES_VERSION_MINOR[\t ]([0-9]).*" "\\1"
-            XERCES_VERSION_MINOR "${XERCES_VERSION_MINOR}")
-    STRING( REGEX MATCH "[^ ]#define[\t ]+XERCES_VERSION_REVISION[\t ]+[0-9]"
-            XERCES_VERSION_REVISION "${XERCES_VERSION_HPP}" )
-    STRING( REGEX REPLACE ".*XERCES_VERSION_REVISION[\t ]([0-9]).*" "\\1"
-            XERCES_VERSION_REVISION "${XERCES_VERSION_REVISION}" )
-  ELSE( EXISTS ${XERCES_INCLUDE_DIR}/xercesc/util/XercesVersion.hpp )
-     SET( XERCES_VERSION_MAJOR "NOT-FOUND" )
-  ENDIF( EXISTS ${XERCES_INCLUDE_DIR}/xercesc/util/XercesVersion.hpp )
-  SET( XERCES_VERSION_MAJOR ${XERCES_VERSION_MAJOR}
-       CACHE STRING "Xerces major version number." )
-  MARK_AS_ADVANCED( XERCES_VERSION_MAJOR )
-  SET( XERCES_VERSION_MINOR ${XERCES_VERSION_MINOR}
-       CACHE STRING "Xerces minor version number." )
-  MARK_AS_ADVANCED( XERCES_VERSION_MINOR )
-  SET( XERCES_VERSION_REVISION ${XERCES_VERSION_REVISION}
-       CACHE STRING "Xerces revision version number." )
-  MARK_AS_ADVANCED( XERCES_VERSION_REVISION )
 ENDIF( XERCES_LIBRARY )
 ENDIF( XERCES_INCLUDE_DIR )
 
@@ -90,3 +62,35 @@ IF(NOT XERCES_FOUND )
   MESSAGE("    Xerces installation...")
   SET( XERCES_DIR "" CACHE PATH "Root of Xerces install directory." )
 ENDIF( NOT XERCES_FOUND )
+
+IF( XERCES_INCLUDE_DIR )
+  FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/xercesv.cc
+    "#include \"${XERCES_INCLUDE_DIR}/xercesc/util/XercesVersion.hpp\"\n"
+    "#include <stdio.h>\n"
+    "int main(int argc, char ** argv) {\n"
+    "        printf(\"%d%d\", XERCES_VERSION_MAJOR, XERCES_VERSION_MINOR);\n"
+    "        return 0;}\n")
+  TRY_RUN (COMPILE_RESULT RUN_RESULT 
+    ${CMAKE_CURRENT_BINARY_DIR}
+    ${CMAKE_CURRENT_BINARY_DIR}/xercesv.cc
+    OUTPUT_VARIABLE OUTPUT)
+
+  IF ( RUN_RESULT )
+    EXECUTE_PROCESS ( COMMAND ${CMAKE_CXX_COMPILER} 
+      ${CMAKE_CURRENT_BINARY_DIR}/xercesv.cc
+      -o ${CMAKE_CURRENT_BINARY_DIR}/xercesv)
+    EXECUTE_PROCESS ( COMMAND ${CMAKE_CURRENT_BINARY_DIR}/xercesv
+      OUTPUT_VARIABLE XERCES_VERSION)
+
+    STRING (REGEX REPLACE "^([0-9])[0-9]" "\\1"
+      XERCES_VERSION_MAJOR "${XERCES_VERSION}")
+    STRING (REGEX REPLACE "^[0-9]([0-9])" "\\1"
+      XERCES_VERSION_MINOR "${XERCES_VERSION}")
+  ELSE ( RUN_RESULT )
+    SET( XERCES_VERSION "0" )
+    SET( XERCES_VERSION_MAJOR "NOT-FOUND" )
+    SET( XERCES_VERSION_MINOR "NOT-FOUND" )
+  ENDIF (RUN_RESULT)
+ENDIF ( XERCES_INCLUDE_DIR )
+
+
