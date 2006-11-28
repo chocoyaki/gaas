3965bcfe22b73cc7b54841831e48af502e524863
Cédric Tedeschi <cedric.tedeschi@inria.fr>
2006-11-28 17:46:15 +0000

    header and update library path

--- src/examples/JXTA/scripts/SeD1/SeD1_jxta_load.sh
+++ src/examples/JXTA/scripts/SeD1/SeD1_jxta_load.sh
@@ -1,9 +1,24 @@
+#****************************************************************************#
+#* JXTA MA loader script                                                    *#
+#*                                                                          *#
+#*  Author(s):                                                              *#
+#*    - Cedric Tedeschi cedric.tedeschi@ens-lyon.fr                         *#
+#*                                                                          *#
+#* $LICENSE$                                                                *#
+#****************************************************************************#
+#* $Id$
+#* $Log$
+#* Revision 1.4  2006/11/28 17:46:15  ctedesch
+#* header and update library path
+#*
+#****************************************************************************#
+
 #!/bin/sh
 # Launch a JXTA SeD providing MatSUM with SeD1.cfg as configuration file for the DIET SeD
 
+export JXTA_LIB=${DIET_HOME}/src/lib
 # To be configured
-# export DIET_HOME=
-# export JXTA_LIB=${DIET_HOME}/install/lib
-
-java -cp ${DIET_HOME}/src/SeD:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/saxpath.jar:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTASeD ${DIET_HOME}/src/examples/cfgs/SeD1.cfg MatSUM
+# export INSTALL_DIR=
+# export EXECUTABLE_DIR=${INSTALL_DIR}/src/SeD
+java -cp ${EXECUTABLE_DIR}:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/saxpath.jar:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTASeD ${DIET_HOME}/src/examples/cfgs/SeD1.cfg MatSUM
 
