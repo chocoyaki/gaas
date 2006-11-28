8a73092da1140242aaeb4239ebbec2781af0bd4b
Cédric Tedeschi <cedric.tedeschi@inria.fr>
2006-11-28 17:31:49 +0000

    update library path

--- src/examples/JXTA/scripts/MMA2/MMA2_load.sh
+++ src/examples/JXTA/scripts/MMA2/MMA2_load.sh
@@ -8,6 +8,9 @@
 #****************************************************************************#
 #* $Id$
 #* $Log$
+#* Revision 1.6  2006/11/28 17:28:33  ctedesch
+#* update library path
+#*
 #* Revision 1.5  2006/11/28 17:27:40  ctedesch
 #* add old logs in header
 #*
@@ -30,9 +33,9 @@
 #!/bin/sh
 # Launch a JXTA MA with MA2.cfg  as configuration file for the DIET MA
 
+export JXTA_LIB=${DIET_HOME}/src/lib
 # To be configured
-# export DIET_HOME=
-# export JXTA_LIB=${DIET_HOME}/install/lib
+# export INSTALL_DIR=
+# export EXECUTABLE_DIR=${INSTALL_DIR}/src/agent
 
-java -cp ${DIET_HOME}/src/agent:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/saxpath.jar:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTAMultiMA ${DIET_HOME}/src/examples/cfgs/MA2.cfg
-
+java -cp ${EXECUTABLE_DIR}:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTAMultiMA ${DIET_HOME}/src/examples/cfgs/MA2.cfg
