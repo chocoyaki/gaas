c10141331224e9f42bf85e8030bcbd92bd5ee2c8
Cédric Tedeschi <cedric.tedeschi@inria.fr>
2006-11-28 17:57:04 +0000

    update library path

--- src/examples/JXTA/scripts/SeD3/SeD3_jxta_load.sh
+++ src/examples/JXTA/scripts/SeD3/SeD3_jxta_load.sh
@@ -8,6 +8,9 @@
 #****************************************************************************#
 #* $Id$
 #* $Log$
+#* Revision 1.4  2006/11/28 17:57:04  ctedesch
+#* update library path
+#*
 #* Revision 1.3  2006/11/28 17:55:56  ctedesch
 #* add old logs in header
 #*
@@ -24,10 +27,8 @@
 #!/bin/sh
 # Launch a JXTA SeD providing MatPROD with SeD2.cfg as configuration file for the DIET SeD
 
+export JXTA_LIB=${DIET_HOME}/src/lib
 # To be configured
-# export DIET_HOME=
-# export JXTA_LIB=${DIET_HOME}/install/lib
-
-java -cp ${DIET_HOME}/src/SeD:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/saxpath.jar:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTASeD ${DIET_HOME}/src/examples/cfgs/SeD3.cfg MaSUM
-
-
+# export INSTALL_DIR=
+# export EXECUTABLE_DIR=${INSTALL_DIR}/src/SeD
+java -cp ${EXECUTABLE_DIR}:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/saxpath.jar:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTASeD ${DIET_HOME}/src/examples/cfgs/SeD3.cfg MatSUM
