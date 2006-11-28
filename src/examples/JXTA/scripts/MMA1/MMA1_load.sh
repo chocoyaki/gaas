412dee4848e4eac6126cd5cf5e2a42d91ab65402
Cédric Tedeschi <cedric.tedeschi@inria.fr>
2006-11-28 17:06:27 +0000

    header

--- src/examples/JXTA/scripts/MMA1/MMA1_load.sh
+++ src/examples/JXTA/scripts/MMA1/MMA1_load.sh
@@ -1,3 +1,17 @@
+#****************************************************************************#
+#* JXTA MA loader script                                                    *#
+#*                                                                          *#
+#*  Author(s):                                                              *#
+#*    - Cedric Tedeschi cedric.tedeschi@ens-lyon.fr                         *#
+#*                                                                          *#
+#* $LICENSE$                                                                *#
+#****************************************************************************#
+#* $Id$
+#* $Log$
+#* Revision 1.4  2006/11/28 17:06:27  ctedesch
+#* header
+#*
+#****************************************************************************#
 #!/bin/sh
 # Launch a JXTA MA with MA1.cfg as configuration file for the DIET MA
 
