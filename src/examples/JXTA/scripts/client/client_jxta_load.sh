e0536cc5b0fb7bbf3ba555e951890c531d7700fd
Cédric Tedeschi <cedric.tedeschi@inria.fr>
2006-11-28 17:04:35 +0000

    update the path for JXTA libraries

--- src/examples/JXTA/scripts/client/client_jxta_load.sh
+++ src/examples/JXTA/scripts/client/client_jxta_load.sh
@@ -8,6 +8,9 @@
 #****************************************************************************#
 #* $Id$#
 #* $Log$
+#* Revision 1.6  2006/11/28 17:04:35  ctedesch
+#* update the path for JXTA libraries
+#*
 #* Revision 1.5  2006/11/28 17:02:03  ctedesch
 #* add old logs in header
 #*
@@ -35,9 +38,7 @@
 #!/bin/sh
 # Launch a JXTA client requesting for MatSUM
 
-# To be configured
-# export DIET_HOME=
-# export JXTA_LIB=${DIET_HOME}/install/lib
+export JXTA_LIB=${DIET_HOME}/src/lib
 
 java -cp .:${DIET_HOME}/src/examples/JXTA:${JXTA_LIB}/jxtaext.jar:${JXTA_LIB}/jaxen-core.jar:/${JXTA_LIB}/jxtaSwing.jar:${JXTA_LIB}/jaxen-jdom.jar:${JXTA_LIB}/jdom.jar:${JXTA_LIB}/saxpath.jar:${JXTA_LIB}/bcprov-jdk14.jar:${JXTA_LIB}/jxta.jar:${JXTA_LIB}/jxtasecurity.jar:${JXTA_LIB}/junit.jar:${JXTA_LIB}/cryptix32.jar:${JXTA_LIB}/org.mortbay.jetty.jar:${JXTA_LIB}/cryptix-asn1.jar:${JXTA_LIB}/jxtaptls.jar:${JXTA_LIB}/log4j.jar:${JXTA_LIB}/javax.servlet.jar JXTAClient MatSUM
 
