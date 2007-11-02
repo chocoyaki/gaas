9dfaaa5011cc3b6920466aa2bcf9b54ed98a2f17
Yves Caniou <yves.caniou@ens-lyon.fr>
2007-11-02 16:12:57 +0000

    Ajout des fichiers exemple. Tests en cours.

--- src/examples/Batch/TestAllBatch/batch_server.c
+++ src/examples/Batch/TestAllBatch/batch_server.c
@@ -1,5 +1,5 @@
 /****************************************************************************/
-/* DIET server for Batch submission                                         */
+/* General batch submission: serial service on a parallel machine           */
 /*                                                                          */
 /*  Author(s):                                                              */
 /*    - Yves CANIOU (ycaniou@ens-lyon.fr)                                   */
@@ -7,18 +7,9 @@
 /****************************************************************************/
 /* $Id$
  * $Log$
- * Revision 1.12  2007/11/02 16:12:57  ycaniou
+ * Revision 1.1  2007/11/02 16:12:57  ycaniou
  * Ajout des fichiers exemple. Tests en cours.
  *
- * Revision 1.11  2007/04/16 22:29:02  ycaniou
- * Added an async client
- * Improved batch example
- *
- * Revision 1.10  2006/11/28 20:40:31  ycaniou
- * Only headers
- *
- * Revision 1.9  2006/11/27 08:13:59  ycaniou
- * Added missing fields Id and Log in headers
  ****************************************************************************/
 
 #include <string.h>
@@ -43,7 +34,7 @@ void make_perf(diet_profile_t *pb)
   
   /* For the moment, give arbitrary values */
   pb->walltime = 125 ;
-  pb->nbprocs = 2 ;
+  pb->nbprocs = 1 ;
   pb->nbprocess = pb->nbprocs ;
 }
 
@@ -108,18 +99,24 @@ int solve_concatenation(diet_profile_t *pb)
     return 2 ;
   }
   sprintf(prologue,
+	  "batchID=$DIET_BATCHNAME\n"
 	  "echo \"Name of the frontale station: $DIET_NAME_FRONTALE\"\n"
-	  "echo \"Reserved Nodes are:\"\n"
-	  "echo $DIET_BATCH_NODESLIST\n"
 	  "echo \"Number of nodes:  $DIET_BATCH_NBNODES\"\n"
-	  "echo \"Name of the batch file containing their identity:"
-	  " $DIET_BATCH_NODESFILE\"\n"
+	  "case \"$batchID\" in\n"
+	  "  loadleveler) echo DIET_BATCH_NODESLIST, DIET_BATCH_NODESFILE \
+	                  cannot be used in this case ;;\n"
+	  "  *) echo \"Reserved Nodes are:\"\n"
+	  "     echo $DIET_BATCH_NODESLIST\n"
+	  "     echo Name of the batch file containing their identity: \
+	        $DIET_BATCH_NODESFILE;;\n"
+	  "esac\n"
 	  "\n") ;
-  
+
   /* Data management: scp for file1, NFS for file2 */
-  /* Note: one can do the NFS cp in a C thread (see batch_server_2) */
-  /* mettre dans batch_server_2 un test sur la taille pour s'assurer 
-     que le fichier est copié :
+  /* Note for advanced usage:  
+     one can do the NFS cp in a C thread (see batch_server_2)
+     Don't forget to put in batch_server_2 a test on size to be sure that file
+     is copied:
 
      while ( size != %d ) ; do
      sleep 1
@@ -132,19 +129,29 @@ int solve_concatenation(diet_profile_t *pb)
     return 2 ;
   }
   sprintf(copying,
-	  "WORKING_DIRECTORY=/home/ycaniou/JobMPI/\n"
-	  "# Copy the file on reserved nodes\n"
-	  "for i in $DIET_BATCH_NODESLIST ; do\n"
-	  "  scp $DIET_NAME_FRONTALE:%s $i:/tmp/%s_local\n"
-	  "done\n"
-	  "input_file1=/tmp/%s_local\n"
-	  "\n"
-	  "# Use NFS (we are not on the frontale anymore!)\n"
-	  "ssh $DIET_NAME_FRONTALE \"cp %s $WORKING_DIRECTORY/\"\n"
-	  "input_file2=%s\n\n",
-	  path1,basename(path1),basename(path1),path2,basename(path2)) ; 
+	  "case $batchID in\n"
+	  "  oar1.6) echo Execute on OAR1.6\n"
+	  "          WORKING_DIRECTORY=/home/ycaniou/JobMPI/\n"
+	  "          # Copy the file on reserved nodes\n"
+	  "          for i in $DIET_BATCH_NODESLIST ; do\n"
+	  "            scp $DIET_NAME_FRONTALE:%s $i:/tmp/%s_local\n"
+	  "          done\n"
+	  "          input_file1=/tmp/%s_local\n\n"
+	  "          # Use NFS (we are not on the frontale anymore!)\n"
+	  "          ssh $DIET_NAME_FRONTALE \"cp %s $WORKING_DIRECTORY/\"\n"
+	  "          input_file2=$WORKING_DIRECTORY/%s ;;\n",
+	  "  loadleveler) echo Execute on Loadleveler\n"
+	  "               # Machine //, all files accessible from all nodes\n"
+	  "               WORKING_DIRECTORY=/users/cri/diet/YC/JobMPI/\n"
+	  "               input_file1=%s\n"
+	  "               input_file2=%s ;;\n",
+	  "  *) echo NOT TESTED! ;;\n"
+	  "esac\n",
+	  path1,basename(path1),basename(path1),path2,basename(path2)
+	  path1,path2
+	  ) ; 
   
-  /* The MPI command itself */
+  /* The proceeding of the command */
   local_output_filename = "/tmp/result_local.txt" ;
   cmd = (char*)malloc(500*sizeof(char)) ;  
   if( cmd == NULL ) {
@@ -153,12 +160,26 @@ int solve_concatenation(diet_profile_t *pb)
   }
   sprintf(cmd,
 	  "# Execution\n"
-	  "cd $WORKING_DIRECTORY\n"
-	  "local_output_filename=%s\n"
-	  "mpirun.mpich_1_2 -np $DIET_USER_NBPROCS "
-	  "-machinefile $DIET_BATCH_NODESFILE "
-	  "concatenation $input_file1 %.2f "
-	  "$input_file2 $local_output_filename\n"
+	  "case $batchID in\n"
+	  "  oar1.6) cd $WORKING_DIRECTORY\n"
+	  "          local_output_filename=%s\n"
+	  "          mpirun.mpich_1_2 -np $DIET_USER_NBPROCS \
+	             -machinefile $DIET_BATCH_NODESFILE \
+	             concatenation $input_file1 %.2f \
+	             $input_file2 $local_output_filename ;;\n"
+	  "  loadleveler) cd $WORKING_DIRECTORY\n"
+	  "          local_output_filename=%s\n"
+	  "          # Test if job is serial or parallel (usage of poe)\n"
+	  "          if [ %d -eq 1 ] ; then\n"
+	  "            ./concatenation $input_file1 %.2f \
+	               $input_file2 $local_output_filename\n"
+	  "          else\n"
+	  "            poe -np $DIET_USER_NBPROCS \
+	               -machinefile $DIET_BATCH_NODESFILE \
+	               concatenation $input_file1 %.2f \
+	               $input_file2 $local_output_filename\n"
+	  "          fi\n"
+	  "esac\n"
 	  "\n",local_output_filename, *ptr_nbreel) ;
   
   /* Put the Output file in the right place */
---
+++ src/examples/Batch/TestAllBatch/client.c
@@ -0,0 +1,129 @@
+/****************************************************************************/
+/* DIET client for Batch submission: loadleveler on a parallel machine      */
+/*                                                                          */
+/*  Author(s):                                                              */
+/*    - Yves CANIOU (ycaniou@ens-lyon.fr)                                   */
+/* $LICENSE$                                                                */
+/****************************************************************************/
+/* $Id$
+ * $Log$
+ * Revision 1.1  2007/11/02 16:12:57  ycaniou
+ * Ajout des fichiers exemple. Tests en cours.
+ *
+ ****************************************************************************/
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#include "DIET_client.h"
+
+#include <sys/time.h>
+
+/* argv[1]: client config file path */
+
+#define SUBMISSION_TYPE 0 /* 0: seq or //, 1: // only, 2: seq only */
+
+int
+main(int argc, char* argv[])
+{
+  char* path = NULL;
+  diet_profile_t* profile = NULL;
+  double nbreel=0 ;
+  size_t file_size = 0 ;
+  struct timeval tv ;
+  struct timezone tz ;
+  int server_found = 0 ;
+      
+  if (argc != 5) {
+    fprintf(stderr, "Usage: %s <file.cfg> <file1> <double> <file2>\n",
+	    argv[0]);
+    return 1;
+  }
+
+  if (diet_initialize(argv[1], argc, argv)) {
+    fprintf(stderr, "DIET initialization failed !\n");
+    return 1;
+  }
+
+  path = "concatenation" ;
+  profile = diet_profile_alloc(path, 2, 2, 3);
+  if (diet_file_set(diet_parameter(profile,0), DIET_VOLATILE, argv[2])) {
+    printf("file1: diet_file_set error\n");
+    return 1;
+  }
+  nbreel = strtod(argv[3],NULL) ;
+  diet_scalar_set(diet_parameter(profile,1), &nbreel, DIET_VOLATILE,
+		  DIET_DOUBLE);
+  if (diet_file_set(diet_parameter(profile,2), DIET_VOLATILE, argv[4])) {
+    printf("file2: diet_file_set error\n");
+    return 1;
+  }
+  if (diet_file_set(diet_parameter(profile,3), DIET_VOLATILE, NULL)) {
+    printf("result_file: diet_file_set error\n");
+    return 1;
+  }
+
+
+  /*********************
+   * DIET Call
+   *********************/
+
+  gettimeofday(&tv, &tz);
+  printf("L'heure de soumission est %ld:%ld\n\n",tv.tv_sec,tv.tv_usec) ;
+
+  if( SUBMISSION_TYPE == 1 ) {
+    /* To ask explicitely for a parallel submission */
+    printf("Call explicitly a parallel service\n") ;
+    if (!diet_parallel_call(profile)) {
+      printf("Job correctly submitted!\n\n\n") ;
+      server_found = 1 ;
+      diet_file_get(diet_parameter(profile,3), NULL, &file_size, &path);
+      if (path && (*path != '\0')) {
+	printf("Location of returned file is %s, its size is %d.\n",
+	       path, (int) file_size);
+      }
+    } else printf("Error in diet_parallel_call()\n") ;
+  } else if ( SUBMISSION_TYPE == 0 ) {
+    printf("All services, seq and parallel, with the correct name are Ok.\n") ;
+    if (!diet_call(profile)) {
+      printf("Job correctly submitted!\n\n\n") ;
+      server_found = 1 ;
+      diet_file_get(diet_parameter(profile,3), NULL, &file_size, &path);
+      if (path && (*path != '\0')) {
+	printf("Location of returned file is %s, its size is %d.\n",
+	       path, (int) file_size);
+      }
+    } else printf("Error in diet_call()\n") ;
+  } else { /* only sequential servers are considered */
+    printf("Only proposed sequential services can be selected.\n") ;
+    if (!diet_sequential_call(profile)) {
+      printf("Job correctly submitted!\n\n\n") ;
+      server_found = 1 ;
+      diet_file_get(diet_parameter(profile,3), NULL, &file_size, &path);
+      if (path && (*path != '\0')) {
+	printf("Location of returned file is %s, its size is %d.\n",
+	       path, (int) file_size);
+      }
+    } else printf("Error in diet_sequential_call()\n") ;
+  }
+
+  gettimeofday(&tv, &tz);
+  printf("L'heure de terminaison est %ld:%ld\n\n",tv.tv_sec,tv.tv_usec) ;
+  
+  if( server_found == 1 ) {
+    /* If uncommented, the result file is removed */
+    /*  diet_free_data(diet_parameter(profile,4)); */
+    free(path) ;
+  }
+  
+  diet_profile_free(profile);
+  
+  diet_finalize();
+
+  return 0;
+}
+
