e8c5fb44e48d60b79cc1d607bad130d790af423c
Eddy Caron <eddy.caron@ens-lyon.fr>
2010-03-28 20:27:57 +0000

    Take into account configuration with and without CVS

--- Cmake/Ctest/CDash-local-EX-CORI-DAGDA.cmake
+++ Cmake/Ctest/CDash-local-EX-CORI-DAGDA.cmake
@@ -1,7 +1,8 @@
+
 ####### Include the file CDash-common-env.txt  with the Ctest environment 
 ####### You can fill this file with information of your local environment
 ############################
-
+SET ( CVSFORCE "0" )
 INCLUDE ( "CDash-common-env.txt" )
 
 SET(CTEST_BUILD_NAME "myos_gcc_x.x.x_OmniORB_x.x.x_EX_CORI_DAGDA")
