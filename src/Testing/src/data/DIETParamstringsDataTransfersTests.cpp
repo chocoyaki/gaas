9d0ce9629969a63a70739576b5e4141a60122d8f
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-03-01 17:43:37 +0000

    Tests for paramstrings transfers

--- src/Testing/src/data/DIETParamstringsDataTransfersTests.cpp
+++ src/Testing/src/data/DIETParamstringsDataTransfersTests.cpp
@@ -1,7 +1,7 @@
 /*
- * DIETStringsDataTransfers.cpp
+ * DIETParamstringsDataTransfers.cpp
  *
- * call a service with 1 in, 1 inout and 1 out string
+ * call a service with 1 in, 1 inout paramstring
  *
  * Created on: 01 march 2011
  * Author: bdepardo
@@ -19,22 +19,21 @@
 
 const unsigned int SIZE = 3;
 
-static const unsigned int NB_PB_STRING = 1;
-static const char* PB_STRING[NB_PB_STRING] =
-  {"SPRINT"};
+static const unsigned int NB_PB_PSTRING = 1;
+static const char* PB_PSTRING[NB_PB_PSTRING] =
+  {"PSPRINT"};
 
 
-BOOST_FIXTURE_TEST_SUITE( strings_data_transfers, AllDataTransferSeDFixture )
+BOOST_FIXTURE_TEST_SUITE( paramstrings_data_transfers, AllDataTransferSeDFixture )
 
-BOOST_AUTO_TEST_CASE( strings ) {
-  BOOST_TEST_MESSAGE( "-- Test: strings" );
+BOOST_AUTO_TEST_CASE( paramstrings ) {
+  BOOST_TEST_MESSAGE( "-- Test: paramstrings" );
   diet_profile_t* profile = NULL;
   unsigned int i;
   
 
   char *s1;
   char *s2;
-  char *s3;
 
   s1 = (char *) malloc(sizeof(char) * (SIZE + 1));
   s2 = (char *) malloc(sizeof(char) * (SIZE + 1));
@@ -46,7 +45,7 @@ BOOST_AUTO_TEST_CASE( strings ) {
   s2[SIZE] = '\0';
 
 
-  utils::ClientArgs c("strings", "client_testing.cfg");
+  utils::ClientArgs c("paramstrings", "client_testing.cfg");
     
   diet_error_t error = diet_initialize(c.config(), c.argc(), c.argv());
   
@@ -59,26 +58,19 @@ BOOST_AUTO_TEST_CASE( strings ) {
   
 
   /* Characters: no choice it has to be DIET_CHAR */
-  profile = diet_profile_alloc(PB_STRING[0], 0, 1, 2);
+  profile = diet_profile_alloc(PB_PSTRING[0], 0, 1, 1);
   BOOST_CHECK(profile != NULL);
 
-  error = diet_string_set(diet_parameter(profile,0), s1, DIET_VOLATILE);
+  error = diet_paramstring_set(diet_parameter(profile,0), s1, DIET_VOLATILE);
   BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
-  error = diet_string_set(diet_parameter(profile,1), s2, DIET_VOLATILE);
-  BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
-  error = diet_string_set(diet_parameter(profile,2), NULL, DIET_VOLATILE);
+  error = diet_paramstring_set(diet_parameter(profile,1), s2, DIET_VOLATILE);
   BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
 
   error = diet_call(profile);
   BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
 
-  error = diet_string_get(diet_parameter(profile, 2), &s3, NULL);
-  BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
   error = diet_free_data(diet_parameter(profile, 1));
   BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
-  error = diet_free_data(diet_parameter(profile, 2));
-  BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
-  free(s3);
   
   error = diet_profile_free(profile);
   BOOST_CHECK_EQUAL( GRPC_NO_ERROR, error);
