14e5947280e612018aa4857ea78776046dbe0e7a
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-03-19 12:56:45 +0000

    Added test with paramstrings in workflows

--- src/Testing/src/workflow/DIETDataInoutSinkPStringsTests.cpp
+++ src/Testing/src/workflow/DIETDataInoutSinkPStringsTests.cpp
@@ -17,16 +17,16 @@
 #include "utils.hpp"
 
 
-BOOST_FIXTURE_TEST_SUITE( workflows_data_inout_strings_sink, AllDataTransferSeDFixtureWF )
+BOOST_FIXTURE_TEST_SUITE( workflows_data_inout_pstrings_sink, AllDataTransferSeDFixtureWF )
 
 #include "genericSimpleWfClientTest.hpp"
 
 
-BOOST_AUTO_TEST_CASE( data_inout_sink_strings ) {
-  BOOST_TEST_MESSAGE( "-- Test: workflow data inout strings sink" );
+BOOST_AUTO_TEST_CASE( data_inout_sink_pstrings ) {
+  BOOST_TEST_MESSAGE( "-- Test: workflow data inout paramstrings sink" );
 
-  genericSimpleWorkflowClient(WF_DIRECTORY "inout_sink_SPRINT.xml",
-                              "Inout Scalars Sink strings",
+  genericSimpleWorkflowClient(WF_DIRECTORY "inout_sink_PSPRINT.xml",
+                              "Inout Scalars Sink paramstrings",
                               DIET_WF_FUNCTIONAL,
                               WF_DIRECTORY "data_inout_sink_strings.xml");
 }
