ccdb0f110304cbb6f8c81d5b1c9cfda0ca034b37
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-03-21 08:25:48 +0000

    Added a macro to generate tests with logservice activated

--- Cmake/tests/DietTestingMacros.cmake
+++ Cmake/tests/DietTestingMacros.cmake
@@ -70,3 +70,69 @@ macro( diet_test NAME )
 endmacro()
 
 ###############################################################################
+# generate_diet_tests: macro that setup a test with potentially a test using
+# log central
+# @param[in] NAME  test filename stripped from extension
+# @param[in] FIXTURENAME fixture name
+# @param[in] DISABLED (optional) when defined, remove test from master suite
+#                     and display a message to the user.
+###############################################################################
+
+macro( generate_diet_tests NAME FIXTURENAME )
+  if( ${ARGC} MATCHES 3 )
+    if( ${ARGV2} MATCHES "DISABLED" )
+      message( STATUS "${ARGV0}: explicitly disabled by developer" )
+      set( ${ARGV0}-DISABLED ON ) 
+    endif()
+  endif()
+
+  if( NOT DEFINED ${ARGV0}-DISABLED )
+    # create unit tests
+    diet_test( ${NAME} )
+
+    if( DIET_USE_LOG )
+      # create unit tests with log support
+      file( READ "${NAME}.cpp" TEST_CODE )
+      string( REPLACE "${FIXTURENAME}" "${FIXTURENAME}Log" TEST_CODE_LOG "${TEST_CODE}" )
+      set( NEWNAME "${GENERATED_LOG_TESTS_DIR}/${NAME}Log" )
+      file( WRITE "${NEWNAME}.cpp" "${TEST_CODE_LOG}" )
+
+
+      # create unit tests executable
+      add_executable( ${NAME}Log
+        "${NEWNAME}.cpp"
+        #entry point
+        ../common/TestRunner.cpp )
+      
+      # link libraries
+      target_link_libraries( ${NAME}Log
+        ${Boost_LIBRARIES}
+        ${DIET_CLIENT_LIBRARIES} 
+        utils
+        pthread )
+      
+      # test executable installation has not been tested yet -sic-
+      # install( TARGETS ${NAME} DESTINATION bin )
+      
+      string( REGEX REPLACE "DIET(.*)" "\\1" TEST_NAME ${NAME}Log )
+
+      add_test( ${TEST_NAME} ${BIN_DIR}/${NAME}Log )
+
+      # prevent Boost.Test to catch unrelated exceptions 
+      set_property( TEST ${TEST_NAME} 
+        PROPERTY ENVIRONMENT "BOOST_TEST_CATCH_SYSTEM_ERRORS=no;" )
+      # just make sure that our test are run in a serial fashion
+      set_property( TEST ${TEST_NAME} PROPERTY RUN_SERIAL ON )
+      
+      #
+      add_custom_target( ${TEST_NAME}-xml
+	COMMAND ${CMAKE_COMMAND}
+	-DTEST_PROG=${NEWNAME}
+	-DBIN_PATH=${BIN_DIR}
+	-DREPORT_PATH=${REPORT_OUTPUT_PATH}
+	-P ${PROJECT_SOURCE_DIR}/Cmake/tests/runtest.cmake )
+      add_dependencies( test-xml ${TEST_NAME}-xml )
+
+    endif()
+  endif()
+endmacro()
