dd3f647bd66fbfef3811b60a979f9cfb77cdbc8f
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-03-28 22:52:49 +0000

    Launch DIETTestTool for log tests

--- src/Testing/src/common/fixtures.hpp
+++ src/Testing/src/common/fixtures.hpp
@@ -409,6 +409,56 @@ public:
     BOOST_TEST_MESSAGE( "== Test teardown [END]: Stopping LogService ==" );
   }
 };
+
+
+template <const char *config>
+class DIETLogToolFixture : public LogServiceFixture<config> {
+  boost::scoped_ptr<bp::child> process;
+
+public:
+  DIETLogToolFixture() : process(NULL) {
+    BOOST_TEST_MESSAGE( "== Test setup [BEGIN]: Launching DIETLogTool ==" );
+	
+    std::string exec;
+    try {
+      exec = bp::find_executable_in_path(DIETLOGTOOL_COMMAND, DIETLOGTOOL_PATH);
+    } catch (bs::system_error& e) {
+      BOOST_TEST_MESSAGE( "can't find " << DIETLOGTOOL_COMMAND << ": " << e.what() );
+      return;
+    }
+
+    BOOST_TEST_MESSAGE( DIETLOGTOOL_COMMAND << " found: " << exec );
+
+    // setup LogService environment
+    bp::context ctx;
+    ctx.process_name = DIETLOGTOOL_COMMAND;
+    ctx.env["OMNINAMES_LOGDIR"] = OMNINAMES_LOGDIR;
+    ctx.env["OMNIORB_CONFIG"] = OMNIORB_CONFIG;
+	
+    // redirect output to /dev/null
+    ctx.streams[bp::stdout_id] = bp::behavior::null();
+    ctx.streams[bp::stderr_id] = bp::behavior::null();
+
+    // setup LogService arguments
+    std::vector<std::string> args= ba::list_of("");
+
+    // launch DIETLogTool
+    bp::child c = bp::create_child(exec, args, ctx);
+    process.reset(utils::copy_child(c));
+    boost::this_thread::sleep(boost::posix_time::milliseconds(SLEEP_TIME));
+    BOOST_TEST_MESSAGE( "== Test setup [END]:  Launching DIETLogTool ==" );
+  }
+
+  ~DIETLogToolFixture() {
+    BOOST_TEST_MESSAGE( "== Test teardown [BEGIN]: Stopping DIETLogTool ==" );
+    if (process) {
+      process->terminate();
+      process->wait();
+    }
+    boost::this_thread::sleep(boost::posix_time::milliseconds(SLEEP_TIME));
+    BOOST_TEST_MESSAGE( "== Test teardown [END]: Stopping DIETLogTool ==" );
+  }
+};
 #endif // USE_LOG_SERVICE
 
 
@@ -445,7 +495,7 @@ typedef DietSeDFixture <GRPCAddSeD, GRPCBinDir, ConfigSimpleAddSeDLA, DietLAFixt
 
 #ifdef USE_LOG_SERVICE
 char LogServiceConfig[] = LOGSERVICE_CONFIG;
-typedef LogServiceFixture<LogServiceConfig> LogServiceFixtureConf;
+typedef DIETLogToolFixture<LogServiceConfig> LogServiceFixtureConf;
 typedef DietAgentFixture<ConfigMasterAgent, LogServiceFixtureConf> DietMAFixtureLog;
 typedef DietAgentFixture<ConfigLocalAgent, DietMAFixtureLog> DietLAFixtureLog;
 typedef DietMADAGFixture<ConfigMADAG, DietLAFixtureLog> DietMADAGFixtureLALog;
