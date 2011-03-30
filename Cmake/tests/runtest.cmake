b21c2a743c70cb1e3d081f65f22f53968f1c4c31
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-03-30 18:09:56 +0000

    XML reports are now in a file, separated from outputs going to stdout and stderr

--- Cmake/tests/runtest.cmake
+++ Cmake/tests/runtest.cmake
@@ -5,5 +5,5 @@ set( ENV{BOOST_TEST_CATCH_SYSTEM_ERRORS} "no" )
 execute_process( COMMAND ${BIN_PATH}/${TEST_PROG} 
   --report_format=xml 
   --report_level=detailed
-  OUTPUT_FILE "${REPORT_PATH}/${TEST_PROG}.xml"
-  ERROR_FILE "${REPORT_PATH}/${TEST_PROG}.xml" )
+  --report_sink="${REPORT_PATH}/${TEST_PROG}.xml"
+)
