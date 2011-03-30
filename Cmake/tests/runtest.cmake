8368b000e9b7a9c1462eabba429eaa05e8ccbc00
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-03-30 18:35:06 +0000

    Should now correctly create XML reports

--- Cmake/tests/runtest.cmake
+++ Cmake/tests/runtest.cmake
@@ -5,7 +5,7 @@ set( ENV{BOOST_TEST_CATCH_SYSTEM_ERRORS} "no" )
 execute_process( COMMAND ${BIN_PATH}/${TEST_PROG} 
   --report_format=xml 
   --report_level=detailed
-  --report_sink="${REPORT_PATH}/${TEST_PROG}.xml"
+  --report_sink=${REPORT_PATH}/${TEST_PROG}.xml
   OUTPUT_QUIET
   ERROR_QUIET
 )
