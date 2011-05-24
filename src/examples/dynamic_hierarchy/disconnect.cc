a4cccf30f3aefcbc0366c76695391fca54e4e177
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-05-24 17:15:05 +0200

    Use new admin API.

--- src/examples/dynamic_hierarchy/disconnect.cc
+++ src/examples/dynamic_hierarchy/disconnect.cc
@@ -18,14 +18,8 @@
  *****************************************************************************/
 
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
 
-#include <omniORB4/CORBA.h>
-
-#include "SeD.hh"
-#include "LocalAgent.hh"
-#include "ORBMgr.hh"
+#include "DIET_admin.h"
 
 using namespace std;
 
@@ -36,107 +30,31 @@ main (int argc, char** argv) {
     exit(1);
   }
   
-  char * type = argv[1];
-  char * name = argv[2];
+  dynamic_type_t type;
+  char *name = argv[2];
+  int res;
   cout << "Element name: " << name << endl;
 	
+  if (!strcmp(argv[1], "SED")) {
+    type = SED;
+  } else {
+    type = LA;
+  }
 
-  try {
-    //------------------------------------------------------------------------
-    // Initialize ORB object.
-    //------------------------------------------------------------------------
-    CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
-                                                                                
-    //------------------------------------------------------------------------
-    // Bind ORB object to name service object.
-    // (Reference to Name service root context.)
-    //------------------------------------------------------------------------
-    CORBA::Object_var obj = orb->resolve_initial_references("NameService");
-    if (CORBA::is_nil(obj.in()))
-      cout << "Pb nameservice" << endl;
+  res = diet_disconnect_from_hierarchy(type, name);
 
-    //------------------------------------------------------------------------
-    // Narrow this to the naming context (Narrowed reference to root context.)
-    //------------------------------------------------------------------------
-    CosNaming::NamingContext_var nc =
-      CosNaming::NamingContext::_narrow(obj.in());
-    if (CORBA::is_nil(nc.in()))
-      cout << "pb narrow nameservice" << endl;
-                                                                                
-    //------------------------------------------------------------------------
-    // The "name text" put forth by CORBA server in name service.
-    // This same name ("DataServiceName1") is used by the CORBA server when
-    // binding to the name server (CosNaming::Name).
-    //------------------------------------------------------------------------
-    CosNaming::Name _corbaCosName;
-    _corbaCosName.length(2);
-    _corbaCosName[0].kind=(const char*)"";
-    _corbaCosName[1].id=CORBA::string_dup(name);
-    _corbaCosName[1].kind=(const char*)"";
-
-    if (!strcmp(type, "SED"))
-      _corbaCosName[0].id="dietSeD";
-    else
-      _corbaCosName[0].id="dietAgent";
-
-
-    //------------------------------------------------------------------------
-    // Resolve "name text" identifier to an object reference.
-    //------------------------------------------------------------------------
-    CORBA::Object_var obj1;
-    try {
-      obj1 = nc->resolve(_corbaCosName);
-    } catch(CosNaming::NamingContext::NotFound& ex) {
-      cerr <<"context for " << name << " not found" << endl;
-    } catch (CORBA::COMM_FAILURE& ex) {
-      cerr << "system exception caught (COMM_FAILURE): unable to connect to "
-	   << "the CORBA name server" << endl;
-    } catch(omniORB::fatalException& ex) {
-      throw;
-    } catch (...) {
-      cerr << "system exception caught while using the naming service" << endl;
-    }
-
-    if (!strcmp(type, "SED")) {
-      SeD_ptr sed = SeD::_narrow(obj1.in());
-      if (! sed->disconnect())
-	cout << "** Disconnecting done **" << endl;
-      else
-	cout << "** Disconnecting failed **" << endl;
+  if (res) {
+    cout << "** A problem occured while disconnecting ";
+    if (type == SED) {
+      cout << "SeD ";
     } else {
-      LocalAgent_ptr agent = LocalAgent::_narrow(obj1.in());
-      if (! agent->disconnect())
-	cout << "** Disconnecting done **" << endl;
-      else
-	cout << "** Disconnecting failed **" << endl;
+      cout << "LA ";
     }
-  }
-  catch(CORBA::COMM_FAILURE& ex) {
-    cerr << "Caught system exception COMM_FAILURE -- unable to contact the "
-         << "object." << endl;
-    cerr << "CORBA COMM_FAILURE" << endl;
-    return 1;
-  }
-  catch(CORBA::SystemException& ) {
-    cerr << "Caught a CORBA::SystemException." << endl;
-    cerr << "CORBA Exception" << endl;
-    return 1;
-  }
-  catch(CORBA::Exception& ) {
-    cerr << "Caught CORBA::Exception." << endl;
-    return 1;
-  }  catch(omniORB::fatalException& fe) {
-    cerr << "Caught omniORB::fatalException:" << endl;
-    cerr << "  file: " << fe.file() << endl;
-    cerr << "  line: " << fe.line() << endl;
-    cerr << "  mesg: " << fe.errmsg() << endl;
-    cerr << "CORBA Fatal Exception" << endl;
-    return 1;
-  }
-  catch(...) {
-    cerr << "Caught unknown exception." << endl;
-    return 1;
+    cout << name << "." << endl;
+
+    return res;
   }
 
+  cout << "** " << name << " has been disconnected from the hierarchy." << endl;
   return 0;
 }
--- src/examples/dynamic_hierarchy/remove.cc
+++ src/examples/dynamic_hierarchy/remove.cc
@@ -19,14 +19,8 @@
  *****************************************************************************/
 
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
 
-#include <omniORB4/CORBA.h>
-
-#include "SeD.hh"
-#include "LocalAgent.hh"
-#include "ORBMgr.hh"
+#include "DIET_admin.h"
 
 using namespace std;
 
@@ -36,112 +30,37 @@ main (int argc, char** argv) {
     cout << "Usage: " << argv[0] << " <SED|AGENT> <element name> [recursive: 0|1]" << endl;
     exit(1);
   }
-  
-  char * type = argv[1];
-  char * name = argv[2];
+
+  dynamic_type_t type;
+  char *name = argv[2];
   bool recursive = false;
+  int res;
+
+  if (argc == 4) {
+    recursive = atoi(argv[3]);
+  }
+  
+  if (!strcmp(argv[1], "SED")) {
+    type = SED;
+  } else {
+    type = AGENT;
+  }
   cout << "Element name: " << name << endl;
 
-  if (argc > 3 && !strcmp(argv[3], "1"))
-    recursive = true;
-	
+  res = diet_remove_from_hierarchy(type, name, recursive);
 
-  try {
-    //------------------------------------------------------------------------
-    // Initialize ORB object.
-    //------------------------------------------------------------------------
-    CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
-                                                                                
-    //------------------------------------------------------------------------
-    // Bind ORB object to name service object.
-    // (Reference to Name service root context.)
-    //------------------------------------------------------------------------
-    CORBA::Object_var obj = orb->resolve_initial_references("NameService");
-    if (CORBA::is_nil(obj.in()))
-      cout << "Pb nameservice" << endl;
-
-    //------------------------------------------------------------------------
-    // Narrow this to the naming context (Narrowed reference to root context.)
-    //------------------------------------------------------------------------
-    CosNaming::NamingContext_var nc =
-      CosNaming::NamingContext::_narrow(obj.in());
-    if (CORBA::is_nil(nc.in()))
-      cout << "pb narrow nameservice" << endl;
-                                                                                
-    //------------------------------------------------------------------------
-    // The "name text" put forth by CORBA server in name service.
-    // This same name ("DataServiceName1") is used by the CORBA server when
-    // binding to the name server (CosNaming::Name).
-    //------------------------------------------------------------------------
-    CosNaming::Name _corbaCosName;
-    _corbaCosName.length(2);
-    _corbaCosName[0].kind=(const char*)"";
-    _corbaCosName[1].id=CORBA::string_dup(name);
-    _corbaCosName[1].kind=(const char*)"";
-
-    if (!strcmp(type, "SED"))
-      _corbaCosName[0].id="dietSeD";
-    else
-      _corbaCosName[0].id="dietAgent";
-
-
-    //------------------------------------------------------------------------
-    // Resolve "name text" identifier to an object reference.
-    //------------------------------------------------------------------------
-    CORBA::Object_var obj1;
-    try {
-      obj1 = nc->resolve(_corbaCosName);
-    } catch(CosNaming::NamingContext::NotFound& ex) {
-      cerr <<"context for " << name << " not found" << endl;
-    } catch (CORBA::COMM_FAILURE& ex) {
-      cerr << "system exception caught (COMM_FAILURE): unable to connect to "
-	   << "the CORBA name server" << endl;
-    } catch(omniORB::fatalException& ex) {
-      throw;
-    } catch (...) {
-      cerr << "system exception caught while using the naming service" << endl;
-    }
-
-    if (!strcmp(type, "SED")) {
-      SeD_ptr sed = SeD::_narrow(obj1.in());
-      if (! sed->removeElement())
-	cout << "** SED " << name << " killed. **" << endl;
-      else
-	cout << "** SED " << name << " removal failed. **" << endl;
+  if (res) {
+    cout << "** A problem occured during the removal of ";
+    if (type == SED) {
+      cout << "SeD ";
     } else {
-      Agent_ptr agent = Agent::_narrow(obj1.in());
-      if (! agent->removeElement(recursive))
-	cout << "** Agent " << name << " killed. **" << endl;
-      else
-  	cout << "** Agent " << name << " removal failed. **" << endl;
+      cout << "agent ";
     }
-  }
-  catch(CORBA::COMM_FAILURE& ex) {
-    cerr << "Caught system exception COMM_FAILURE -- unable to contact the "
-         << "object." << endl;
-    cerr << "CORBA COMM_FAILURE" << endl;
-    return 1;
-  }
-  catch(CORBA::SystemException& ) {
-    cerr << "Caught a CORBA::SystemException." << endl;
-    cerr << "CORBA Exception" << endl;
-    return 1;
-  }
-  catch(CORBA::Exception& ) {
-    cerr << "Caught CORBA::Exception." << endl;
-    return 1;
-  }  catch(omniORB::fatalException& fe) {
-    cerr << "Caught omniORB::fatalException:" << endl;
-    cerr << "  file: " << fe.file() << endl;
-    cerr << "  line: " << fe.line() << endl;
-    cerr << "  mesg: " << fe.errmsg() << endl;
-    cerr << "CORBA Fatal Exception" << endl;
-    return 1;
-  }
-  catch(...) {
-    cerr << "Caught unknown exception." << endl;
-    return 1;
+    cout << name << "." << endl;
+
+    return res;
   }
 
+  cout << "** " << name << " has been removed." << endl;
   return 0;
 }
