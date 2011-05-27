4e66ff8a32917a06c062e7a6df2f39bdc8d963fb
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-05-27 11:29:54 +0200

    Added new tests

--- src/Testing/src/dynamicHierarchy/DIETDynamicsRemoveTests.cpp
+++ src/Testing/src/dynamicHierarchy/DIETDynamicsRemoveTests.cpp
@@ -21,8 +21,8 @@ BOOST_FIXTURE_TEST_SUITE( DynamicRemove,
 
 
 /* Remove */
-BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove1 ) {
-  BOOST_TEST_MESSAGE( "-- Test: dynamic_hierarchy_remove1" );
+BOOST_AUTO_TEST_CASE( dyn_hier_remove1 ) {
+  BOOST_TEST_MESSAGE( "-- Test: dyn_hier_remove1" );
   int res;
 
   // No recursive operation
@@ -43,20 +43,20 @@ BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove1 ) {
   BOOST_CHECK_EQUAL( DIET_NO_ERROR, res );
 }
 
-BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove2 ) {
-  BOOST_TEST_MESSAGE( "-- Test: dynamic_hierarchy_remove2" );
+BOOST_AUTO_TEST_CASE( dyn_hier_remove2 ) {
+  BOOST_TEST_MESSAGE( "-- Test: dyn_hier_remove2" );
   int res;
 
   // No recursive operation
 
   // Remove MA
   BOOST_TEST_MESSAGE( "-- Remove MA1 (not recursive)" );
-  res = diet_remove_from_hierarchy(AGENT, "MA1", false);
+  res = diet_remove_from_hierarchy(MA, "MA1", false);
   BOOST_CHECK_EQUAL( DIET_NO_ERROR, res );
 
   // Remove LA
   BOOST_TEST_MESSAGE( "-- Remove LA1 (not recursive)" );
-  res = diet_remove_from_hierarchy(AGENT, "LA1", false);
+  res = diet_remove_from_hierarchy(LA, "LA1", false);
   BOOST_CHECK_EQUAL( DIET_NO_ERROR, res );
 
   // Remove SeD
@@ -65,8 +65,8 @@ BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove2 ) {
   BOOST_CHECK_EQUAL( DIET_NO_ERROR, res );
 }
 
-BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove3 ) {
-  BOOST_TEST_MESSAGE( "-- Test: dynamic_hierarchy_remove3" );
+BOOST_AUTO_TEST_CASE( dyn_hier_remove3 ) {
+  BOOST_TEST_MESSAGE( "-- Test: dyn_hier_remove3" );
   int res;
 
   // Recursive operation
@@ -87,8 +87,8 @@ BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove3 ) {
   BOOST_CHECK_EQUAL( DIET_NO_ERROR, res );
 }
 
-BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove4 ) {
-  BOOST_TEST_MESSAGE( "-- Test: dynamic_hierarchy_remove4" );
+BOOST_AUTO_TEST_CASE( dyn_hier_remove4 ) {
+  BOOST_TEST_MESSAGE( "-- Test: dyn_hier_remove4" );
   int res;
 
   // Recursive operation
@@ -99,5 +99,23 @@ BOOST_AUTO_TEST_CASE( dynamic_hierarchy_remove4 ) {
   BOOST_CHECK_EQUAL( DIET_NO_ERROR, res );
 
 }
+
+BOOST_AUTO_TEST_CASE( dyn_hier_remove5 ) {
+  BOOST_TEST_MESSAGE( "-- Test: dyn_hier_remove5" );
+  int res;
+
+
+  // Remove non existing element
+  BOOST_TEST_MESSAGE( "-- Remove MAx" );
+  res = diet_remove_from_hierarchy(AGENT, "MAx", true);
+  BOOST_CHECK_EQUAL( DIET_COMM_ERROR, res );
+
+  // Remove non existing element
+  BOOST_TEST_MESSAGE( "-- Remove SeDx" );
+  res = diet_remove_from_hierarchy(SED, "SeDx", true);
+  BOOST_CHECK_EQUAL( DIET_COMM_ERROR, res );
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
