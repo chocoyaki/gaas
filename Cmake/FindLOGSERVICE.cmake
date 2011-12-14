97237dc0166a968cd23fde8a45310f66095b8682
Haikel Guemar <haikel.guemar@sysfera.com>
2011-12-15 00:10:44 +0100

    cmake: fix compilation with DIET_USE_LOG

--- Cmake/FindLOGSERVICE.cmake
+++ Cmake/FindLOGSERVICE.cmake
@@ -8,10 +8,18 @@
 set (libName "LogForwarderUtils")
 MARK_AS_ADVANCED (libName)
 
+if(BUILD_SHARED_LIBS)
+  set(PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX})
+  set(SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+else()
+  set(PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
+  set(SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif()
+
 find_path(LOG_INCLUDE_DIR LogORBMgr.hh
   PATHS ${LOGSERVICE_DIR}/include 
   DOC "Directory containing the log service include files")
-find_library(LOG_LIBRARY ${libName}
+find_library(LOG_LIBRARY  ${PREFIX}${libName}${SUFFIX}
   PATHS ${LOGSERVICE_DIR}/lib 
   DOC "The log service library")
 
