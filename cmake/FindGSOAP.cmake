c2237db6b176817042d7d657766953764dd253b4
Haikel Guemar <haikel.guemar@sysfera.com>
2012-01-09 16:06:32 +0100

    cmake: fixup module FindGSOAP

--- cmake/FindGSOAP.cmake
+++ cmake/FindGSOAP.cmake
@@ -8,14 +8,21 @@
 #
 
 
-find_library(GSOAP_LIBRARY NAMES gsoapssl)
-find_path(GSOAP_INCLUDE_DIR stdsoap2.h)
+find_library(GSOAP_LIBRARY NAMES gsoapssl
+  PATHS ${GSOAP_SRC_DIR}/gsoap)
+find_path(GSOAP_INCLUDE_DIR stdsoap2.h
+  PATHS ${GSOAP_SRC_DIR}/gsoap)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GSOAP DEFAULT_MSG GSOAP_LIBRARY GSOAP_INCLUDE_DIR)
 
 if(GSOAP_FOUND)
-    set(GSOAP_LIBRARIES ${GSOAP_LIBRARY})
-    set(GSOAP_INCLUDE_DIRS ${GSOAP_INCLUDE_DIR})
+  set(GSOAP_LIBRARIES ${GSOAP_LIBRARY})
+  set(GSOAP_INCLUDE_DIRS ${GSOAP_INCLUDE_DIR})
+  mark_as_advanced(GSOAP_SRC_DIR)
+  mark_as_advanced(GSOAP_LIBRARY)
+  mark_as_advanced(GSOAP_LIBRARIES)
+  mark_as_advanced(GSOAP_INCLUDE_DIR)
+  mark_as_advanced(GSOAP_INCLUDE_DIRS)
 endif(GSOAP_FOUND)
 
