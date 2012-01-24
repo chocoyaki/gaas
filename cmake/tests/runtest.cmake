516c4f465509e620779f872c68b26fe3611f4c19
Haikel Guemar <haikel.guemar@sysfera.com>
2012-01-24 17:47:39 +0100

    cmake: runtest.cmake pass --catch_system_errors=no option to test binary

--- cmake/tests/runtest.cmake
+++ cmake/tests/runtest.cmake
@@ -3,6 +3,7 @@
 # don't use $ENV{XXX} as in documentation ==> does not work
 set( ENV{BOOST_TEST_CATCH_SYSTEM_ERRORS} "no" )
 execute_process( COMMAND ${BIN_PATH}/${TEST_PROG} 
+  --catch_system_errors=no
   --result_code=no
   --report_level=no
   --log_format=xml
