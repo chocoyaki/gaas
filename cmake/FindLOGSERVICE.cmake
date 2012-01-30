d9c368d2d60062dac668b8cfe75cd397ffb03418
Haikel Guemar <haikel.guemar@sysfera.com>
2012-01-30 14:32:20 +0100

    cmake: fix FindLOGSERVICE.cmake

--- cmake/FindLOGSERVICE.cmake
+++ cmake/FindLOGSERVICE.cmake
@@ -19,29 +19,31 @@ endif()
 find_path(LOG_INCLUDE_DIR LogORBMgr.hh
   PATHS ${LOGSERVICE_DIR}/include 
   DOC "Directory containing the log service include files")
-if(WIN32)  
-  find_library(LOG_LIBRARY_UTILS  "LogForwarderUtils" 
-    PATHS ${LOGSERVICE_DIR}/lib 
-    DOC "The log service library")
-  find_library(LOG_LIBRARY_BASE "LogServiceComponentBase" 
-    PATHS ${LOGSERVICE_DIR}/lib 
-    DOC "The log service library") 
-  find_library(LOG_LIBRARY_TBASE "LogServiceToolBase" 
-    PATHS ${LOGSERVICE_DIR}/lib 
-    DOC "The log service library") 
+
+find_library(LOG_LIBRARY_COMPONENT ${libName}
+  PATHS ${LOGSERVICE_DIR}/lib
+  DOC "The log service library")
+find_library(LOG_LIBRARY_UTILS  "LogForwarderUtils"
+  PATHS ${LOGSERVICE_DIR}/lib
+  DOC "The log service library")
+find_library(LOG_LIBRARY_BASE "LogServiceComponentBase"
+  PATHS ${LOGSERVICE_DIR}/lib
+  DOC "The log service library")
+find_library(LOG_LIBRARY_BASE "LogServiceToolBase"
+  PATHS ${LOGSERVICE_DIR}/lib
+  DOC "The log service library")
+set(LOG_LIBRARY ${LOG_LIBRARY_COMPONENT})
+set(LOG_LIBRARIES ${LOG_LIBRARY_COMPONENT}
+  ${LOG_LIBRARY_UTILS} ${LOG_LIBRARY_BASE}
+  ${LOG_LIBRARY_TBASE} ${LOG_LIBRARY_CORBA})
+if(WIN32)
   find_library(LOG_LIBRARY_CORBA "LogCorba" 
     PATHS ${LOGSERVICE_DIR}/lib 
-    DOC "The log service library") 
-  set(LOG_LIBRARY
-    ${LOG_LIBRARY_UTILS} ${LOG_LIBRARY_BASE}
-    ${LOG_LIBRARY_TBASE} ${LOG_LIBRARY_CORBA})  
-else()
-  find_library(LOG_COMPONENT_LIBRARY ${libName})
-  find_library(LOG_UTILS_LIBRARY LogForwarderUtils)
-  set(LOG_LIBRARY ${LOG_COMPONENT_LIBRARY})
-  set(LOG_LIBRARIES ${LOG_COMPONENT_LIBRARY} ${LOG_UTILS_LIBRARY})
+    DOC "The log service library")
+  set(LOG_LIBRARIES ${LOG_LIBRARIES} ${LOG_LIBRARY_CORBA})
 endif()
-  
+
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LOGSERVICE DEFAULT_MSG LOG_INCLUDE_DIR LOG_LIBRARY)
 
