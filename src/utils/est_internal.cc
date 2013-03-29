a5ab9fa8b39906afdec6eb4d8dccbefb6733fa74
Matthieu Imbert <matthieu.imbert@inria.fr>
2013-03-29 12:47:52 +0100

    add storing binary data or NULL terminated strings in estimation vector

--- src/utils/est_internal.cc
+++ src/utils/est_internal.cc
@@ -31,7 +31,7 @@ diet_est_set_internal(estVector_t ev, int tag, double value) {
   for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
     if (tag == (EV_ELT(ev, valIter)).v_tag &&
         (EV_ELT(ev, valIter)).v_idx < 0) {
-      (EV_ELT(ev, valIter)).v_value = value;
+      (EV_ELT(ev, valIter)).v_value.d(value);
       return (1);
     }
   }
@@ -39,7 +39,7 @@ diet_est_set_internal(estVector_t ev, int tag, double value) {
   EV_SETLEN(ev, EV_LEN(ev) + 1);
   (EV_ELT(ev, EV_LEN(ev) - 1)).v_tag = tag;
   (EV_ELT(ev, EV_LEN(ev) - 1)).v_idx = EST_IDX_SCALAR;
-  (EV_ELT(ev, EV_LEN(ev) - 1)).v_value = value;
+  (EV_ELT(ev, EV_LEN(ev) - 1)).v_value.d(value);
 
   return (1);
 } // diet_est_set_internal
@@ -60,7 +60,7 @@ diet_est_get_internal(estVectorConst_t ev, int tag, double errVal) {
   for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
     if (tag == (EV_ELT(ev, valIter)).v_tag &&
         (EV_ELT(ev, valIter)).v_idx < 0) {
-      return ((EV_ELT(ev, valIter)).v_value);
+      return ((EV_ELT(ev, valIter)).v_value.d());
     }
   }
 
@@ -123,7 +123,7 @@ diet_est_array_set_internal(estVector_t ev, int tag, int idx, double value) {
   for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
     if (tag == (EV_ELT(ev, valIter)).v_tag &&
         (EV_ELT(ev, valIter)).v_idx == idx) {
-      (EV_ELT(ev, valIter)).v_value = value;
+      (EV_ELT(ev, valIter)).v_value.d(value);
       return (1);
     }
   }
@@ -131,7 +131,7 @@ diet_est_array_set_internal(estVector_t ev, int tag, int idx, double value) {
   EV_SETLEN(ev, EV_LEN(ev) + 1);
   (EV_ELT(ev, EV_LEN(ev) - 1)).v_tag = tag;
   (EV_ELT(ev, EV_LEN(ev) - 1)).v_idx = idx;
-  (EV_ELT(ev, EV_LEN(ev) - 1)).v_value = value;
+  (EV_ELT(ev, EV_LEN(ev) - 1)).v_value.d(value);
 
   return (1);
 } // diet_est_array_set_internal
@@ -162,7 +162,7 @@ diet_est_array_get_internal(estVectorConst_t ev,
   for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
     if (tag == (EV_ELT(ev, valIter)).v_tag &&
         (EV_ELT(ev, valIter)).v_idx == idx) {
-      return ((EV_ELT(ev, valIter)).v_value);
+      return ((EV_ELT(ev, valIter)).v_value.d());
     }
   }
 
@@ -190,4 +190,109 @@ diet_est_array_defined_internal(estVectorConst_t ev, int tag, int idx) {
 
   return (0);
 } // diet_est_array_defined_internal
+
+int
+diet_est_set_str_internal(estVector_t ev, int tag, const char *value) {
+  if (ev == NULL) {
+    ERROR_DEBUG(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR_DEBUG(__FUNCTION__ << ": tag must be non-negative (" << tag << ")\n", -1);
+  }
+
+  for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
+    if (tag == EV_ELT(ev, valIter).v_tag &&
+        EV_ELT(ev, valIter).v_idx < 0) {
+      EV_ELT(ev, valIter).v_value.s(value);
+      return (1);
+    }
+  }
+
+  EV_SETLEN(ev, EV_LEN(ev) + 1);
+  EV_ELT(ev, EV_LEN(ev) - 1).v_tag = tag;
+  EV_ELT(ev, EV_LEN(ev) - 1).v_idx = EST_IDX_SCALAR;
+  EV_ELT(ev, EV_LEN(ev) - 1).v_value.s(value);
+
+  return (1);
+}
+
+const char*
+diet_est_get_str_internal(estVectorConst_t ev, int tag) {
+  if (ev == NULL) {
+    ERROR_DEBUG(__FUNCTION__ << ": NULL estimation vector", NULL);
+  }
+  if (tag < 0) {
+    ERROR_DEBUG(__FUNCTION__ <<
+          ": tag must be non-negative (" <<
+          tag <<
+          ")\n",
+          NULL);
+  }
+
+  for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
+    if (tag == EV_ELT(ev, valIter).v_tag &&
+        EV_ELT(ev, valIter).v_idx < 0) {
+      return (EV_ELT(ev, valIter).v_value.s());
+    }
+  }
+
+  return (NULL);
+}
+
+int
+diet_est_set_bin_internal(estVector_t ev, int tag, const unsigned char *buf, size_t size) {
+  if (ev == NULL) {
+    ERROR_DEBUG(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR_DEBUG(__FUNCTION__ << ": tag must be non-negative (" << tag << ")\n", -1);
+  }
+
+  for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
+    if (tag == EV_ELT(ev, valIter).v_tag &&
+        EV_ELT(ev, valIter).v_idx < 0) {
+      SeqOctet b;
+      b.length(size);
+      memcpy(b.get_buffer(), buf, size);
+      EV_ELT(ev, valIter).v_value.b(b);
+      return (1);
+    }
+  }
+
+  EV_SETLEN(ev, EV_LEN(ev) + 1);
+  EV_ELT(ev, EV_LEN(ev) - 1).v_tag = tag;
+  EV_ELT(ev, EV_LEN(ev) - 1).v_idx = EST_IDX_SCALAR;
+  SeqOctet b;
+  b.length(size);
+  memcpy(b.get_buffer(), buf, size);
+  EV_ELT(ev, EV_LEN(ev) - 1).v_value.b(b);
+
+  return (1);
+}
+
+int
+diet_est_get_bin_internal(estVectorConst_t ev, int tag, const unsigned char **buf, size_t *size) {
+  if (ev == NULL) {
+    ERROR_DEBUG(__FUNCTION__ << ": NULL estimation vector", -1);
+  }
+  if (tag < 0) {
+    ERROR_DEBUG(__FUNCTION__ <<
+          ": tag must be non-negative (" <<
+          tag <<
+          ")\n",
+          -1);
+  }
+
+  for (unsigned int valIter = 0; valIter < EV_LEN(ev); valIter++) {
+    if (tag == EV_ELT(ev, valIter).v_tag &&
+        EV_ELT(ev, valIter).v_idx < 0) {
+      *size = EV_ELT(ev, valIter).v_value.b().length();
+      *buf = EV_ELT(ev, valIter).v_value.b().get_buffer();
+      return (1);
+    }
+  }
+
+  return (-1);
+}
+
 #endif /* OLD_EV */
