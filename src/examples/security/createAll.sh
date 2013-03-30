9c7ddb899030d02045f03d79e40f603386cb3c29
Guillaume Verger <guillaume.verger@inria.fr>
2013-03-30 15:18:26 +0100

    Security scripts modified

--- src/examples/security/createAll.sh
+++ src/examples/security/createAll.sh
@@ -1,15 +1,112 @@
 #! /bin/bash
 
+function printUsage {
+  echo "Usage: $BASH_SOURCE [OPTION...]"
+  echo
+  echo "  -l, --la NUMBER set the number of LAs (default = 0)"
+  echo "  -c, --configfile"
+  echo "  -h, --help                displays this help"  
+  echo
+  exit 0
+}
+
+function getType {
+  type=$1
+  case "$type" in
+    MA) echo DIET_MASTER_AGENT;;
+    LA) echo DIET_LOCAL_AGENT;;
+    SeD) echo "";;
+  esac
+  }
+
+function create() {
+  num=$1
+  
+  # Create config and certificates
+  echo "Creating $num : ${agentname[$num]}"
+  parent="${parent[$num]}"
+  name="${agentname[$num]}"
+  cfg="configs/$name.cfg"
+  > "$cfg"
+  if [ -n "${agenttype[$num]}" ]; then
+    echo "agentType = ${agenttype[$num]}" >> "$cfg"
+  fi
+  echo "name = $name" >> "$cfg"
+  if [ -n "${parent}" ]; then
+    echo "parentName = $parent" >> "$cfg"
+  fi
+  "$scriptDir/createCert.sh" --agentname "$name" -n "${hostname[$num]}"
+  "$scriptDir/validateCert.sh" --agentname "$name" --authdir "$dir" --config-file "$cfg"
+
+  set -x
+  # Create launch script
+  if [ -n "${binary[$num]}" ];then
+    binary=`echo "${binary[$num]}" | sed 's:\/:\\\/:g'`
+    args="${binargs[$num]}"
+  else
+    binary="dietAgent"
+    args=""
+  fi
+
+  cat "$scriptDir/launch-scripts/Agent.launch" | sed "s/\#NAME\#/$name/g" \
+    | sed "s/\#BINARY\#/$binary/g" \
+    > "$name.launch"
+  chmod +x "$name.launch"
+ 
+  set +x
+  
+  }
+
+function readFile {
+  file=$1
+  nb=0
+  while IFS=";" read parentname agenttype agentname hostname bin_name bin_args
+  do
+    nb=$(($nb+1))
+    parent[$nb]=$parentname
+    agenttype[$nb]=$( getType "$agenttype" )
+    agentname[$nb]=$agentname
+    hostname[$nb]=$hostname
+    binary[$nb]=$bin_name
+    binargs[$nb]=$bin_args
+    echo "${parent[$nb]} -> ${agentname[$nb]} ( ${agenttype[$nb]} ) : ${hostname[$nb]}"
+  done < $file
+  nbelements=$nb
+ echo "No implemented yet"
+}
+
 set -e
 set -u
 
+nbLAs=0
+
+i=0
+argv=()
+for arg in "$@"; do
+    argv[$i]="$arg"
+    i=$((i + 1))
+done
+
+agentname=()
+
+i=0
+while test $i -lt $# ; do
+    arg="${argv[$i]}"
+    case "$arg" in
+        -h|--help) printUsage;;
+        -l|--la) i=$((i + 1)); nbLAs=${argv[$i]};;
+        -c|--configfile) i=$((i + 1)); readFile "${argv[$i]}" ;;
+        *) error "Unknown argument '$arg'";;
+    esac
+    i=$((i + 1))
+done
+
 scriptDir=$(cd `dirname $0` && pwd)
 dir="$PWD/Auth"
+mkdir -p "$PWD/configs"
 hostname=`hostname -I | cut -d" " -f1`
 
-mkdir -p "$PWD/configs"
-
-# Creating omniorn config file
+# Creating omniorb config file
 cfg="configs/omniORB4.cfg"
 > "$cfg"
 echo "InitRef = NameService=corbaname::$hostname:2809" >> "$cfg"
@@ -21,43 +118,20 @@ echo "maxGIOPConnectionPerServer = 1000" >> "$cfg"
 echo "Creating authority"
 "$scriptDir/createAuth.sh" --hostname "$hostname" --directory "$dir"
 
-# Creating MA
-echo "Creating MA"
-name="MA_0"
-maname="$name"
-cfg="configs/$name.cfg"
-> "$cfg"
-echo "agentType = DIET_MASTER_AGENT" >> "$cfg"
-echo "name = $name" >> "$cfg"
-"$scriptDir/createCert.sh" --agentname "$name"
-"$scriptDir/validateCert.sh" --agentname "$name" --authdir "$dir" --config-file "$cfg"
+echo $nbelements
+nb=1
+while test $nb -le $nbelements
+do
+  create $nb  
+  if [ "${agenttype[$nb]}" == "DIET_MASTER_AGENT" ]; then
+    maname="${agentname[$nb]}"
+  fi
+  nb=$(($nb +1))
+done
 
-# Creating LA
-echo "Creating LA"
-parent="$name"
-name="LA_0"
-cfg="configs/$name.cfg"
-> "$cfg"
-echo "agentType = DIET_LOCAL_AGENT" >> "$cfg"
-echo "name = $name" >> "$cfg"
-echo "parentName = $parent" >> "$cfg"
-"$scriptDir/createCert.sh" --agentname "$name"
-"$scriptDir/validateCert.sh" --agentname "$name" --authdir "$dir" --config-file "$cfg"
-
-# Creating SeD
-echo "Creating SeD"
-parent="$name"
-name="SeD_0"
-cfg="configs/$name.cfg"
-> "$cfg"
-echo "name = $name" >> "$cfg"
-echo "parentName = $parent" >> "$cfg"
-"$scriptDir/createCert.sh" --agentname "$name"
-"$scriptDir/validateCert.sh" --agentname "$name" --authdir "$dir" --config-file "$cfg"
 
 # Creating client
 echo "Creating client"
-parent="$name"
 name="client"
 cfg="configs/$name.cfg"
 > "$cfg"
