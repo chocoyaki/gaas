fcfd29369dba8e4c979cfc490378de477d523b86
Guillaume Verger <guillaume.verger@inria.fr>
2014-01-17 15:10:01 +0100

    SeD for ramses experiment has now only 3 parameters for launch_another_sed + Cleaning

--- src/examples/ramses/test-ramses-client-wf-expe.cc
+++ src/examples/ramses/test-ramses-client-wf-expe.cc
@@ -13,26 +13,26 @@
 
 
 int destroy_vms(const char* ips_file_path, int select_private_ip) {
-	//destruction of vms
+  //destruction of vms
 
-	diet_profile_t* profile_destroy_vm;
+  diet_profile_t* profile_destroy_vm;
 
-	profile_destroy_vm = diet_profile_alloc( "vm_destruction_by_ip", 1, 1, 1);
-	diet_file_set(diet_parameter(profile_destroy_vm, 0), ips_file_path, DIET_VOLATILE);
-	diet_scalar_set(diet_parameter(profile_destroy_vm, 1), &select_private_ip, DIET_VOLATILE, DIET_INT);
-	int env = diet_call(profile_destroy_vm);
+  profile_destroy_vm = diet_profile_alloc( "vm_destruction_by_ip", 1, 1, 1);
+  diet_file_set(diet_parameter(profile_destroy_vm, 0), ips_file_path, DIET_VOLATILE);
+  diet_scalar_set(diet_parameter(profile_destroy_vm, 1), &select_private_ip, DIET_VOLATILE, DIET_INT);
+  int env = diet_call(profile_destroy_vm);
 
-	diet_profile_free(profile_destroy_vm);
+  diet_profile_free(profile_destroy_vm);
 
-	return env;
+  return env;
 }
 
 //with OPENSTACK
 
 int main(int argc, char ** argv) {
-	time_t time_start = time(NULL);
-	time_t time_alloc_end;
-	time_t time_end;
+  time_t time_start = time(NULL);
+  time_t time_alloc_end;
+  time_t time_end;
 
   diet_wf_desc_t * bootstrap_profile;
   diet_wf_desc_t * ramses_profile;
@@ -45,8 +45,8 @@ int main(int argc, char ** argv) {
   char * ramses_dag; //the ramses dag grafic1->ramses3d->halo->tree->galaxy
 
   if (argc < 5) {
-	printf("usage %s cfg bootstrap-file-path.xml ramses-dag.xml expe-file-path\n", argv[0]);
-	exit(-1);
+    printf("usage %s cfg bootstrap-file-path.xml ramses-dag.xml expe-file-path\n", argv[0]);
+    exit(-1);
   }
 
   if(diet_initialize(argv[1], argc, argv)) {
@@ -67,53 +67,53 @@ int main(int argc, char ** argv) {
   if(result_diet_wf_call_bootstap == 0) {
     printf("bootstrap Workflow successfuly run\n");
 
-	time_alloc_end = time(NULL);
+    time_alloc_end = time(NULL);
   } else {
     printf("BOOTSTRAP - Workflow execution failed\n");
     exit(-1);
   }
 
 
-	ramses_profile = diet_wf_profile_alloc(ramses_dag, "ramses", DIET_WF_DAG);
-	int result_diet_wf_call_ramses = diet_wf_call(ramses_profile);
+  ramses_profile = diet_wf_profile_alloc(ramses_dag, "ramses", DIET_WF_DAG);
+  int result_diet_wf_call_ramses = diet_wf_call(ramses_profile);
 
-	if(result_diet_wf_call_ramses == 0) {
-		printf("ramses Workflow successfuly run\n");
-		time_end = time(NULL);
-	} else {
-		printf("RAMSES - Workflow execution failed\n");
-		exit(-1);
-	}
-
-
-	time_t alloc_duration = time_alloc_end - time_start;
-	time_t ramses_runtime = time_end - time_alloc_end;
-
-	FILE* expe_file = fopen(expe_file_path, "w+");
-	fprintf(expe_file, "%ld %ld", alloc_duration, ramses_runtime);
-	fclose(expe_file);
+  if(result_diet_wf_call_ramses == 0) {
+    printf("ramses Workflow successfuly run\n");
+    time_end = time(NULL);
+  } else {
+    printf("RAMSES - Workflow execution failed\n");
+    exit(-1);
+  }
 
 
-	//DESTROY VMs
+  time_t alloc_duration = time_alloc_end - time_start;
+  time_t ramses_runtime = time_end - time_alloc_end;
+
+  FILE* expe_file = fopen(expe_file_path, "w+");
+  fprintf(expe_file, "%ld %ld", alloc_duration, ramses_runtime);
+  fclose(expe_file);
+
+
+  //DESTROY VMs
 
   if (result_diet_wf_call_bootstap == 0) {
 
-	char* ramses_ips_file_path;
-	char* nfs_ip_file_path;
-	size_t size;
+    char* ramses_ips_file_path;
+    char* nfs_ip_file_path;
+    size_t size;
 
 
-	//destroy nfs vms
-	diet_wf_file_get(bootstrap_profile, "node-init-vm-nfs-server#ips", &size, &nfs_ip_file_path);
-	env = destroy_vms(nfs_ip_file_path, 1);
-	printf("destruction of nfs-server vm : %s\n", env ? "fails" : "success");
+    //destroy nfs vms
+    diet_wf_file_get(bootstrap_profile, "node-init-vm-nfs-server#ips", &size, &nfs_ip_file_path);
+    env = destroy_vms(nfs_ip_file_path, 1);
+    printf("destruction of nfs-server vm : %s\n", env ? "fails" : "success");
 
 
 
-	//destroy ramses vms
-	diet_wf_file_get(bootstrap_profile, "node-init-vms#ips", &size, &ramses_ips_file_path);
-	env = destroy_vms(ramses_ips_file_path, 1);
-	printf("destruction of ramses vms : %s\n", env ? "fails" : "success");
+    //destroy ramses vms
+    diet_wf_file_get(bootstrap_profile, "node-init-vms#ips", &size, &ramses_ips_file_path);
+    env = destroy_vms(ramses_ips_file_path, 1);
+    printf("destruction of ramses vms : %s\n", env ? "fails" : "success");
   }
 
 
@@ -123,9 +123,9 @@ int main(int argc, char ** argv) {
   diet_wf_free(bootstrap_profile);
   diet_wf_free(ramses_profile);
 
-	int w = 15;
-	printf("sleeping %d s to unsure SedCloudMachinesActions are destroyed\n", w);
-	sleep(w);
+  int w = 15;
+  printf("sleeping %d s to unsure SedCloudMachinesActions are destroyed\n", w);
+  sleep(w);
 
   return 0;
 }
--- src/utils/delta-cloud/IaasInterface.hh
+++ src/utils/delta-cloud/IaasInterface.hh
@@ -18,40 +18,40 @@ authors : Adrian Muresan, Lamiel Toch
 namespace IaaS {
 
 
-class IaasInterface {
+  class IaasInterface {
 
-  public:
-    IaasInterface() {};
+    public:
+      IaasInterface() {};
 
-    /* retrieve all images */
-    virtual std::vector<Image*> * get_all_images() = 0;
+      /* retrieve all images */
+      virtual std::vector<Image*> * get_all_images() = 0;
 
-    /* retrieve all instances */
-    virtual std::vector<Instance*> * get_all_instances() = 0;
+      /* retrieve all instances */
+      virtual std::vector<Instance*> * get_all_instances() = 0;
 
-    /* launches a specified number of instances from an image */
-    virtual std::vector<std::string*> * run_instances(const std::string & image_id, int count, const std::vector<Parameter>& params = std::vector<Parameter>()) = 0;
+      /* launches a specified number of instances from an image */
+      virtual std::vector<std::string*> * run_instances(const std::string & image_id, int count, const std::vector<Parameter>& params = std::vector<Parameter>()) = 0;
 
-    /* terminates a set of instances */
-    virtual int terminate_instances(const std::vector<std::string*> & instance_ids) = 0;
+      /* terminates a set of instances */
+      virtual int terminate_instances(const std::vector<std::string*> & instance_ids) = 0;
 
-	virtual int terminate_instances_by_ips(const std::vector<std::string>& ips, bool select_private_ip = false) = 0;
+      virtual int terminate_instances_by_ips(const std::vector<std::string>& ips, bool select_private_ip = false) = 0;
 
-	/* get the instance id from the ip*/
-	virtual std::string get_id_from_ip(const std::string& ip, bool select_private_ip = false) = 0;
+      /* get the instance id from the ip*/
+      virtual std::string get_id_from_ip(const std::string& ip, bool select_private_ip = false) = 0;
 
-	/* get an Instance by Selecting Id */
+      /* get an Instance by Selecting Id */
 
-	virtual Instance* get_instance_by_id(const std::string& instanceId) = 0;
+      virtual Instance* get_instance_by_id(const std::string& instanceId) = 0;
 
-	/* wait that the instance get ready*/
+      /* wait that the instance get ready*/
 
-	virtual int wait_instance_running(const std::string& instanceId) = 0;
+      virtual int wait_instance_running(const std::string& instanceId) = 0;
 
-    virtual ~IaasInterface() {};
+      virtual ~IaasInterface() {};
 
-    virtual std::string get_instance_state(const std::string& instance_id) = 0;
-};
+      virtual std::string get_instance_state(const std::string& instance_id) = 0;
+  };
 
 }
 
